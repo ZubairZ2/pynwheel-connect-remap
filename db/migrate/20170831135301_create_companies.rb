7280d9d78a24dd1615150fe0c5378b1a6c7e62a1
Asim Sajjad <a.sajjad@intagleo.co.uk>
2017-08-31 19:56:50 +0500

    added company,community models

---
+++ db/migrate/20170831135301_create_companies.rb
@@ -0,0 +1,17 @@
+class CreateCompanies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :email
+      t.string :phone
+      t.string :logo
+      t.boolean :locked
+
+      t.timestamps
+    end
+  end
+end
