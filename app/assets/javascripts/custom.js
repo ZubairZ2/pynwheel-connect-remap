69d829a5c05e15ea18285c270cdd72f1f62c1221
Asim Sajjad <a.sajjad@intagleo.co.uk>
2017-09-11 21:30:54 +0500

    loader js issue

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -26,9 +26,9 @@ $("#company_logo,#community_logo").change(function(){
     readURL(this);
 });
 
-$("#import_data").on("click",function(e){
+$("#import_data").click(function(){
     $("#page-loader").removeClass("hidden")
-})
+});
 
 })
 
