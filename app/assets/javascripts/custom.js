5ce71432bb79f725bc45783f805614af92507312
Asim Sajjad <a.sajjad@intagleo.co.uk>
2017-09-15 18:09:55 +0500

    companies and communities hierarchy restored

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -216,7 +216,7 @@ function showDataTables(){
     $('#miyazaki.table').DataTable({
         "ordering": false
     });
-    $('#communities').DataTable({
+    $('#communities.table').DataTable({
         initComplete : function() {
             $("#communities_filter").detach().appendTo('#new-search-area');
         },
@@ -226,6 +226,18 @@ function showDataTables(){
         language: {
             search: "",
             searchPlaceholder: "Search by community or company name"
+        }
+    });
+    $('#companies.table').DataTable({
+        initComplete : function() {
+            $("#companies_filter").detach().appendTo('#new-search-area');
+        },
+        "ordering": false,
+        "info": false, //Dont display info e.g. "Showing 1 to 4 of 4 entries"
+        "paging": false, //Dont want paging
+        language: {
+            search: "",
+            searchPlaceholder: "Search by company name"
         }
     });
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  resources :companies
+  resources :companies do
+    resources :communities
+  end
   resources :communities do
     resources :floorplans
     resources :units
     get :import
     get :credentials
+    resources :settings , only: :index
   end
   resources :settings , only: :index
 end
