6977f0ecd1bed834041d76013e82d398643d2f95
Asim Sajjad <a.sajjad@intagleo.co.uk>
2017-09-18 17:33:35 +0500

    autopopulate lat long completed

--- app/assets/javascripts/populate_geocode.js
+++ app/assets/javascripts/populate_geocode.js
@@ -1,29 +1,39 @@
 $(document).ready(function () {
-    $(document).on('change', '#community_city', function() {
-        console.log(" * address *")
-        var address = document.getElementById('community_city').value;
-        getLatitudeLongitude(showResult, address)
+    find_geocodes();
+    $(document).on('change', '.address_fields', function() {
+        console.log(" * address changed *")
+        var address = buildAddress()
+        find_geocodes(address);
     })
 
 })
 
-function getLatitudeLongitude(callback, address) {
-    // If adress is not supplied, use default value 'Ferrol, Galicia, Spain'
-    address = address || 'Ferrol, Galicia, Spain';
-    // Initialize the Geocoder
-    geocoder = new google.maps.Geocoder();
-    if (geocoder) {
-        geocoder.geocode({
-            'address': address
-        }, function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                callback(results[0]);
+function find_geocodes(address) {
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode( { 'address': address}, function(results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+            if (latitude != null){
+                console.log(longitude)
+                showResult(latitude,longitude)
             }
-        });
-    }
+        }
+    });
 }
 
-function showResult(result) {
-    document.getElementById('community_latitude').value = result.geometry.location.lat();
-    document.getElementById('community_longitude').value = result.geometry.location.lng();
+function showResult(latitude,longitude) {
+    document.getElementById('community_latitude').value = latitude;
+    document.getElementById('community_longitude').value = longitude;
+}
+
+function buildAddress(){
+    var address = document.getElementById('community_address').value;
+    var city = document.getElementById('community_city').value;
+    var state = document.getElementById('community_state').value;
+    var zip = document.getElementById('community_zip').value;
+    var completeAddress = address+city+state+zip;
+    return completeAddress;
 }
