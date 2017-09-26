1519a93f92ea631150ec6f21b9ae8bc8f687250b
Asim Sajjad <a.sajjad@intagleo.co.uk>
2017-09-26 16:15:21 +0500

    smtp settings added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # MAILER SETTINGS
+  config.action_mailer.default_url_options = { host: ENV['HOST_URL'] }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+       :address => "smtp.gmail.com",
+       :port => 587,
+       :user_name => ENV['SMTP_USER_NAME'],
+       :password => ENV['SMTP_PASSWORD'],
+       :authentication => :plain,
+       :enable_starttls_auto => true
+  }
 end
