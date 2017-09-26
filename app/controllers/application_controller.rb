35f43cae1d6e05a8b2149f60ab6892b957efef6c
asim sajjad <a.sajjad@intagleo.co.uk>
2017-09-26 21:36:10 +0500

    add after sign out path in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,10 @@ class ApplicationController < ActionController::Base
       format.js   { head :forbidden, content_type: 'text/html' }
     end
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
   protected
 
   def layout_by_resource
