e6754d715e5a4f8bdfc8171b1323819c9a8f1e6b
asim sajjad <a.sajjad@intagleo.co.uk>
2017-09-28 19:11:02 +0500

    change alert message classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
 	def flash_class(level)
 	    case level
 	    when 'notice' then "alert alert-success"
-	    when 'error' then "alert alert-warning"
-	    when 'alert' then "alert alert-warning"	
+	    when 'error' then "alert alert-danger"
+	    when 'alert' then "alert alert-danger"	
 	    end
     end
 end
