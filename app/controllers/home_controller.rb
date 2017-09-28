21dd7df77d98e859b385c18020e80af7d758fd68
asim sajjad <a.sajjad@intagleo.co.uk>
2017-09-28 12:54:41 +0500

    fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-  	if current_user.role == "super admin"
+  	if current_user.is_super_admin?
     	@communities = Community.all
     else
     	company = current_user.company
