ad79b7a254eeaffeb103c4c0fa41d017b5296581
asim sajjad <a.sajjad@intagleo.co.uk>
2017-10-13 16:47:58 +0500

    fixes

--- app/models/sitemap.rb
+++ app/models/sitemap.rb
@@ -1,4 +1,5 @@
 class Sitemap < ApplicationRecord
   mount_uploader :image, SiteMapUploader
   belongs_to :community
+  validates :image, :presence => {message: "can't be blank. Please upload site map image first."}
 end
