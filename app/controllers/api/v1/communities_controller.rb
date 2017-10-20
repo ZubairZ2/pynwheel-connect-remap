494a5ac19998257e6fdfdbc9081bd5c556492d96
Asim Sajjad <a.sajjad@intagleo.co.uk>
2017-10-20 15:41:20 +0500

    login service api

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -1,5 +1,24 @@
 class Api::V1::CommunitiesController < ActionController::Base
-	before_action :set_community
+	before_action :set_community, only: [:data]
+
+	def login
+		begin
+			str = params[:community_string].split("@")
+			company = Company.find_by_name(str[0])
+			if company.present?
+				community = company.communities.where(name: str[1])
+				if community.present?
+					render :json=> {:success=>true, :community => community.first.id}, :status => 200
+				else
+					render :json=> {:success=>false, :message => "Community not found"}, :status=>404
+				end
+			else
+				render :json=> {:success=>false, :message => "Community not found"}, :status=>404
+			end
+		rescue Exception => e   
+			render :json=> {:success=>false, :message => e.message}, :status=>500
+		end
+	end
 
 	def data
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,9 @@ Rails.application.routes.draw do
         member do
           get :data
         end
+        collection do
+          post :login
+        end
       end
     end
   end
