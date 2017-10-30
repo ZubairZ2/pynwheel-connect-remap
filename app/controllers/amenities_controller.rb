ceee4518715a71c70b280d13d4b73d05a3c5a950
Asim Sajjad <a.sajjad@intagleo.co.uk>
2017-10-30 17:46:31 +0500

    crud operations for amenity completed

--- app/controllers/amenities_controller.rb
+++ app/controllers/amenities_controller.rb
@@ -1,8 +1,32 @@
 class AmenitiesController < ApplicationController
-	before_action :set_community
+	before_action :set_community_and_sitemap
 
 	def new
-		@amenity = @community.sitemap.amenities.new
+		@amenity = @community.sitemap.amenities.build
+	end
+
+	def edit
+		@amenity = @community.sitemap.amenities.find(params[:id])
+	end
+
+	def create
+		@amenity = @community.sitemap.amenities.build(amenity_params)
+		if @amenity.save
+			redirect_to list_amenities_community_sitemaps_path(@community), notice: "Amenity created successfully"
+		else
+			flash[:error] = @amenity.errors.full_messages.join(',')
+      render :new
+		end
+	end
+
+	def update
+		@amenity = @community.sitemap.amenities.find(params[:id])
+		if @amenity.update_attributes(amenity_params)
+			redirect_to list_amenities_community_sitemaps_path(@community), notice: "Amenity updated successfully"
+		else
+			flash[:error] = @amenity.errors.full_messages.join(',')
+      render :edit
+		end
 	end
 
 	def destroy
@@ -16,7 +40,12 @@ class AmenitiesController < ApplicationController
 
 	private
 
-	def set_community
+	def set_community_and_sitemap
 		@community = Community.find params[:community_id]
+		@sitemap = @community.sitemap
 	end
+
+	def amenity_params
+    params.require(:amenity).permit(:name,:image)
+  end
 end
