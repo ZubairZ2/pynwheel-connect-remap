a510dab5c4b44726b4468ed310962cb815231a32
asim sajjad <a.sajjad@intagleo.co.uk>
2017-11-15 14:11:17 +0500

    work on webpages

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -1,5 +1,12 @@
 class WebpagesController < ApplicationController
 	def index
+		#Unit.joins("LEFT OUTER JOIN floorplans ON floorplans.provider_floorplan_id = units.floorplan_id")
 		@units = current_community.units.to_json
+		units = current_community.units
+		units.each do |u|
+			puts '--***************************' , u.floorplan_id
+		end
+		@floorplan_ids = units.collect{|u| u.floorplan_id}
+		puts '--------------------------' , @floorplan_ids
 	end
 end
