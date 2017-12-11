4cba1c8e20a0f686ed2be4ab08c51aca5b9cf5a5
Asim Sajjad <a.sajjad@intagleo.co.uk>
2017-12-11 21:03:16 +0500

    gallery breadcrumb pluralize

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -3,7 +3,7 @@ class GalleriesController < ApplicationController
 	add_breadcrumb "Home", :root_path
 
 	def index
-		add_breadcrumb "Gallery", community_galleries_path(@community)
+		add_breadcrumb "Galleries", community_galleries_path(@community)
 		@gallery_images = @community.gallery_images.order(:sort).all
 	end
 
