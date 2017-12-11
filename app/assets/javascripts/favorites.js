1cc6b7300735eea01f064d61da47a3739fee615d
asim sajjad <a.sajjad@intagleo.co.uk>
2017-12-11 13:27:39 +0500

    html fixes

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 	if ($('.is-favorites')[0]){
+		$('#clickme').click(function() {
+    var $slider = $('.mydiv');
+	//$('.right-side').css('margin-left', '0');
+    $slider.animate({
+      left: parseInt($slider.css('left'),10) == -331 ?
+       0 : -331
+    });
+  });
 		$(".datepicker").datepicker({dateFormat: 'mm/dd/yy' }); 
 		new Clipboard('.clipboard-btn');
 	}//if ending curl
