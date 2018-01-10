94c37a7501705d9703a523363af4254f44591eca
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-01-10 17:15:33 +0500

    fixed issues

--- app/models/floorplan.rb
+++ app/models/floorplan.rb
@@ -2,4 +2,5 @@ class Floorplan < ApplicationRecord
   mount_base64_uploader :image, AvatarUploader
   belongs_to :community
   has_many :amenities, as: :amenityable
+  validates_uniqueness_of :name, scope: :community
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -4,7 +4,7 @@ class Unit < ApplicationRecord
   belongs_to :floorplate
   #has_many :favorites
   #has_many :favorite_communities, :through => :favorites,source: :community
-  validates :effective_rent, :numericality => { :greater_than => 0 }
+  validates :effective_rent, :numericality => { :greater_than => 0 }, :length => { :maximum => 7}
   validates_uniqueness_of :marketing_name, scope: :community_id
   has_many :amenities, as: :amenityable
 
