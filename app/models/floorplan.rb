bd6022af80ec264c61bd0f6c8faaa1461892b6fd
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-02-14 18:10:37 +0500

    uniquessness validation only on create floorplan

--- app/models/floorplan.rb
+++ app/models/floorplan.rb
@@ -2,5 +2,5 @@ class Floorplan < ApplicationRecord
   mount_base64_uploader :image, AvatarUploader
   belongs_to :community
   has_many :amenities, as: :amenityable
-  validates_uniqueness_of :name, scope: :community
+  validates_uniqueness_of :name, scope: :community, on: :create
 end
