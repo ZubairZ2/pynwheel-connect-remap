35e0562d355b1302a0cc1383e101d5d25fc71a6a
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-03-26 19:17:27 +0500

    validations added to content pages

--- app/models/imagepage.rb
+++ app/models/imagepage.rb
@@ -1,4 +1,9 @@
 class Imagepage < ApplicationRecord
   belongs_to :community
   has_many :additional_images, dependent: :destroy
+  validates_uniqueness_of :name
+  validates_presence_of :name
+	validates_length_of :name, :maximum => 12
+
+  scope :active, -> { where(hide_page: false) }
 end
--- app/models/webpage.rb
+++ app/models/webpage.rb
@@ -1,3 +1,8 @@
 class Webpage < ApplicationRecord
   belongs_to :community
+  validates_uniqueness_of :name
+  validates_presence_of :url, :name
+  validates_length_of :name, :maximum => 12
+
+  scope :active, -> { where(hide_page: false) }
 end
