e04f62bc84192e4b9709ac5b6117c641313fb3c2
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-03-29 19:10:00 +0500

    dummy name and url in gallery response in minimum data

--- app/json_presenters/gallery_presenter.rb
+++ app/json_presenters/gallery_presenter.rb
@@ -13,20 +13,25 @@ class GalleryPresenter < JsonPresenters
         struct = {}
         unless action == "ios_data"
           if img.image.file.extension.downcase == 'mp4'
-            struct[:url] = img.image.url
+            #struct[:url] = img.image.url
+            struct[:url] = "https://images-pynwheel-cms-v2.s3.amazonaws.com/uploads/amenity/image/124/124-1518624577-video-placeholder.jpg"
             struct[:video] = true
             struct[:poster] = "https://images-pynwheel-cms-v2.s3.amazonaws.com/uploads/amenity/image/124/124-1518624577-video-placeholder.jpg"
           else
-            struct[:url] = img.image.url(:large)
+            #struct[:url] = img.image.url(:large)
+            struct[:url] = "https://images-pynwheel-cms-v2.s3.amazonaws.com/uploads/amenity/image/124/124-1518624577-video-placeholder.jpg"
             struct[:video] = false
           end
-          struct[:type] = img.gallery.name
+          #struct[:type] = img.gallery.name
+          struct[:type] = "Dummy Name"
           struct[:id] = img.id
         else
           unless img.is_video?
-            struct[:url] = img.image.url(:ios)
+            #struct[:url] = img.image.url(:ios)
+            struct[:url] = "https://images-pynwheel-cms-v2.s3.amazonaws.com/uploads/amenity/image/124/124-1518624577-video-placeholder.jpg"
             struct[:video] = false
-            struct[:type] = img.gallery.name
+            #struct[:type] = img.gallery.name
+            struct[:type] = "dummy Name"
             struct[:id]= img.id
           end
         end
