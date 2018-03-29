ed7ec959b904c5b08d8842bc148e280d744d3cd6
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-03-29 21:13:07 +0500

    change in gallery presenter

--- app/json_presenters/gallery_presenter.rb
+++ app/json_presenters/gallery_presenter.rb
@@ -9,40 +9,29 @@ class GalleryPresenter < JsonPresenters
       end
       hash[:categories] = categories
       images = []
-      for img in community.gallery_images do
-        puts '-----------------------------' , Time.now 
-        puts '**********************************' , img.gallery.name
-        puts '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', img.id
-        #puts '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', img.image_url.include?(".mp4") #this slows down the loop
+      for img in community.gallery_images do 
+        puts '****************************************' , Time.now
         #puts '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', img.image_url #this also slows down the loop
-        puts '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', img.large_image_url
-        puts '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', img.ios_image_url 
-        puts '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', img.ios_image_url.include?(".mp4") 
-        # struct = {}
-        # unless action == "ios_data"
-        #   if img.image_url.include?(".mp4")
-        #     struct[:url] = img.image_url
-        #     struct[:video] = true
-        #     struct[:poster] = "https://images-pynwheel-cms-v2.s3.amazonaws.com/uploads/amenity/image/124/124-1518624577-video-placeholder.jpg"
-        #   else
-        #     struct[:url] = img.large_image_url
-        #     struct[:video] = false
-        #   end
-        #   struct[:type] = img.gallery.name
-        #   struct[:id] = img.id
-        # else
-        #   unless img.image_url.include?(".mp4")
-        #     struct[:url] = img.ios_image_url
-        #     struct[:video] = false
-        #     struct[:type] = img.gallery.name
-        #     struct[:id]= img.id
-        #   end
-        # end
-        #images << struct
-        struct = {
-          id: img.id,
-          type: img.gallery.name
-        }
+        struct = {}
+        unless action == "ios_data"
+          if img.ios_image_url.include?(".mp4")
+            struct[:url] = img.large_image_url
+            struct[:video] = true
+            struct[:poster] = "https://images-pynwheel-cms-v2.s3.amazonaws.com/uploads/amenity/image/124/124-1518624577-video-placeholder.jpg"
+          else
+            struct[:url] = img.large_image_url
+            struct[:video] = false
+          end
+          struct[:type] = img.gallery.name
+          struct[:id] = img.id
+        else
+          unless img.ios_image_url.include?(".mp4")
+            struct[:url] = img.ios_image_url
+            struct[:video] = false
+            struct[:type] = img.gallery.name
+            struct[:id]= img.id
+          end
+        end
         images << struct
       end
       hash[:images] = images
