13160161d2bf085f380ede01f345ab0f0a6f8f68
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-03-30 18:02:14 +0500

    breadcrumb fix on data  page

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,10 +1,10 @@
 	class SettingsController < ApplicationController
 	add_breadcrumb "Home", :root_path
-	add_breadcrumb "Data", "##"
+	
     before_action :set_community
 	def index
 		authorize! :add_settings,current_user
-		add_breadcrumb "Add Credentials", community_settings_path(@community)
+		add_breadcrumb "Data", community_settings_path(@community)
 		@communities = current_company.communities
 	end
 	private
