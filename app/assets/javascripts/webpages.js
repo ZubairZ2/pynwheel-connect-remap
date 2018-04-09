1177b1d9059f84293fe013a78f02be9bf70dbbb4
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-04-09 13:24:06 +0500

    markers plotting issue on 1st load fixed

--- app/assets/javascripts/webpages.js
+++ app/assets/javascripts/webpages.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+jQuery(function($) {
   if ($('.is-webpage')[0]){
     $(".panzoom").addClass("transform-none");
     $(document).on("click touchstart", ".zoom-controls", function (){
@@ -281,7 +281,7 @@ $(document).ready(function(){
       $(this).css({"left": current_left,"top": current_top});
     });   
  } // if condition ending curl
-});
+})(jQuery);
 
 function setFilters(){
  var zero_bedroom = false;
