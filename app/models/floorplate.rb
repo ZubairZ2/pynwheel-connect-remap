1341eca798653962804668eefd5bf6032e0d90fe
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-04-26 16:31:38 +0500

    disable floorplate number validation

--- app/models/floorplate.rb
+++ app/models/floorplate.rb
@@ -4,7 +4,7 @@ class Floorplate < ApplicationRecord
 	has_many :units
 	has_many :amenities, as: :amenityable
 	validates_uniqueness_of :name, scope: :community_id
-	validates_uniqueness_of :number, scope: :community_id
+	# validates_uniqueness_of :number, scope: :community_id
 	validates :image, :presence => {message: "cannot be blank. Please upload Floor Plate image first."}
 	before_destroy :reset_units_plots
 
