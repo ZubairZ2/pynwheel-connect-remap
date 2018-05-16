f94c0e002e73050541cdfad31ced1b7b35ca7941
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-05-16 18:45:09 +0500

    devise issue tries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,13 +28,13 @@ class ApplicationController < ActionController::Base
   end
 
   # protect_from_forgery with: :exception
-  rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { head :forbidden, content_type: 'text/html' }
-      format.html { redirect_to main_app.root_url, notice: exception.message }
-      format.js   { head :forbidden, content_type: 'text/html' }
-    end
-  end
+  # rescue_from CanCan::AccessDenied do |exception|
+  #   respond_to do |format|
+  #     format.json { head :forbidden, content_type: 'text/html' }
+  #     format.html { redirect_to main_app.root_url, notice: exception.message }
+  #     format.js   { head :forbidden, content_type: 'text/html' }
+  #   end
+  # end
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
