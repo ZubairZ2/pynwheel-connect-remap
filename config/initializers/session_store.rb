093911a138d66f36b42b69571bd86651cec3fdd6
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-05-16 18:22:55 +0500

    devise issue tries

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session', domain: "pynwheel.herokuapp.com"
 
