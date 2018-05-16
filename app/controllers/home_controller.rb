55780f84321714c79fa7d9a9cedc4fc843ede5d8
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-05-16 17:47:15 +0500

    puts in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+  	puts '----------------------' , current_user.inspect
   	if current_user.is_super_admin?
     	@communities = Community.select(:id,:name,:updated_at,:company_id).includes(:company)
     else
