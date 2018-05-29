a9bee3cb8fdd4ca05ce3cf03bbfebbb6a2e617bc
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-05-29 16:53:14 +0500

    if available date is not present ignore units on favorites page

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -102,7 +102,7 @@ class WebpagesController < ActionController::Base
 
 	def favorites
 		@favorite = Favorite.find_by_session_id(cookies[:session_id])
-		@units = Unit.where(id: JSON.parse(cookies[:favorite_unit_ids]))
+		@units = Unit.where(id: JSON.parse(cookies[:favorite_unit_ids])).where.not(available_date: nil)
 		@floorplans = Floorplan.where(provider_floorplan_id: @units.map(&:floorplan_id),community_id: params[:community_id])
 	end
 
