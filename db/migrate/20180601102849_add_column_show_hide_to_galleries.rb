b8c81d2e9649409964a54cb4d332681ef27be866
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-06-04 13:24:23 +0500

    remved unwanted migration

--- db/migrate/20180601102849_add_column_show_hide_to_galleries.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnShowHideToGalleries < ActiveRecord::Migration[5.0]
-  def change
-    add_column :galleries, :show_gallery, :boolean, default: true
-    add_column :galleries, :gallery_name, :string, default: "Gallery"
-  end
-end
