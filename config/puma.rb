00a3c9cc8334395156d595b9006fca16db3f38b0
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-06-20 16:21:36 +0500

    configure session store and puma killer settings according to enviornment

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { Rails.env.production? ? "production" : "staging" }
 
 # SETTING WORKERS TO 1 BY INTAGLEO
 workers 2
@@ -56,7 +56,7 @@ before_fork do
   require 'puma_worker_killer'
 
   PumaWorkerKiller.config do |config|
-    config.ram           = 2500 # mb
+    config.ram           = Rails.env.production? ? 2500 : 500 # mb
     config.frequency     = 5    # seconds
     config.percent_usage = 0.98
     config.rolling_restart_frequency = 1800 # 30 min in seconds
