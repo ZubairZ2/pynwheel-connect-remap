dac7387808336a488dcb1af354f95a4d3f377dac
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-06-27 16:31:29 +0500

    push migration

--- db/migrate/20180626135406_add_floorplan_units_pop_up_fields_to_designs.rb
+++ db/migrate/20180626135406_add_floorplan_units_pop_up_fields_to_designs.rb
@@ -8,7 +8,7 @@ class AddFloorplanUnitsPopUpFieldsToDesigns < ActiveRecord::Migration[5.0]
     add_column :designs, :available_appartments_bg_color, :string
     add_column :designs, :floor_bg_color, :string
     add_column :designs, :unit_header_bg_color, :string
-    add_column :designs, :unit_header_font_color, :string
+    add_column :designs, :init_header_font_color, :string
     add_column :designs, :unit_details_font_color, :string
     add_column :designs, :unit_details_bg_color, :string
     add_column :designs, :floorplan_name_bg_color, :string
