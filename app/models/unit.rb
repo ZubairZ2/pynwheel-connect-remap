db6276266a0d623142569bedb4eb430b8f771c45
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-08-02 19:13:34 +0500

    Revert last commit

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -15,8 +15,7 @@ class Unit < ApplicationRecord
   scope :has_x_plot, -> { where("x_plot > ? and available_date > ? and available_date < ?", 0, Date.today, Date.today+2.year) }
   scope :has_y_plot, -> { where("y_plot > ? and available_date > ? and available_date < ?", 0, Date.today, Date.today+2.year) }
   scope :ploted_units, -> { has_x_plot.or(has_y_plot) }
-  #scope :available_units, -> { ploted_units.or(past_available_units).where.not(available: true) }
-  scope :available_units, -> { ploted_units.or(past_available_units) }
+  scope :available_units, -> { ploted_units.or(past_available_units).where.not(available: true) }
   after_commit :populate_image_urls, on: [:create,:update]
   
   def floorplan
