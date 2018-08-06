afaa13d5432f3835e37fd3ce419076821149932e
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-08-06 20:07:41 +0500

    display height in ascending order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -426,7 +426,7 @@ module ApplicationHelper
 
 
   def navigation_button_height_options
-    ["50px","75px","100px","110px","136px","120px","125px"]
+    ["50px","75px","100px","110px","120px","125px","136px"]
   end
 
   def navigation_button_width_options
