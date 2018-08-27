b92f4079ee304ae190ee6f696f1a24253561ff77
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-08-27 16:39:26 +0500

    add image distance time and rating fields in custom locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,14 @@
 class Location < ApplicationRecord
+  include StandardUrl
+  mount_uploader :image, AvatarUploader
   belongs_to :neighborhood
   validates_presence_of :latitude, :longitude, :title
   validates :latitude , numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }
 	validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+  after_commit :populate_image_urls, on: [:create,:update]
+  def populate_image_urls
+    if image.present?
+      set_standard_url('Location',id)
+    end
+  end
 end
