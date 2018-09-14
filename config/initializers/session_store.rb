f322f23e3c9c9f6a2cfe64f1a1f7fd4c0ef48fc4
Asim Sajjad <a.sajjad@intagleo.co.uk>
2018-09-14 14:22:13 +0500

    changes in session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-unless Rails.env.test? 
+unless Rails.env.development? 
   Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session', domain: Rails.env.production? ? "pynwheel.herokuapp.com" :  "pynwheel-staging.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session'
