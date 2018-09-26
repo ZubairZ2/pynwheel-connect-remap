575c9e05de5ee197517eba008520d791fd5b1eb2
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2018-09-26 17:57:25 +0500

    add community level users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,7 +50,7 @@ class ApplicationController < ActionController::Base
   end
     
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:invite, keys: [:role,:company_id])
+    devise_parameter_sanitizer.permit(:invite, keys: [:role,:community_ids])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :avatar])
   end
 end
