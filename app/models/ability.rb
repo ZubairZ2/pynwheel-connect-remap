786784e73b4f447f60a3e42400b7378254b1cd6d
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2018-09-26 20:06:19 +0500

    profile issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,7 @@ class Ability
         can :manage, Unit
         can :read, Company, id: user.company_id	
         can :update, Company, id: user.company_id	 
-        can :manage ,User, id: user.id
+        #can :manage ,User, id: user.id
         cannot :add_settings, User, id: user.id
         cannot :select_theme, User, id: user.id            
     end
