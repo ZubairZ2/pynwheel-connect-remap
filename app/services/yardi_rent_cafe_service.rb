05343d757bdb16eb2f38ec31e3c59e8e0a03dcaa
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2018-10-18 17:08:50 +0500

    (validate: false) issue yardi rent cafe

--- app/services/yardi_rent_cafe_service.rb
+++ app/services/yardi_rent_cafe_service.rb
@@ -45,7 +45,7 @@ class YardiRentCafeService < BaseService
                   if unit.effective_rent <= 0
                     unit.effective_rent = 1.0
                   end
-                  unit.save(validate: false)
+                  unit.save
                 end
               end
             rescue => e
