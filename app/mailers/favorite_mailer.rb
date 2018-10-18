db7403f252a600730829271b163fa69e3668270b
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2018-10-18 19:58:58 +0500

    revert change unit id addtions

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,11 +1,12 @@
 class FavoriteMailer < ApplicationMailer
 
-	def email_favorites(email_from,email_to,email_bcc,email_body,favorites,units,ios,community)
+	def email_favorites(email_from,email_to,email_bcc,email_body,favorites,ios,community)
     @favorites = favorites
     @email_body = email_body
     @ios = ios
     @community = community
-    @units = units
+
+    # @units = units
     mail(to: email_to, from: email_from, bcc: email_bcc, subject: 'My Favorites')
   end
 end
