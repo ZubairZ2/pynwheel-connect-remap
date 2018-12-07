2c2366149adde1a9ce3e35f0a64e01594859d0b4
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2018-12-07 19:22:01 +0500

    zaremba multiple provider unit id

--- app/services/zaremba_swap_service.rb
+++ app/services/zaremba_swap_service.rb
@@ -48,13 +48,40 @@ class ZarembaSwapService < BaseService
     units.each do |u|
       puts u
       vacateDate = ""
-      unit = Unit.where(community_id: credentials.community_id,marketing_name: u["MarketingName"]).first
+      unit = Unit.find_by(community_id: credentials.community_id,marketing_name: u["MarketingName"])
+      unless unit.present?
+        unit = Unit.find_by(community_id: credentials.community_id,marketing_name: u["BuildingID"]+"-"+u["MarketingName"])
+      end
+      # if u["MarketingName"] == "0620" && u["BuildingID"] == "002"
+      #   byebug
+      # end
+
       if unit.present?
+
+        flag = 0
+
+        u1 = Unit.where(community_id: credentials.community_id, provider_unit_id: u["IDValue"])
+        u1.each do |u2|
+          unless u2.building == u["BuildingID"]
+            flag = 1
+          end
+          # if flag == 1 && !(u2.marketing_name.split('-')[0] == u2.building)
+          #   u2.marketing_name = u2.building+"-"+u2.marketing_name
+          #   u2.save(validate: false)
+          # end
+        end
+
         unit.property_id = property_id
         unit.provider = "zaremba_new"
         unit.provider_unit_id = u["IDValue"]
         unit.unit_type = u["UnitType"]
-        unit.marketing_name = u["MarketingName"]
+
+        if flag == 1 #&& unit.marketing_name.split('-')[0] == unit.building
+          unit.marketing_name = u["BuildingID"]+"-"+u["MarketingName"]
+        else
+          unit.marketing_name = u["MarketingName"]
+        end
+
         unit.floorplan_id = u["Units"]["Unit"]["Identification"][1]["IDValue"]
         unit.effective_rent = 1.0 #Setting rent to avoid validation issues
         if u["MarketRent"].present?
@@ -74,19 +101,39 @@ class ZarembaSwapService < BaseService
         building = u["BuildingID"]
         unit.building = building.present? ? building.gsub("Building ", "") : ""
         unit.manually_updated = false
-        unit.save
+        unit.save(validate: false)
       else
-        dup = Unit.find_by(community_id: credentials.community_id,provider_unit_id: u["IDValue"])
+        dup = Unit.find_by(community_id: credentials.community_id,provider_unit_id: u["IDValue"],building: u["BuildingID"])
         if dup.present?
           dup.destroy
         end
         unit = Unit.new
+
+        flag = 0
+
+        u1 = Unit.where(community_id: credentials.community_id, provider_unit_id: u["IDValue"])
+        u1.each do |u2|
+          unless u2.building == u["BuildingID"]
+            flag = 1
+          end
+          # if flag == 1 && !(u2.marketing_name.split('-')[0] == u2.building)
+          #   u2.marketing_name = u2.building+"-"+u2.marketing_name
+          #   u2.save(validate: false)
+          # end
+        end
+
         unit.community_id = credentials.community_id
         unit.property_id = property_id
         unit.provider = "zaremba_new"
         unit.provider_unit_id = u["IDValue"]
         unit.unit_type = u["UnitType"]
-        unit.marketing_name = u["MarketingName"]
+
+        if flag == 1 #&& unit.marketing_name.split('-')[0] == unit.building
+          unit.marketing_name = u["BuildingID"]+"-"+u["MarketingName"]
+        else
+          unit.marketing_name = u["MarketingName"]
+        end
+
         unit.floorplan_id = u["Units"]["Unit"]["Identification"][1]["IDValue"]
         unit.effective_rent = 1.0 #Setting rent to avoid validation issues
         if u["MarketRent"].present?
@@ -106,7 +153,7 @@ class ZarembaSwapService < BaseService
         building = u["BuildingID"]
         unit.building = building.present? ? building.gsub("Building ", "") : ""
         unit.manually_updated = false
-        unit.save
+        unit.save(validate: false)
       end
     end
     unit = Unit.where(community_id: credentials.community_id)
