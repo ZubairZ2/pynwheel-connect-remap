bbd4ddfe0d295a4cd87fe8d11ad1d2c4338b4d19
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2018-12-11 16:24:46 +0500

    gables additional fields

--- app/assets/javascripts/design.js
+++ app/assets/javascripts/design.js
@@ -413,6 +413,20 @@ $(document).ready(function () {
               }
           }
       }
+      var gable_filter_panel_bg_image_upload_holder = document.getElementById('gable-filter-panel-bg-image-upload-holder');
+      if (gable_filter_panel_bg_image_upload_holder) {
+
+          gable_filter_panel_bg_image_upload_holder.ondrop = function (e) {
+              e.preventDefault();
+              files = e.dataTransfer.files;
+              if (files[0].type == "image/png" || files[0].type == "image/jpeg" || files[0].type == "image/jpg") {
+                  gableFilterPanelBackgroundImageSrc(files[0]);
+              } else {
+                  console.log('file type is not allowed');
+                  $('#image-upload-warning').modal('show');
+              }
+          }
+      }
 
     var global_nav_background_image_upload_holder = document.getElementById('global-nav-background-image-upload-holder');
     if (global_nav_background_image_upload_holder) {
@@ -626,6 +640,14 @@ $(document).ready(function () {
   $("#home_page_nav_bg_image").change(function () {
       readGlobalHomePageNavBackgroundImageFromInput(this);
   });
+
+    $("#global_nav_bg_image").change(function () {
+        readGableGlobalNavBackgroundImageFromInput(this);
+    });
+
+    $("#filter_panel_bg_image").change(function () {
+        readGableFilterPanelBackgroundImageFromInput(this);
+    });
   $("#global_nav_background_image").change(function () {
     readGlobalNavBackgroundImageFromInput(this);
   });
@@ -1057,7 +1079,17 @@ function gableGlobalNavBackgroundImageSrc(file) {
     reader.onload = function (e) {
         $('#gable-global-nav-bg-image-preview').attr('src', e.target.result);
         $('#gable-global-nav-bg-image-preview').parent().attr('href', e.target.result);
-        globalNavBackgroundImage(e.target.result);
+        gableGlobalNavBackgroundImage(e.target.result);
+    }
+    reader.readAsDataURL(file);
+}
+function gableFilterPanelBackgroundImageSrc(file) {
+    $(".divLoading").removeClass("hidden");
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $('#gable-filter-panel-bg-image-preview').attr('src', e.target.result);
+        $('#gable-filter-panel-bg-image-preview').parent().attr('href', e.target.result);
+        gableFilterPanelBackgroundImage(e.target.result);
     }
     reader.readAsDataURL(file);
 }
@@ -1520,21 +1552,6 @@ function globalHomePageNavBackgroundImage(src) {
         console.log("success");
     });
 }
-function globalNavBackgroundImage(src) {
-    $(".divLoading").removeClass("hidden");
-    var url = "/communities/" + community_id;
-    $.ajax({
-        url: url,
-        type: "PUT",
-        dataType: "script",
-        data: {
-            community: {design_attributes: {id: design_id, gable_attributes: {id: gable_id, global_nav_bg_image: src}}}
-        }
-    }).done(function () {
-        $(".divLoading").addClass("hidden");
-        console.log("success");
-    });
-}
 function gableGlobalNavBackgroundImage(src) {
     $(".divLoading").removeClass("hidden");
     var url = "/communities/" + community_id;
@@ -1550,6 +1567,22 @@ function gableGlobalNavBackgroundImage(src) {
         console.log("success");
     });
 }
+function gableFilterPanelBackgroundImage(src) {
+    $(".divLoading").removeClass("hidden");
+    var url = "/communities/" + community_id;
+    $.ajax({
+        url: url,
+        type: "PUT",
+        dataType: "script",
+        data: {
+            community: {design_attributes: {id: design_id, gable_attributes: {id: gable_id, filter_panel_bg_image: src}}}
+        }
+    }).done(function () {
+        $(".divLoading").addClass("hidden");
+        console.log("success");
+    });
+}
+
 function globalNavBackgroundImage(src) {
   $(".divLoading").removeClass("hidden");
   var url = "/communities/" + community_id;
@@ -2205,7 +2238,7 @@ function readGlobalHomePageNavBackgroundImageFromInput(input) {
     }
 }
 
-function readGlobalHomePageNavBackgroundImageFromInput(input) {
+function readGableGlobalNavBackgroundImageFromInput(input) {
     if (input.files && input.files[0]) {
         if (input.files[0].type == "image/png" || input.files[0].type == "image/jpeg" || input.files[0].type == "image/jpg") {
             var reader = new FileReader();
@@ -2214,6 +2247,26 @@ function readGlobalHomePageNavBackgroundImageFromInput(input) {
                 $('#gable-global-nav-bg-image-preview').attr('src', e.target.result);
                 $('#gable-global-nav-bg-image-preview').parent().attr('href', e.target.result);
                 gableGlobalNavBackgroundImage(e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        } else {
+            $(input).val('');
+            $('#image-upload-warning').modal('show');
+            //console.log($(input).val());
+        }
+    }
+}
+
+function readGableFilterPanelBackgroundImageFromInput(input) {
+    if (input.files && input.files[0]) {
+        if (input.files[0].type == "image/png" || input.files[0].type == "image/jpeg" || input.files[0].type == "image/jpg") {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#gable-filter-panel-bg-image-preview').attr('src', e.target.result);
+                $('#gable-filter-panel-bg-image-preview').parent().attr('href', e.target.result);
+                gableFilterPanelBackgroundImage(e.target.result);
             }
 
             reader.readAsDataURL(input.files[0]);
