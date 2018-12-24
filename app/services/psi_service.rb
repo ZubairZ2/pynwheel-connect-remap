eb2ae8e0967af52181f6f74141f0981e4e1a45f3
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2018-12-24 15:10:47 +0500

    entrata issue fixed

--- app/services/psi_service.rb
+++ app/services/psi_service.rb
@@ -68,8 +68,8 @@ class PsiService < BaseService
           end
           if u["EffectiveRent"].present?
             unit.effective_rent = u["EffectiveRent"]
-          elsif u["UnitRent"].present?
-            unit.effective_rent = u["UnitRent"]
+          elsif u["Units"]["Unit"]["UnitRent"].present?
+            unit.effective_rent = u["Units"]["Unit"]["UnitRent"]
           else
             unit.effective_rent = 0
           end
@@ -179,7 +179,7 @@ class PsiService < BaseService
           psi_units.each do |u|
             unit = Unit.find_by(provider_unit_id: u[1]["@attributes"]["PropertyUnitId"],community_id: credentials.community_id)
             pricing = u[1]["Rent"]["@attributes"]["MinRent"].gsub(/[\s,]/ ,"")
-            if unit.effective_rent <= 1 and pricing.to_f > 0
+            if pricing.to_f > 0
               unit.effective_rent = pricing.to_f
               unit.save(validate: false)
               # u[1]['Rent']['TermRent'].each do |a|
--- app/services/psi_swap_service.rb
+++ app/services/psi_swap_service.rb
@@ -76,8 +76,8 @@ class PsiSwapService < BaseService
         end
         if u["EffectiveRent"].present?
           unit.effective_rent = u["EffectiveRent"]
-        elsif u["UnitRent"].present?
-          unit.effective_rent = u["UnitRent"]
+        elsif u["Units"]["Unit"]["UnitRent"].present?
+          unit.effective_rent = u["Units"]["Unit"]["UnitRent"]
         else
           unit.effective_rent = 0
         end
@@ -115,8 +115,8 @@ class PsiSwapService < BaseService
 
         if u["EffectiveRent"].present?
           unit.effective_rent = u["EffectiveRent"]
-        elsif u["UnitRent"].present?
-          unit.effective_rent = u["UnitRent"]
+        elsif u["Units"]["Unit"]["UnitRent"].present?
+          unit.effective_rent = u["Units"]["Unit"]["UnitRent"]
         else
           unit.effective_rent = 0
         end
@@ -306,7 +306,7 @@ class PsiSwapService < BaseService
           psi_units.each do |u|
             unit = Unit.find_by(provider_unit_id: u[1]["@attributes"]["PropertyUnitId"],community_id: credentials.community_id)
             pricing = u[1]["Rent"]["@attributes"]["MinRent"].gsub(/[\s,]/ ,"")
-            if unit.effective_rent <= 1 and pricing.to_f > 0
+            if pricing.to_f > 0
               unit.effective_rent = pricing.to_f
               unit.save(validate: false)
               # u[1]['Rent']['TermRent'].each do |a|
