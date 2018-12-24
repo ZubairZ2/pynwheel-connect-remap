7c2bd050a017b2fff9d2fafeeab46edd5d80e4f7
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2018-12-24 11:21:18 +0500

    code field temp push 1

---
+++ db/migrate/20181221105637_add_global_navigation_border_thickness_field_to_expressionists.rb
@@ -0,0 +1,15 @@
+class AddGlobalNavigationBorderThicknessFieldToExpressionists < ActiveRecord::Migration[5.0]
+  def change
+    add_column :expressionists, :global_navigation_border_thickness, :string
+    add_column :expressionists, :home_page_logo_visible, :boolean, default: false
+    add_column :expressionists, :gables_home_page_images, :boolean, default: false
+    add_column :expressionists, :global_navigation_text_outside_the_button_border, :boolean, default: false
+    add_column :expressionists, :use_gables_buttons, :boolean, default: false
+    add_column :expressionists, :home_page_icons_position, :string
+    add_column :expressionists, :global_navigation_icons_position, :string
+    add_column :expressionists, :filter_buttons_icons_position, :string
+    add_column :expressionists, :global_navigation_show_background_color, :boolean, default: false
+    add_column :expressionists, :filter_panel_buttons_show_backround_color, :boolean, default: false
+
+  end
+end
