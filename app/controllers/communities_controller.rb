778a8ffb6eeec67b513bd7543997309b9444c118
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-01-29 18:13:21 +0500

    map color for mordenist

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -343,7 +343,7 @@ class CommunitiesController < ApplicationController
       :data_provider,:theme_name,:code,:is_sitemap,:locked,:website,:equal_housing_opportunity_logo,:handicap_accessible_logo,
       :credential_attributes=>[:id,:url,:username,:password,:property_id,:pmc_id,:server_name,:database,:platform,:interface_entity,:site_id,:c_code,
         :api_token,:p_code,:apply_now,:file,:resman_apikey, :resman_partner_id, :resman_account_id, :resman_property_id,:zaremba_filename,:zaremba_property_id,:zaremba_username, :zaremba_password],:design_attributes=>[:id,:logo_position,:secondary_logo_position,:global_navigation_position,
-        :property_map_size,:property_map_color,:amenity_map_marker_size,:amenity_map_marker_color,:secondary_page_background_image,:loop_type,:primary_color,:secondary_color,:primary_font_family,:primary_font_size,:primary_font_weight,
+        :property_map_size,:property_map_color,:modernist_map_marker_color,:amenity_map_marker_size,:amenity_map_marker_color,:secondary_page_background_image,:loop_type,:primary_color,:secondary_color,:primary_font_family,:primary_font_size,:primary_font_weight,
         :primary_text_align,:primary_font_color,:secondary_font_family,:secondary_font_size,:secondary_font_weight,:secondary_text_align,
         :secondary_font_color,:global_navigation_font_color,:global_navigation_background_color,:global_navigation_button_color,
         :global_navigation_buttons_opacity,:global_nav_bg_opacity,:button_shape,:global_nav_buttons_height,:global_nav_buttons_width,
