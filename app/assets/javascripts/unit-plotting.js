86602d3f2a88d37573a824ab25848586082fd729
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-02-22 11:18:43 +0500

    amenity marker deag issue

--- app/assets/javascripts/unit-plotting.js
+++ app/assets/javascripts/unit-plotting.js
@@ -197,7 +197,7 @@
                 arr[j][2] = Math.round(ui.position.top);
               }
             }
-            savePlot(temp[i], Math.round(ui.position.left ) + left_margin, Math.round(ui.position.top ) + right_margin);
+            savePlot(temp[i], Math.round(ui.position.left ) + lmargin, Math.round(ui.position.top ) + rmargin);
           }
         }
       }
