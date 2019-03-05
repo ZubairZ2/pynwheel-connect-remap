f6f7ff03be4efb834b0da81577953804749e1e88
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-03-05 20:00:51 +0500

    expression consis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -788,6 +788,9 @@ module ApplicationHelper
   def home_page_icons_position_option
     ["Left of text","Right of text","Above of text"]
   end
+  def global_nav_icons_position_option
+    ["Left of text","Right of text","Above the text"]
+  end
   def global_nav_button_icon_size_option
     ["65px","55px","45px","35px","25px"]
   end
