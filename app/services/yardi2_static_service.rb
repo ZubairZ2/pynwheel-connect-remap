1e078132b67ec5eb3bdd47b5bf812b191ab02d6a
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-03-14 16:26:17 +0500

    check

--- app/services/yardi2_static_service.rb
+++ app/services/yardi2_static_service.rb
@@ -29,20 +29,20 @@ class Yardi2StaticService < BaseService
           property_response = result[:"soap:Envelope"][1][:"soap:Body"][:UnitAvailability_LoginResponse][1][:UnitAvailability_LoginResult][:PhysicalProperty][1][:Property]
           property_response.each do |pr|
             if pr[0].to_s == "PropertyID"
-              puts "PropertyID" *30
               external_property_id  = pr[1][:"MITS:Identification"][1][:"MITS:PrimaryID"]
             end
             if pr[0].to_s == "Floorplan"
-              puts "Floorplan" *30
               floorplans << pr[1]
             end
             if pr[0].to_s == "ILS_Unit"
-              puts "ILS_Unit" *30
-
               ils_units << pr[1]
             end
-            puts "======================================================>", pr[0]
           end
+          unitt = []
+          result[:"soap:Envelope"][1][:"soap:Body"][:UnitAvailability_LoginResponse][1][:UnitAvailability_LoginResult][:PhysicalProperty][1][:Property][:ILS_Unit].each do |u|
+            unitt << u
+          end
+          puts "444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444", unitt
           save_yardi2_units(ils_units,external_property_id)
           save_yardi2_floorplans(floorplans)
           #else
