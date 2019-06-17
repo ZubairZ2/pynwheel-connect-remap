91949bd0b55b7dd667f26a2d657e1bb30f01cda1
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-06-17 17:09:23 +0500

    entrata avaialblity issue M.O

--- app/services/psi_service.rb
+++ app/services/psi_service.rb
@@ -235,12 +235,14 @@ class PsiService < BaseService
                   unless unit.present? # for unit with have extra 'A' in unit number getavailabilityandpricing
                     unit = Unit.find_by(provider_unit_id: u["@attributes"]["Id"].to_s+"-"+u["@attributes"]["UnitNumber"].to_s[0..(u["@attributes"]["UnitNumber"].length - 2)]+"-"+us[1]["@attributes"]["UnitNumber"].to_s,community_id: credentials.community_id)
                   end
-                  if us[1]["@attributes"]["Availability"].present? && us[1]["@attributes"]["Availability"] == "Available"
-                    unit.availability = 'Unoccupied'
-                    unit.available = true
-                  else
-                    unit.availability = 'Occupied'
-                    unit.available = false
+                  unless unit.availability_is_updated.present? && unit.availability_is_updated && unit.manual_override
+                    if us[1]["@attributes"]["Availability"].present? && us[1]["@attributes"]["Availability"] == "Available"
+                      unit.availability = 'Unoccupied'
+                      unit.available = true
+                    else
+                      unit.availability = 'Occupied'
+                      unit.available = false
+                    end
                   end
 
                   if us[1]["@attributes"]["AvailableOn"].present?
@@ -249,7 +251,9 @@ class PsiService < BaseService
                     day = dateSplit[0]
                     month = dateSplit[1]
                     year = dateSplit[2]
-                    unit.available_date = Date.parse("#{month}-#{day}-#{year}")
+                    unless unit.available_date_is_updated.present? && unit.available_date_is_updated && unit.manual_override
+                      unit.available_date = Date.parse("#{month}-#{day}-#{year}")
+                    end
                   end
                   unless unit.effective_rent_is_updated.present? && unit.effective_rent_is_updated && unit.manual_override
                     if (us[1]["Rent"]["@attributes"]["MinRent"].gsub(/[\s,]/ ,"")).present? && (us[1]["Rent"]["@attributes"]["MinRent"].gsub(/[\s,]/ ,"")).to_i > 0
@@ -336,12 +340,14 @@ class PsiService < BaseService
                       unless unit.present? # for unit with have extra 'A' in unit number getavailabilityandpricing
                         unit = Unit.find_by(provider_unit_id: u["@attributes"]["Id"].to_s+"-"+u["@attributes"]["UnitNumber"].to_s[0..(u["@attributes"]["UnitNumber"].length - 2)]+"-"+us[1]["@attributes"]["UnitNumber"].to_s,community_id: credentials.community_id)
                       end
-                      if us[1]["@attributes"]["Availability"].present? && us[1]["@attributes"]["Availability"] == "Available"
-                        unit.availability = 'Unoccupied'
-                        unit.available = true
-                      else
-                        unit.availability = 'Occupied'
-                        unit.available = false
+                      unless unit.availability_is_updated.present? && unit.availability_is_updated && unit.manual_override
+                        if us[1]["@attributes"]["Availability"].present? && us[1]["@attributes"]["Availability"] == "Available"
+                          unit.availability = 'Unoccupied'
+                          unit.available = true
+                        else
+                          unit.availability = 'Occupied'
+                          unit.available = false
+                        end
                       end
 
                       if us[1]["@attributes"]["AvailableOn"].present?
@@ -350,7 +356,9 @@ class PsiService < BaseService
                         day = dateSplit[0]
                         month = dateSplit[1]
                         year = dateSplit[2]
-                        unit.available_date = Date.parse("#{month}-#{day}-#{year}")
+                        unless unit.available_date_is_updated.present? && unit.available_date_is_updated && unit.manual_override
+                          unit.available_date = Date.parse("#{month}-#{day}-#{year}")
+                        end
                       end
                       unless unit.effective_rent_is_updated.present? && unit.effective_rent_is_updated && unit.manual_override
                         if (us[1]["Rent"]["@attributes"]["MinRent"].gsub(/[\s,]/ ,"")).present? && (us[1]["Rent"]["@attributes"]["MinRent"].gsub(/[\s,]/ ,"")).to_i > 0
