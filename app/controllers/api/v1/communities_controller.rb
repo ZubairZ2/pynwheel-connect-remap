bc5dd2cb10acfdfe032edc8a008e6d9f142a758f
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-07-08 15:51:38 +0500

    update version

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -60,9 +60,14 @@ class Api::V1::CommunitiesController < ActionController::Base
   def update_version
     app_version = AppVersion.first
     # if app_version.version != params[:version]
-    app_version.update_attribute(:version,params[:version])
+    res = app_version.update_attribute(:version,params[:version])
     # end
-    render :json=> {:success=>true, :message => "success", :operation => "update version"}
+    if res
+      render :json=> {:success=>true, :message => "success", :operation => "update version"}
+    else
+      render :json=> {:success=>false, :message => "success", :operation => "not update version"}
+    end
+    # render :json=> {:success=>true, :message => "success", :operation => "update version"}
   end
 
   def list_communities
