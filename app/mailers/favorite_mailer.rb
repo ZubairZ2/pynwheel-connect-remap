01dd812b4229031da0c94f7e8a0cdfbbe0a6dd49
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-07-17 18:04:07 +0500

    updates to ebrochure

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -8,4 +8,12 @@ class FavoriteMailer < ApplicationMailer
     @units = units
     mail(to: email_to, from: email_from, bcc: email_bcc, subject: 'My Favorites')
   end
+  def email_favorites_text(email_from,email_to,email_bcc,email_body,favorites,units,ios,community)
+      @favorites = favorites
+      @email_body = email_body
+      @ios = ios
+      @community = community
+      @units = units
+      mail(to: email_to, from: email_from, bcc: email_bcc, subject: 'My Favorites text version')
+  end
 end
