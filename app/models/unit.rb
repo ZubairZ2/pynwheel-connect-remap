285dd626b445d382ded5792885a3e318d1957cc5
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-07-31 15:30:51 +0500

    unit page name

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -77,10 +77,11 @@ class Unit < ApplicationRecord
   end
 
   def unit_market
-    unless self.marketing_name.present?
-      return ""
-    end
-    return unit_check_same_marketname(self)
+    return self.building.to_s + "-" + self.marketing_name
+    # unless self.marketing_name.present?
+    #   return ""
+    # end
+    # return unit_check_same_marketname(self)
     # return unit_check_like_marketname(self)
     #
     # return self.marketing_name
