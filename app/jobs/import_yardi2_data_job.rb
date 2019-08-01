8bcf0ce7f2e60d319cc8dda0a101708ec64f4441
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-08-01 17:55:41 +0500

    byebug

--- app/jobs/import_yardi2_data_job.rb
+++ app/jobs/import_yardi2_data_job.rb
@@ -5,7 +5,7 @@ class ImportYardi2DataJob < ApplicationJob
 
     # yardi2_static_service = Yardi2StaticService.new(JSON.parse(credentials))
     # yardi2_static_service.perform
-
+    byebug
     yardi2_service = Yardi2Service.new(JSON.parse(credentials))
     yardi2_service.perform
   end
