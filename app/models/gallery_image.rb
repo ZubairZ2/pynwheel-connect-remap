96099423b24b3f5aaf4755dff048e7fe28794eab
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-09-05 13:48:25 +0500

    video gallery issues

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -25,7 +25,8 @@ class GalleryImage < ApplicationRecord
   set_sortable :sort  
 	#mount_base64_uploader :image, GalleryUploader
 	mount_uploader :image, GalleryUploader
-	process_in_background :image
+	mount_uploader :video, VideoUploader
+	process_in_background :video
 	# before_create :set_image_name
 	# before_save :populate_image_urls
 	after_update :crop_image
@@ -36,7 +37,11 @@ class GalleryImage < ApplicationRecord
   end
 
   def is_video?
-		image.file.extension.downcase == 'mp4' 
+		begin
+			image.file.extension.downcase == 'mp4'
+		rescue => ex
+			true
+		end
 	end
 
 	def set_image_name
