7429261faf4cdcf772d26e92f7c0707cb801a81f
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-09-23 16:32:09 +0500

    error login

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -18,9 +18,11 @@ class Api::V1::CommunitiesController < ActionController::Base
         community_group = CommunityGroup.where(code: str)
       end
       if community_group.present?
+
         community_group = community_group.first
+
         if community_group.inactivate == false
-          render :json=> {:success=>true, :community => community_group.id,:name => community_group.name,:community_name => (Company.find community_group.id).name,:type => "community_group",:link => "/api/v1/communities/#{community_group.id}/data_group.json", :message => "success", :operation => "login"}
+          render :json=> {:success=>true, :community => community_group.id,:name => community_group.name,:community_name => (Company.find community_group.company_id).name,:type => "community_group",:link => "/api/v1/communities/#{community_group.id}/data_group.json", :message => "success", :operation => "login"}
         else
           render :json=> {:success=>false, :message => "Your application is inactive. Please contact support@pynwheel.com for help. Thank you!", :operation => "login"}
         end
