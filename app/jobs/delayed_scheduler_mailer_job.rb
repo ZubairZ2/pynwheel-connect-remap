4bca9ea5583cbc64757fc871884a2bd125330e02
Nasir Shamshad <nasirshamshad@pc140.ninealpha.com>
2019-09-24 21:01:37 +0500

    deliver now

--- app/jobs/delayed_scheduler_mailer_job.rb
+++ app/jobs/delayed_scheduler_mailer_job.rb
@@ -2,6 +2,6 @@ class DelayedSchedulerMailerJob < ApplicationJob
   include SuckerPunch::Job
 
   def perform(subject, msg, to)
-    NotificationMailer.tour_history_mail(subject, msg, to).deliver
+    NotificationMailer.tour_history_mail(subject, msg, to).deliver_now
   end
 end
