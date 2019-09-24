9ddfbd8ce89fe24c5502305d94d6a94fcd6f931f
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-09-24 15:11:57 +0500

    check 6

--- app/jobs/import_psi_data_job.rb
+++ app/jobs/import_psi_data_job.rb
@@ -6,7 +6,8 @@ class ImportPsiDataJob < ApplicationJob
 
     # psi_static_service = PsiStaticService.new(JSON.parse(credentials))
     # psi_static_service.perform
-
+    current_community.entrata_exception_logs = current_community.entrata_exception_logs + "main 1"
+    current_community.save
     psi_service = PsiService.new(JSON.parse(credentials))
     psi_service.perform
   end
