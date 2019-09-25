b180d6a1ab12a457a02a86106e8594fc87cedfb1
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-09-25 14:48:42 +0500

    temp push 4

--- app/services/real_page_svc_service.rb
+++ app/services/real_page_svc_service.rb
@@ -330,11 +330,15 @@ class RealPageSvcService < BaseService
               # unitHash = Hash.new
               rentStr = ""
               unitLeaseTerm = []
+              min_rent = nil
+              max_rent = nil
               unit_no = u[:Address][:UnitID]
               if hash[:units].include?(unit_no)
                 if u[:RentMatrix].present?
                   best_price = nil
                   begin
+                    min_rent = u[:RentMatrix][1][:Rows][:Row][0][:MinRent]
+                    max_rent = u[:RentMatrix][1][:Rows][:Row][0][:MaxRent]
 
                     u[:RentMatrix][1][:Rows][:Row].each_with_index do |opts,index|
                       next if index == 0
@@ -368,6 +372,8 @@ class RealPageSvcService < BaseService
                     unless unit.effective_rent_is_updated.present? && unit.effective_rent_is_updated && unit.manual_override
                       unit.effective_rent = best_price
                     end
+                    unit.min_effective_rent = min_rent
+                    unit.max_effective_rent = max_rent
 
                     unit.lease_pricing = rentStr
                     unit.save(:validate => false)
