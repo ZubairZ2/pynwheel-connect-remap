46c374ab05c0e092ce8a8fd89b51f98ce3ea4f60
Nasir Shamshad <nasirshamshad@pc140.ninealpha.com>
2019-10-08 20:46:18 +0500

    Added jen email as testing

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -34,9 +34,9 @@ class Api::V1::ToursController < ActionController::Base
         vs.image = tempFile
         if vs.id_card.present? && vs.image.present?
           vs.id_selfie_mismatch = false
-          puts "<<<<<<<<<<<<<<<<<<<<<< #{vs}"
           email_content = "Please verify user on the following link <br/> <a href='#{manual_selfie_match_url vs.id }' target='_blank'> Visitor's ID page </a>"
           DelayedSchedulerMailerJob.perform_async("ID / Selfie Matching (Manual)", email_content, 'arslan.mirza@intagleo.com')
+          DelayedSchedulerMailerJob.perform_async("ID / Selfie Matching (Manual)", email_content, 'jennifer@pynwheel.com')
         end
         puts "<<<<<<<<<<<<<<<<<<<<<<<<< #{vs.valid?}"
         vs.save!(validate: false)
