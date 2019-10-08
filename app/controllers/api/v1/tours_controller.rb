2cb802592dd6110fc117c36744c7e178ca435456
Nasir Shamshad <nasirshamshad@pc140.ninealpha.com>
2019-10-08 17:06:46 +0500

    False fix

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -34,11 +34,12 @@ class Api::V1::ToursController < ActionController::Base
         vs.image = tempFile
         if vs.id_card.present? && vs.image.present?
           vs.id_selfie_mismatch = false
-          
+          puts "<<<<<<<<<<<<<<<<<<<<<< #{vs}"
           email_content = "Please verify user on the following link <br/> <a href='#{manual_selfie_match_url self.id }' target='_blank'> Visitor's ID page </a>"
           DelayedSchedulerMailerJob.perform_async("ID / Selfie Matching (Manual)", email_content, 'arslan.mirza@intagleo.com')
         end
-        vs.save
+        puts "<<<<<<<<<<<<<<<<<<<<<<<<< #{vs.valid?}"
+        vs.save!(validate: false)
       rescue => ex
         puts "<<<<<<<<<<<<<<<<<<<<<<<<< #{ex.message}"
         render :json=> {:success=>false, :message => "failed"} and return
