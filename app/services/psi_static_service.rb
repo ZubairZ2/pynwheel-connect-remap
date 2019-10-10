efc5ea84c8fb13fbdce0abda87e31071249cc456
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-10-10 16:00:52 +0500

    sleep xada

--- app/services/psi_static_service.rb
+++ app/services/psi_static_service.rb
@@ -252,6 +252,7 @@ class PsiStaticService < BaseService
                                    :headers => { 'Content-Type' => 'application/json' } )
           response =  JSON.parse(response.body)
         else
+          sleep 5
           response = HTTParty.post(url,
                                    :body => {
                                        "auth": {
@@ -275,7 +276,7 @@ class PsiStaticService < BaseService
           puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"* 300,move_in_date
           response =  JSON.parse(response.body)
         end
-        sleep 2
+        sleep 4
 
         if response["response"]["code"] == 200
           unless response["response"]["result"].include?('No records found')
