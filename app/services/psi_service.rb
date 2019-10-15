703b05b4a6ffa43d54f4dca6045a6bb05de54ca9
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-10-15 11:21:27 +0500

    sleep time changed

--- app/services/psi_service.rb
+++ app/services/psi_service.rb
@@ -324,10 +324,10 @@ class PsiService < BaseService
                                          }
                                      }.to_json,
                                      :headers => { 'Content-Type' => 'application/json' } )
-            sleep 4
+            sleep 1
             response =  JSON.parse(response.body)
+            sleep 2
           end
-          sleep 3
 
           if response["response"]["code"] == 200
             unless response["response"]["result"].include?('No records found')
@@ -440,7 +440,7 @@ class PsiService < BaseService
                                          }.to_json,
                                          :headers => { 'Content-Type' => 'application/json' } )
                 response =  JSON.parse(response.body)
-                sleep 3
+                sleep 2
                 if response["response"]["code"] == 200
                   psi_units = response["response"]["result"]["PropertyUnits"]["PropertyUnit"]
                   psi_floorplan = response["response"]["result"]["Properties"]["Property"][0]["Floorplans"]["Floorplan"]
