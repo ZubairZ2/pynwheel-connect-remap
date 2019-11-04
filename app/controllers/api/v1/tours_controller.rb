886c725ca040306b410abdbadca4848ae2cdf42c
Nasir Shamshad <nasirshamshad@pc140.ninealpha.com>
2019-11-04 20:56:47 +0500

    fix for image path

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -177,11 +177,11 @@ class Api::V1::ToursController < ActionController::Base
         if u.community.is_sitemap?
           sitemap_image = u.community.sitemap.image.url(:svg_for_metro).present? ? u.community.sitemap.image.url(:svg_for_metro) : u.community.sitemap.image.url
           # u[:image] = ""
-          # u[:image] = sitemap_image
+          u[:image] = sitemap_image
         else
           floorplate = Floorplate.find_by_id(u.floorplate_id)
           floorplate_image = floorplate.image.url if floorplate.present?
-          # u[:image] = floorplate_image
+          u[:image] = floorplate_image
         end
       end
       success = true
