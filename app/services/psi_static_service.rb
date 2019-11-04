3a2763a495837cc033f96e2b196a3c4cf4599d7c
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-11-04 14:51:00 +0500

    url avaiability

--- app/services/psi_static_service.rb
+++ app/services/psi_static_service.rb
@@ -148,7 +148,7 @@ class PsiStaticService < BaseService
       unless unit.building_is_updated.present? && unit.building_is_updated
         unit.building = building.present? ? building.gsub("Building ", "") : ""
       end
-      unit.availability_url = u['UnitAvailabilityURL'] if u['UnitAvailabilityURL'].present?
+      unit.availability_url = u['Availability']['UnitAvailabilityURL'] if u['Availability'].present?
       unit.manually_updated = false
       unit.save(validate: false)
 
