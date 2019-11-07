1eab39cd8af6d209636a25eda24d62eb2ccb3580
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-11-07 13:17:29 +0500

    reverts

--- app/jobs/clone_community_job.rb
+++ app/jobs/clone_community_job.rb
@@ -3,20 +3,20 @@ class CloneCommunityJob < ApplicationJob
   include SuckerPunch::Job
 
   def perform(community)
-    copy community
-    # copy_community = community.amoeba_dup
-    # count = nil
-    # while Community.where(name: community.name + " (Copy#{count.present? ? count : ''})").count > 0
-    #   if count.nil?
-    #     count = 2
-    #   else
-    #     count = count + 1
-    #   end
-    # end
-    # copy_community.name = community.name + " (Copy#{count.present? ? count : ""})"
-    # copy_community.code = (community.code.present? ? community.code + " (Copy#{(count.present? ? count : '')})" : "")
-    #
-    # copy_community.save validate:false
+    # copy community
+    copy_community = community.amoeba_dup
+    count = nil
+    while Community.where(name: community.name + " (Copy#{count.present? ? count : ''})").count > 0
+      if count.nil?
+        count = 2
+      else
+        count = count + 1
+      end
+    end
+    copy_community.name = community.name + " (Copy#{count.present? ? count : ""})"
+    copy_community.code = (community.code.present? ? community.code + " (Copy#{(count.present? ? count : '')})" : "")
+
+    copy_community.save validate:false
 
 
   end
