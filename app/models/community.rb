41c7498f67294aff646746ed826c1430c76867f1
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-11-07 16:51:43 +0500

    check

--- app/models/community.rb
+++ app/models/community.rb
@@ -123,10 +123,6 @@ class Community < ApplicationRecord
   scope :self_tour_enabled_only, -> { where('self_tour = ?', true) }
   amoeba do
     include_association :design
-    customize(lambda { |original_object,new_object|
-      new_object.logo = original_object.logo
-      new_object.secondary_logo = original_object.secondary_logo
-    })
   end
   def crop_image
     logo.recreate_versions! if (crop_x.present? && image_bit && do_crop)
