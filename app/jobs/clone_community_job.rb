7a591b045c0c2da96b489a6ccc9dba5a705ea6fc
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-11-08 09:41:31 +0500

    logo update

--- app/jobs/clone_community_job.rb
+++ app/jobs/clone_community_job.rb
@@ -15,8 +15,13 @@ class CloneCommunityJob < ApplicationJob
     end
     copy_community.name = community.name + " (Copy#{count.present? ? count : ""})"
     copy_community.code = (community.code.present? ? community.code + " (Copy#{(count.present? ? count : '')})" : "")
-    copy_community.save validate:false
 
+    copy_community.save validate:false
+    sleep 20
+    copy_community.logo = community.logo
+    copy_community.secondary_logo = community.secondary_logo
+
+    copy_community.save validate:false
 
   end
     def copy(community)
