f0967604caf7d0f532b737d46a12ec837a81f9cb
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-11-15 16:51:01 +0500

    image quality

--- app/uploaders/site_map_uploader.rb
+++ app/uploaders/site_map_uploader.rb
@@ -17,13 +17,13 @@ class SiteMapUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   version :svg_for_metro , :if => :svg? do
     process convert: 'jpg'
-    process optimize: [{quality: 50, level: 7}]
     resize_to_fit(1412, 932)
     def full_filename (for_file = model.image.file) 
       #{}"#{timestamp}-#{model.id.to_s + '.png'}"  
       super.chomp(File.extname(super)) + '.jpg'
     end 
   end
+  process optimize: [{quality: 50, level: 7}]
 
   def set_file_dimensions
     if image?(file)
