2bbebe9c19ccbaf2880bd9a314a14a3a4dc2a69a
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-11-27 10:37:18 +0500

    pry

--- app/uploaders/gallery_uploader.rb
+++ app/uploaders/gallery_uploader.rb
@@ -53,7 +53,7 @@ class GalleryUploader < CarrierWave::Uploader::Base
     # process :crop
     resize_to_limit(1920, 1080)
   end
-  process optimize: [{quality: 50, level: 7}]
+  # process optimize: [{quality: 50, level: 7}]
   def png_name for_file, version_name
     %Q{#{version_name}_#{for_file.chomp(File.extname(for_file))}.png}
   end
