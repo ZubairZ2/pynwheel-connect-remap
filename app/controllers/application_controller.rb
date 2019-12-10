30c4fe91147062996364e0cfd69092217951aef2
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-12-10 13:03:06 +0500

    application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,9 @@ class ApplicationController < ActionController::Base
 		  @community ||= Community.find params[:id]
 	  end  	
   end
-  def info_for_paper_trail
-    { community_id: (current_community.present? ? current_community.id : nil),company_id: (current_company.present? ? current_company.id : nil) }
-  end
+  # def info_for_paper_trail
+  #   { community_id: (current_community.present? ? current_community.id : nil),company_id: (current_company.present? ? current_company.id : nil) }
+  # end
 
   def current_company
     if params[:company_id].present?
