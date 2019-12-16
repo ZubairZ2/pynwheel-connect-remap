b9108c3fa02bb90187fc4c1294e0a7e358832b2b
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-12-16 15:19:39 +0500

    floor range

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -268,7 +268,7 @@ class ToursController < ApplicationController
     elev_name = "Elevator#{last_elevator_id}"
     elev_desc = "Elevator#{last_elevator_id}"
     @floorplate = Floorplate.find params[:floorplate] if params[:floorplate].present?
-    floorplate_range = "-"
+    floorplate_range = @floorplate.present? ? @floorplate.range : "-"
 
 
     elevator = Elevator.create(name: elev_name, description: elev_desc, x_plot: 10, y_plot: 40, floorplate_covering_range: floorplate_range, image: File.open("app/assets/images/elev2.png"),floorplate_id: @floorplate.present? ? @floorplate.id : nil)
