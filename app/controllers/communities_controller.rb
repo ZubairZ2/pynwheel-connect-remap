ad04ce84b694fd00344506f5ff3fee3090a7de38
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2019-12-20 11:23:57 +0500

    add keys done

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -432,6 +432,15 @@ class CommunitiesController < ApplicationController
     @community.display_floorplan_gallery = params[:display_floorplan_gallery].present? ? params[:display_floorplan_gallery] : false
     @community.display_unit_on_homepage = params[:display_unit_on_homepage].present? ? params[:display_unit_on_homepage] : false
     @community.apartment_page_name = params[:apartment_page_name] if params[:apartment_page_name].present?
+
+
+    
+    @community.show_property_map_key = params[:show_property_map_key].present? ? params[:show_property_map_key] : false
+    @community.show_amenity_key = params[:show_amenity_key].present? ? params[:show_amenity_key] : false
+
+    @community.show_property_map_key_text = params[:show_property_map_key_text] if params[:show_property_map_key_text].present?
+    @community.show_amenity_key_text = params[:show_amenity_key_text] if params[:show_amenity_key_text].present?
+
     if @community.save
       flash[:notice] = "Apartment settings updated successfully."
       redirect_back(fallback_location: root_path)
