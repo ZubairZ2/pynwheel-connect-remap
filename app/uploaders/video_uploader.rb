efdf82a9e92563564a8e608362d8476bb0bdf7c6
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-01-01 15:10:45 +0500

    revert

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,11 +1,11 @@
 class VideoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  include CarrierWave::Video  # for your video processing
-  include CarrierWave::Video::Thumbnailer
-  # include ::CarrierWave::Backgrounder::Delay
+  #include CarrierWave::Video  # for your video processing
+  #include CarrierWave::Video::Thumbnailer
+  include ::CarrierWave::Backgrounder::Delay
   include CarrierWaveDirect::Uploader
   # Choose what kind of storage to use for this uploader:
   #storage :file
