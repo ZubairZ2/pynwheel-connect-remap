5c7db16f0ebb9bb60f7c215f45edc04714d67362
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-01-15 12:05:06 +0500

    issue fixed changes

--- app/controllers/tour_users_controller.rb
+++ app/controllers/tour_users_controller.rb
@@ -4,7 +4,7 @@ class TourUsersController < ApplicationController
 
   def index
     @community = Community.find params[:community_id]
-    user_ids = TourHistory.where(tour_id: @community.tour.present? ? @community.tour.id : nil).map{|x| x.tour_user_id}.uniq
+    user_ids = TourHistory.where(tour_id: @community.tour.present? ? @community.tour.id : nil).order('arrived desc').map{|x| x.tour_user_id}.uniq
     @tour_users = []
     user_ids.each {|x| @tour_users << TourUser.find_by_id(x)}
   end
