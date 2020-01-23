66378d9bc7498e13f6be668b7b2de91673bfb1a6
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-01-23 16:42:31 +0500

    changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -43,3 +43,16 @@ CarrierWave.configure do |config|
   config.fog_directory  = ENV['S3_BUCKET_NAME']                                  # required
   config.cache_dir = "#{Rails.root}/public/uploads/tmp"
 end
+module CarrierWave
+  module RMagick
+
+    def quality(percentage)
+      manipulate! do |img|
+        img.write(current_path){ self.quality = percentage } unless img.quality == percentage
+        img = yield(img) if block_given?
+        img
+      end
+    end
+
+  end
+end
