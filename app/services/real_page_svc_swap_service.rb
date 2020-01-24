521a177fe6226418147419f4bfc043e822421690
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-01-24 10:44:04 +0500

    current date

--- app/services/real_page_svc_swap_service.rb
+++ app/services/real_page_svc_swap_service.rb
@@ -524,7 +524,7 @@ class RealPageSvcSwapService < BaseService
         license_key = REALPAGESVC_LICENSE_KEY
         community_id = credentials.community_id
 
-        date_check = Date.today + 540
+        date_check = Date.today
         response = HTTParty.post(
             url,
             :headers => {"Content-Type" => "text/xml","Content-Length"=>'1993',"Accept"=>"text/xml","Cache-Control"=>"no-cache","Pragma"=>"no-cache","SOAPAction"=>soap_action},
