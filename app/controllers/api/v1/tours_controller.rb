524c7a6938e6b791cbdcdc7e210164c580f65c6c
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-01-27 15:51:09 +0500

    available date and available

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -173,7 +173,7 @@ class Api::V1::ToursController < ActionController::Base
   def floorplan_units
     if params[:unit_id].present?
       unit = Unit.find_by_id(params[:unit_id])
-      @units = Unit.where(floorplan_id: unit.floorplan_id,community_id: unit.community_id) if unit.present?
+      @units = Unit.where('floorplan_id = ? AND community_id = ? AND available = ? AND available_date > ?', unit.floorplan_id,unit.community_id,true, Date.today) if unit.present?
       @units.each do |u|
         if u.community.is_sitemap?
           u.sitemap_image_url = u.community.sitemap.image.url(:svg_for_metro).present? ? u.community.sitemap.
