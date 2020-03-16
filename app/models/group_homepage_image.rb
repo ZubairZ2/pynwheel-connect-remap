82615e09fff0194871bd7f9ed176c077aaf395e5
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-03-16 11:55:04 +0500

    changes

--- app/models/group_homepage_image.rb
+++ app/models/group_homepage_image.rb
@@ -1,4 +1,10 @@
 class GroupHomepageImage < ApplicationRecord
   belongs_to :group_design
   mount_uploader :image, ImageUploader
+  before_create :set_image_name
+
+
+  def set_image_name
+    self.name = image.file.filename if image.present?
+  end
 end
