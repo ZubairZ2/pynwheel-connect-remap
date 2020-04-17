5e7c4706827c72e40514ea344f7c123fa2d6c79e
Hamza <humza4142@gmail.com>
2020-04-17 16:28:56 +0500

    added new

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :community
 
-  after_create_commit { MessageBroadcastJob.perform_async(self) }
+  after_create_commit { MessageBroadcastJob.perform_later self }
 end
