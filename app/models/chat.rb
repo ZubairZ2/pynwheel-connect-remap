2c2d62df6e18702bde0870c99980a9655b17c831
Hamza <humza4142@gmail.com>
2020-04-24 14:05:41 +0500

    api's completed

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,9 +1,9 @@
 class Chat < ApplicationRecord
     belongs_to :chatroom
   
-    # after_create :notify_pusher
+    after_create :notify_pusher
 
-    # def notify_pusher
-    #   Pusher.trigger('chat', 'new-chat', self.as_json)
-    # end
+    def notify_pusher
+      Pusher.trigger('chat', 'new-chat', self.as_json)
+    end
 end
