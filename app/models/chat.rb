d0b29cb3dea66272cc56501ee0cd67329c971551
Hamza <humza4142@gmail.com>
2020-04-25 15:13:03 +0500

    latest chat model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -4,6 +4,13 @@ class Chat < ApplicationRecord
     after_create :notify_pusher
 
     def notify_pusher
-      Pusher.trigger('chat', 'new-chat', self.as_json)
+      data = self.attributes
+      unless data[:name] == "Support Team"
+        data[:tour_user] = {}
+        data[:tour_user][:name] = self.chatroom.tour_user.name
+        data[:tour_user][:email] = self.chatroom.tour_user.email
+      end 
+      
+      Pusher.trigger('chat', 'new-chat', data.as_json)
     end
 end
