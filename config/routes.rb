ccaeca651303a84b8e4ce945c4577f91df145e19
Hamza <humza4142@gmail.com>
2020-04-27 12:04:43 +0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -27,9 +27,6 @@ Rails.application.routes.draw do
     
   end 
 
-  resources :messages do
-  end
-
   devise_for :users, :controllers => { :invitations => 'invitations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
