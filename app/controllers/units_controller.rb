7e045ae6e6dda8e1ed5d8597b62064b7d1c0a64c
Hamza <humza4142@gmail.com>
2020-05-04 17:01:11 +0500

    space added

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -36,8 +36,7 @@ class UnitsController < ApplicationController
 
   def edit
     add_breadcrumb "Units", community_units_path(@community)
-    add_breadcrumb "Edit Unit",edit_community_unit_path(@community,@unit)
-    
+    add_breadcrumb "Edit Unit",edit_community_unit_path(@community,@unit)   
   end
 
   def update
