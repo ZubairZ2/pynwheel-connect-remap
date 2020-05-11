4505590382350ec0c8f6f64c16746f5f844fd969
Umer Sani <muhammad.umer@intagleo.com>
2020-05-11 18:22:29 +0500

    almost done

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -634,6 +634,15 @@ function showDataTables(){
         "stateSave": true,
         "paging": false
     });
+    $('#miyazaki.schedule_tour_table').DataTable({
+        'aoColumnDefs': [{
+            'bSortable': false,
+            'aTargets': [5],
+        }],
+        "ordering": true,
+        "stateSave": true,
+        "paging": false
+    });
     $('#miyazaki.floorplan_data_table').DataTable({
         'aoColumnDefs': [{
             'bSortable': false,
