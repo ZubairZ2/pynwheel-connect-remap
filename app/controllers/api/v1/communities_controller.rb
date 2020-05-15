d3185bf28d6c9bd1463ab3f11671298af6f5cdaf
Umer Sani <muhammad.umer@intagleo.com>
2020-05-15 12:24:15 +0500

    revert

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -127,7 +127,7 @@ class Api::V1::CommunitiesController < ActionController::Base
 
   def user_saved_tour
     # @device_id = params[:device_id]
-    @community = Community.find params[:id] if params[:id].present?
+    @community = Community.find params[:community_id] if params[:community_id].present?
     @tour_user = TourUser.find params[:tour_user_id]
     @tours = VisitedStop.where(tour_user_id: @tour_user.id).group('tour_id').group('tour_key').count
   end
