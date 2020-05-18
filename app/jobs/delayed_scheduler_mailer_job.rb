983bb897130a15a576156ec37c71f1d91273be79
Umer Sani <muhammad.umer@intagleo.com>
2020-05-18 16:29:54 +0500

    almost dine

--- app/jobs/delayed_scheduler_mailer_job.rb
+++ app/jobs/delayed_scheduler_mailer_job.rb
@@ -3,6 +3,6 @@ class DelayedSchedulerMailerJob < ApplicationJob
 
   def perform(subject, msg, to, community_subject, community_msg, community_to)
     NotificationMailer.tour_history_mail(subject, msg, to).deliver_now
-    CommunityNotificationMailer.tour_history_mail(community_subject, community_msg, community_to).deliver_now
+    NotificationMailer.tour_history_mail(community_subject, community_msg, community_to).deliver_now
   end
 end
