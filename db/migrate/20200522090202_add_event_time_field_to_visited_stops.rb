0bd30a5f7fc1f0e3db3bea34e113bc4dca34c0ba
Umer Sani <muhammad.umer@intagleo.com>
2020-05-28 16:58:01 +0500

    changes v3

--- db/migrate/20200522090202_add_event_time_field_to_visited_stops.rb
+++ db/migrate/20200522090202_add_event_time_field_to_visited_stops.rb
@@ -1,5 +1,7 @@
 class AddEventTimeFieldToVisitedStops < ActiveRecord::Migration[5.0]
   def change
     add_column :visited_stops, :event_time, :time
+    add_column :visited_stops, :event_date, :date
   end
 end
+
