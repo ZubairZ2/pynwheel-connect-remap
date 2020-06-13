0c7ca236cd6fa21e2e6e5ef2b2f78660c7b174d5
Hamza <humza4142@gmail.com>
2020-06-14 03:27:30 +0500

    updated with expire user guest id

--- app/jobs/import_remotelock_events_job.rb
+++ app/jobs/import_remotelock_events_job.rb
@@ -1,8 +1,7 @@
 class ImportRemotelockEventsJob < ApplicationJob
   include SuckerPunch::Job
 
-  def perform(tour_user, tour_history, arrived, left)
-
+  def perform(tour_user, tour_history, arrived, left, assigned_pin)
 		community = (Tour.find tour_history.tour_id).community
 		as_guests_data = tour_user.as_guests.find_by(community_id: community.id)
 
@@ -19,33 +18,16 @@ class ImportRemotelockEventsJob < ApplicationJob
             rml = RemoteLock.find_by(device_id: event["attributes"]["publisher_id"])
             tour_history.lock_histories.create(event: event["type"], occured_at: occurred_at, stop_id: rml.stop_id, stop_name: rml.stop_name, stop_type: rml.stop_type , tour_user_id: tour_user.id)
           
-          elsif expire_user_exists(event,tour_user.name)
+          elsif expire_user_exists(event, tour_user.name, assigned_pin)
             
             occurred_at = event["attributes"]["occurred_at"].to_datetime.utc
-            
-            puts '***'*50
-            puts occurred_at
-            puts arrived
-            puts left
-            puts '***'*50
-
-            if occurred_at >= arrived and occurred_at <= left
-              puts '==='*50
-              puts event["attributes"]["publisher_id"]
-              puts '==='*50
-
-              rml = RemoteLock.find_by(device_id: event["attributes"]["publisher_id"])
-              tour_history.lock_histories.create(event: event["type"], occured_at: occurred_at, stop_id: rml.stop_id, stop_name: rml.stop_name, stop_type: rml.stop_type , tour_user_id: tour_user.id)
-            end
-
+            rml = RemoteLock.find_by(device_id: event["attributes"]["publisher_id"])
+            tour_history.lock_histories.create(event: event["type"], occured_at: occurred_at, stop_id: rml.stop_id, stop_name: rml.stop_name, stop_type: rml.stop_type , tour_user_id: tour_user.id)
+ 
           elsif sync_events_exists(event,as_guests_data.guest_id) # just for testing
             
-            puts '==='*50
-            puts "sync_events_exists"
-            puts '==='*50
-            
             occurred_at = event["attributes"]["occurred_at"].to_datetime.utc
-            rml = RemoteLock.find_by(device_id: event["attributes"]["publisher_id"]) 
+            rml = RemoteLock.find_by(device_id: event["attributes"]["publisher_id"])
             tour_history.lock_histories.create(event: event["type"], occured_at: occurred_at, stop_id: rml.stop_id, stop_name: rml.stop_name, stop_type: rml.stop_type , tour_user_id: tour_user.id)
           
           end
@@ -61,8 +43,8 @@ class ImportRemotelockEventsJob < ApplicationJob
     return (event["type"] == "unlocked_event" and event["attributes"]["source"] == "user" and  event["attributes"]["status"] == "succeeded" and event["attributes"]["associated_resource_id"].present? and event["attributes"]["associated_resource_id"] == guest_id)
   end
   
-  def expire_user_exists(event,name)
-    return (event["attributes"]["associated_resource_name"] == name and event["attributes"]["method"] == "pin" and event["attributes"]["pin"].present?)
+  def expire_user_exists(event,name,pin)
+    return (event["type"] == "unlocked_event" and event["attributes"]["source"] == "user" and  event["attributes"]["status"] == "succeeded" and  event["attributes"]["associated_resource_name"] == name and event["attributes"]["method"] == "pin" and event["attributes"]["pin"] == pin)
   end
 
   def sync_events_exists(event,guest_id)
--- app/models/tour_history.rb
+++ app/models/tour_history.rb
@@ -49,7 +49,9 @@ class TourHistory < ApplicationRecord
   		@mail_content[1] = "#{@mail_content.last} \n #{self.tour_user.name} \n #{self.tour_user.email}"+ "<br><br>See Tour Summary <a href='#{url}'>Click Here</a>"
   		self.update_attributes(end_tour_email_sent: true)
 		touruser_remotelock_data(self.arrived,self.left)
-		ImportRemotelockEventsJob.perform_in(3600, self.tour_user, self, self.arrived, self.left) if self.tour_user.name == "humza4142@gmail.com"
+		property = (Tour.find self.tour_id).community
+		assigned_pin = self.tour_user.as_guests.find_by(community_id: property.id).edgestate_pin
+		ImportRemotelockEventsJob.perform_in(3600, self.tour_user, self, self.arrived, self.left, assigned_pin) if self.tour_user.name == "humza4142@gmail.com"
 		send_email_sms_or_both @mail_content
 		# community.deleted_ids = []
 		community.save
@@ -60,7 +62,9 @@ class TourHistory < ApplicationRecord
   		@mail_content[1] = "#{@mail_content.last} stop #{self.abandoned_tour_at_stop.to_s}"
 		self.update_attributes(abandoned_tour_email_sent: true)
 		touruser_remotelock_data(self.arrived,self.lengthy_stay)
-		ImportRemotelockEventsJob.perform_in(3600, self.tour_user, self, self.arrived, self.lengthy_stay) if self.tour_user.name == "humza4142@gmail.com"
+		property = (Tour.find self.tour_id).community
+		assigned_pin = self.tour_user.as_guests.find_by(community_id: property.id).edgestate_pin
+		ImportRemotelockEventsJob.perform_in(3600, self.tour_user, self, self.arrived, self.lengthy_stay, assigned_pin) if self.tour_user.name == "humza4142@gmail.com"
 	    send_email_sms_or_both @mail_content
 	    # community.deleted_ids = []
 	    community.save
