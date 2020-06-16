df635b2a767a3274c1414a84a6efb7109642403f
Umer Sani <muhammad.umer@intagleo.com>
2020-06-16 11:32:12 +0500

    mail error

--- app/jobs/delayed_scheduler_mailer_job.rb
+++ app/jobs/delayed_scheduler_mailer_job.rb
@@ -1,8 +1,8 @@
 class DelayedSchedulerMailerJob < ApplicationJob
   include SuckerPunch::Job
 
-  def perform(subject, msg, to, community_subject, community_msg, community_to)
+  def perform(subject, msg, to, community_subject = nil, community_msg = nil, community_to = nil)
     NotificationMailer.tour_history_mail(subject, msg, to).deliver_now
-    NotificationMailer.tour_history_mail(community_subject, community_msg, community_to).deliver_now
+    NotificationMailer.tour_history_mail(community_subject, community_msg, community_to).deliver_now unless community_subject == nil
   end
 end
