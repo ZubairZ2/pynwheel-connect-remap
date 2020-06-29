8fb4169b98ff875161df91863e5289fb7d39d20c
Hamza <humza4142@gmail.com>
2020-06-29 15:17:27 +0500

    updated

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -209,7 +209,7 @@ class Api::V1::CommunitiesController < ActionController::Base
 
   def is_tour_in_visiting_hours(time_param,community)
     current_time = time_param.to_datetime.strftime("%H:%M")
-    current_day = current_datetime.to_datetime.strftime('%A')
+    current_day = current_time.to_datetime.strftime('%A')
     community.opening_hours.where('day = ? and opening_time <= ? and closing_time >= ?', current_day, current_time, current_time)
   end
 
