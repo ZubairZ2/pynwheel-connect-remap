013d9e3500c0cbd79b2def48ca97aae903735f7a
Hamza <humza4142@gmail.com>
2020-07-06 11:03:49 +0500

    added thank you message at tour ending

--- app/models/tour_history.rb
+++ app/models/tour_history.rb
@@ -48,6 +48,10 @@ class TourHistory < ApplicationRecord
       url = Rails.env.production? ? "https://pynwheelapp.com/communities/#{@community.id}/tour%5Fusers" : "https://pynwheel-staging.herokuapp.com/communities/#{@community.id}/tour%5Fusers"
   		@mail_content[1] = "#{@mail_content.last} \n #{self.tour_user.name} \n #{self.tour_user.email}"+ "<br><br>See Tour Summary <a href='#{url}'>Click Here</a>"
 
+		touruser = self.tour_user
+		tour_user_url = Rails.env.production? ? "https://pynwheelapp.com/communities/#{@community.id}/tour%5Fusers/#{touruser.id}" : "https://pynwheel-staging.herokuapp.com/communities/#{@community.id}/tour%5Fusers/#{touruser.id}"
+		@complete_tour_content  = ["#{@community.name} has been visited", "#{touruser.name.capitalize} (#{touruser.email}#{', ' + touruser.phone_number if touruser.phone_number.present?}) has completed a tour of your property! To view the details of their visit, please click here: <a href='#{tour_user_url}'>#{touruser.name.capitalize} Visitor Details</a> "]
+		@thank_you_content  = "Thank you for visiting #{@community.name}! We hope you enjoyed your tour. Go back to the Pynwheel Self Tour app any time to review the details of your tour."
 		touruser_remotelock_data
 		tour = (Tour.find_by_id self.tour_id)
 		assigned_pin = self.tour_user.as_guests.find_by(community_id: tour.community.id).edgestate_pin if tour.present? and self.tour_user.present? and self.tour_user.as_guests.present?
@@ -58,6 +62,8 @@ class TourHistory < ApplicationRecord
 		DelayedSchedulerMailerJob.perform_async("Remote Lock Events", email_content, "humza4142@gmail.com","Lock History has been imported","check the database, its ran in callback","humza4142@gmail.com") if self.tour_user.present? and self.tour_user.as_guests.present?
 
 		send_email_sms_or_both @mail_content
+		send_email_sms_or_both @complete_tour_content
+		send_email_sms_or_both_to_touruser @thank_you_content
 		# community.deleted_ids = []
 		community.save
   	end
@@ -68,6 +74,7 @@ class TourHistory < ApplicationRecord
 
 		@mail_content[1] = "#{@mail_content.last} stop #{self.abandoned_tour_at_stop.to_s}"
 		  
+		@thank_you_content  = "Thank you for visiting #{@community.name}! We hope you enjoyed your tour. Go back to the Pynwheel Self Tour app any time to review the details of your tour."
 		touruser_remotelock_data
 		tour = (Tour.find_by_id self.tour_id)
 		assigned_pin = self.tour_user.as_guests.find_by(community_id: tour.community.id).edgestate_pin if tour.present? and self.tour_user.present? and self.tour_user.as_guests.present?
@@ -78,6 +85,7 @@ class TourHistory < ApplicationRecord
 		DelayedSchedulerMailerJob.perform_async("Remote Lock Events", email_content, "humza4142@gmail.com","Lock History has been imported","check the database, its ran in callback","humza4142@gmail.com") if self.tour_user.present? and self.tour_user.as_guests.present?
 
 		send_email_sms_or_both @mail_content
+		send_email_sms_or_both_to_touruser @thank_you_content
 	    # community.deleted_ids = []
 	    community.save
   	end
@@ -133,27 +141,22 @@ class TourHistory < ApplicationRecord
   	end
   end
 
+  def send_email_sms_or_both_to_touruser thank_you_msg
+	if community.alert_contact == "email"
+		send_email_tour_user "Thank you for visiting #{community.name}", thank_you_msg
+	elsif community.alert_contact == "phone"
+		send_sms_tour_user thank_you_msg
+	else
+		send_email_tour_user "Thank you for visiting #{community.name}", thank_you_msg
+		send_sms_tour_user thank_you_msg
+	end
+  end
 
   def send_sms message_body
-		# DANGER! This is insecure. See http://twil.io/secure
-		# binding.pry
-		# to: '+923236808910'
-		
-
-		# account_sid = 'AC100385e8559f1ad63a5dbfaa3272a8d5'
-		# auth_token = '1f768aeab1be375bfe8da7a5e7310e74'
-
-		# account_sid = 'ACcd5341bccaa0000972f42fded7122d87'
-		# auth_token = 'b3bdde4cf7d6d4b61a7065580920bd53'
-
-		# @client = Twilio::REST::Client.new(account_sid, auth_token)
-
-		# message = @client.messages
-		#   .create( 
-		#   	body: message_body,
-		#     from: '+12017012957',
-		#     to: community.phone
-		#   )
+	# begin
+	# 	DelayedSchedulerTextJob.perform_async(message_body, community.phone) if community.phone.present? 
+	# rescue
+	# end
   end
 
   def send_email subj, body
@@ -164,7 +167,20 @@ class TourHistory < ApplicationRecord
 		end
   end
 
-  
+	def send_sms_tour_user message_body
+		begin
+			DelayedSchedulerTextJob.perform_async(message_body, self.tour_user.phone_number) if self.tour_user.phone_number.present? 
+		rescue
+		end
+	end
+
+  	def send_email_tour_user subj, body
+		begin
+			NotificationMailer.tour_history_mail(subj.humanize, body, self.tour_user.email).deliver
+		rescue
+		end
+	end
+
   def time_difference
   	((Time.zone.now - self.arrived) / 1.minute).round
   end
