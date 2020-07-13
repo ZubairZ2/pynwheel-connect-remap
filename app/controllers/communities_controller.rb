bea2fad4c1ed60fb9f2f5c89f9c81c419e5da7cd
Umer Sani <muhammad.umer@intagleo.com>
2020-07-13 14:24:33 +0500

    almost done

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -538,6 +538,7 @@ class CommunitiesController < ApplicationController
     @tour.save
     @community.show_camera_button = params[:show_camera_button].present? ? true : false
     @community.show_notepad_button = params[:show_notepad_button].present? ? true : false
+    @community.scheduler_widget = params[:scheduler_widget].present? ? true : false
     @community.tour.update_attributes(max_tour_users: params[:max_tour_users])
 
     if @community.save
