0cfa60560677e7687211d1d30d4a8ba2dfad1a44
Umer Sani <muhammad.umer@intagleo.com>
2020-08-06 13:00:14 +0500

    logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,10 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "pynwheel-cms_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.after_initialize do
+    ActiveRecord::Base.logger = Rails.logger.clone
+    ActiveRecord::Base.logger.level = Logger::ERROR
+  end
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
