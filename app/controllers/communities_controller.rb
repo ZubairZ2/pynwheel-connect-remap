7063ee26f20c43f8ee3b14ad3bd69b74f434fe7b
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-09-02 14:42:08 +0500

    done

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -539,6 +539,8 @@ class CommunitiesController < ApplicationController
     @community.thank_you_message = params[:community][:thank_you_message] if params[:community][:thank_you_message].present?
     @tour.dotted_line_color = params[:dotted_line_color].downcase if params[:dotted_line_color].present?
     @tour.credit_card_required = params[:credit_card_required].present? ? true : false
+    
+    @tour.verification_type = params[:verification_type] if params[:verification_type].present?
     @tour.save
     @community.show_camera_button = params[:show_camera_button].present? ? true : false
     @community.show_notepad_button = params[:show_notepad_button].present? ? true : false
