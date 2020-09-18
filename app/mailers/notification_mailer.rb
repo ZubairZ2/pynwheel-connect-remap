49db0f56d205f8e46f090bd685d11d598eaac057
Bilal <bilal.novatore@gmail.com>
2020-09-18 18:46:21 +0500

    email fix

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,8 +4,12 @@ class NotificationMailer < ApplicationMailer
 
 	def tour_history_mail subject, msg, to, email_from
 		@email_body = msg
-		mail(to: to, from: email_from, subject: subject)
-		# mail(to: to, subject: subject, body: msg)
+		if (subject == "Your Tour Tomorrow" or subject == "Your Tour Tomorrow" or subject == "Your tour starts soon!" or subject == "Tour has been scheduled")
+			mail(to: to, from: email_from, subject: subject)
+		else
+			mail(to: to, from: "info@pynwheel.com", subject: subject)
+			# mail(to: to, subject: subject, body: msg)
+		end
 	end
-	
+
 end
