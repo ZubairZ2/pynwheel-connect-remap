1602a25e4c350c05205c5df3574e064f5b5f8914
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-09-21 21:17:04 +0500

    added stop_marketing_names_visited_by_user in background job

--- app/jobs/real_page_guest_card_integration_job.rb
+++ app/jobs/real_page_guest_card_integration_job.rb
@@ -2,9 +2,9 @@ class RealPageGuestCardIntegrationJob < ApplicationJob
     include SuckerPunch::Job
   
     def perform(credentials, tour_user, tour_time, end_time, tour_status, available_stops, visited_stops)
-
         cred = JSON.parse(credentials)
         community = Community.find_by_id cred["community_id"]
+        visited_stops = stop_marketing_names_visited_by_user(tour_user.id, community.tour.id)
         prospect = Prospect.where(tour_user_id: tour_user.id, community_id: community.id,  data_provider: community.data_provider).last if community.present?
         
         # if prospect.present?
@@ -48,4 +48,25 @@ class RealPageGuestCardIntegrationJob < ApplicationJob
         real_page_insert_follow_up_service = RealPageInsertFollowUpService.new(JSON.parse(credentials))
         real_page_insert_follow_up_service.perform(tour_user, tour_time, end_time, leasing_agent)
     end
+
+    def stop_marketing_names_visited_by_user(tour_user_id, tour_id)
+		visited_stops = []
+
+		current_tour = VisitedStop.where(tour_user_id: tour_user_id, tour_id: tour_id).last
+		tour_key = current_tour.tour_key if current_tour.present?
+		if tour_key.present?
+			tour_stop_ids = VisitedStop.where(tour_key: tour_key).pluck(:tour_stop_id)
+			unit_stops = TourStop.where(id: tour_stop_ids, stop_type: "unit").pluck(:stop_id)
+
+			unit_stops.each do |stop_id|
+				unit = Unit.find_by_id stop_id
+				marketing_name = unit.marketing_name
+				visited_stops << marketing_name if unit.present?
+			end
+		end
+        puts "<<<<<<<<<<    visited_stops in background job >>>>>>>>>>>>>>>>"
+		puts visited_stops
+		puts "<<<<<<<<<<    visited_stops in background job >>>>>>>>>>>>>>>>"
+		return visited_stops
+	end
 end
