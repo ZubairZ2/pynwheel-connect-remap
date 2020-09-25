1eb8cd9d11ab3e0a0163d19b490cde24b8bd6747
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-09-25 20:35:39 +0500

    updated session file

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 unless Rails.env.development?
-  Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , secure: :secure , domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
+  Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , same_site: :none, secure: :true , domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
 else
-  Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , secure: :secure
+  Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , same_site: :none, secure: :true
 end
