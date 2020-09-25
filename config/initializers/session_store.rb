273ff60cfbbfece3e2088545a29ca2463b65e76e
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-09-25 19:29:11 +0500

    checking same_site with none

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 unless Rails.env.development?
-  Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , same_site: :strict , domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
+  Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , same_site: :none , domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
 else
-  Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , same_site: :strict
+  Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , same_site: :none
 end
