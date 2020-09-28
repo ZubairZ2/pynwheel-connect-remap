dfda7f5a44f4b72d578fcad22229585b7194b4a9
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-09-28 21:02:16 +0500

    testing push on live

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 # Be sure to restart your server when you modify this file.
 unless Rails.env.development?
   Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , same_site: :none, secure: :true , domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
+  Rails.application.config.session_store :cookie_store, key: 'community_id' , same_site: :none, secure: :true , domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
+  Rails.application.config.session_store :cookie_store, key: 'favorite_unit_ids' , same_site: :none, secure: :true , domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
+  Rails.application.config.session_store :cookie_store, key: 'session_id' , same_site: :none, secure: :true , domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
 else
   Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , same_site: :none, secure: :true
 end
