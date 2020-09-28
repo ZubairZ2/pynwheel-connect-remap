68194e0f40b991bb0723a36d83e86c1e3f9f7b4a
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-09-28 18:16:46 +0500

    merge

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -122,6 +122,7 @@ class Api::V1::CommunitiesController < ActionController::Base
     end
   end
 
+
   def community_tours
     @tour_user = TourUser.find_by_id params[:tour_user_id]
     @community = Community.find params[:id]
