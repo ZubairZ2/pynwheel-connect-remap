be3675caff9ec01e64899c7ee3f8e580cc92771e
Bilal <bilal.novatore@gmail.com>
2020-09-30 12:36:04 +0500

    add link

--- app/models/tour_history.rb
+++ app/models/tour_history.rb
@@ -38,7 +38,9 @@ class TourHistory < ApplicationRecord
   	end
 
   	if self.id_mismatch
-  		@mail_content = ["id_mismatch", "The photo ID/selfie were flagged as a mis-match"] #get_alert_message('id_mismatch')
+			vs = self.tour_user
+			url = Rails.env.production? ? "https://pynwheelconnect.com/id_selfie_matching/#{vs.id }?community=#{community.id}" : "https://pynwheel-staging.herokuapp.com/id_selfie_matching/#{vs.id }?community=#{community.id}"
+  		@mail_content = ["id_mismatch", "The photo ID/selfie for #{vs.name} were flagged as a mis-match <br/> <a href='#{url}' target='_blank'> Visitor's ID page </a>"] #get_alert_message('id_mismatch')
   		send_email_sms_or_both @mail_content
   	end
 
