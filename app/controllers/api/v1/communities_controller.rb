81ee158e311d273d035c48ff8e31b2675d60ae16
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-10-08 14:24:13 +0500

    community tour

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -134,6 +134,8 @@ class Api::V1::CommunitiesController < ActionController::Base
     @building_list = @community.units.map{|x| x.building rescue next}.uniq.compact + @community.amenities.map{|x| x.building rescue next}.uniq.compact
     @building_list = @building_list.compact.reject { |c| c.empty? }.uniq.sort
     @building_list = @building_list.map {|i| i.gsub(/\d+/) {|s| "%08d" % s.to_i } }.zip(@building_list).sort.map{|x,y| y}
+    @community.tour.building_order.present? ? (@building_list =  @community.tour.building_order) : ""
+
     @floor_list = @community.floorplates.map{|x| x.floors}.flatten!.uniq.sort rescue nil
     #####
     @tours = Tour.where(community_id: params[:id])
@@ -206,6 +208,8 @@ class Api::V1::CommunitiesController < ActionController::Base
     @building_list = @community.units.map{|x| x.building rescue next}.uniq.compact + @community.amenities.map{|x| x.building rescue next}.uniq.compact
     @building_list = @building_list.compact.reject { |c| c.empty? }.uniq.sort
     @building_list = @building_list.map {|i| i.gsub(/\d+/) {|s| "%08d" % s.to_i } }.zip(@building_list).sort.map{|x,y| y}
+    @community.tour.building_order.present? ? (@building_list =  @community.tour.building_order) : ""
+
     @floor_list = @community.floorplates.map{|x| x.floors}.flatten!.uniq.sort rescue nil
 
     current_time = params[:current_time].present? ? params[:current_time] : DateTime.now
