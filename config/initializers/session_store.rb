1a4aa2fa3515ec1e8cb26891f30ec3f28c40cccb
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-10-08 12:50:44 +0500

    checking 2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,11 +2,10 @@
 unless Rails.env.development?
   Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' , same_site: :none , secure: true, domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
 
+  Rails.application.config.session_store :cookie_store, key: 'session_id' , same_site: :none , secure: true, domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com"
+
   Rails.application.config.session_store :cookie_store, key: 'favorite_unit_ids' , same_site: :none , secure: true, domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
 
-  Rails.application.config.session_store :cookie_store, key: 'community_id' , same_site: :none , secure: true, domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
-
-  Rails.application.config.session_store :cookie_store, key: 'session_id' , same_site: :none , secure: true, domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
 else
   Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' 
 end
