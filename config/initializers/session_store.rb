458ba0abd519026803295284916cede150b6d054
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-10-08 13:06:51 +0500

    checking 4

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 unless Rails.env.development?
-  Rails.application.config.session_store :cookie_store, key: ['_pynwheel-cms_session','community_id','session_id','favorite_unit_ids'] , same_site: :none , secure: true, domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
-
+  Rails.application.config.session_store :cookie_store, key: :all , domain: Rails.env.production? ? :all :  "pynwheel-staging.herokuapp.com" 
 else
   Rails.application.config.session_store :cookie_store, key: '_pynwheel-cms_session' 
 end
