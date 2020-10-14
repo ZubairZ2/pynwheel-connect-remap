0013deebfef40c928d50a5d9be11e9408266de03
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-10-14 17:37:55 +0500

    added new code with edgestate integration (Hamza)

--- app/controllers/dwelos_controller.rb
+++ app/controllers/dwelos_controller.rb
@@ -1,6 +1,6 @@
 class DwelosController < ApplicationController
   before_action :set_community
-
+  include DweloDevicesHelper
   def index
     if @community.dwelo.present?
       community_remote_locks = @community.dwelo.remote_locks
@@ -42,77 +42,83 @@ class DwelosController < ApplicationController
   end
   def map_dwelo_locks
     community = Community.find(params[:community_id])
-    if community.present?
-      if community.is_sitemap
-        community_units = community.units
-        community_locks = community.dwelo.remote_locks
-        community_units.each do |community_unit|
-          if community_unit.building.present?
-            name = community_unit.building + "-" + community_unit.marketing_name
-            unit_name = name.gsub('-', '').gsub(' ', '')
-            community_locks.each do |community_lock|
-              community_lock_name = community_lock.name.gsub('-', '').gsub(' ', '')
-              if (unit_name == community_lock_name)
-                community_lock.update_attributes(stop_id: community_unit.id, stop_type: "unit", stop_name: community_unit.marketing_name)
-              end
-            end
-          else
-            unit_name = community_unit.marketing_name.gsub('-', '').gsub(' ', '')
-            community_locks.each do |community_lock|
-              community_lock_name = community_lock.name.gsub('-', '').gsub(' ', '')
-              if (unit_name == community_lock_name)
-                community_lock.update_attributes(stop_id: community_unit.id, stop_type: "unit", stop_name: community_unit.marketing_name)
-              end
-            end
-            # same_name_lock = community_locks.find_by(name: community_unit.marketing_name) rescue nil
-          end
-          # if same_name_lock.present?
-          #   same_name_lock.update_attributes(stop_id: community_unit.id, stop_type: "unit", stop_name: community_unit.marketing_name)
-          # end
-
-        end
-      else
-        community_locks = community.dwelo.remote_locks
-        community_floorplates = community.floorplates rescue nil
-        if community_floorplates.present?
-          community_floorplates.each do |community_floorplate|
-            community_floorplate_units = community_floorplate.units rescue nil
-            community_floorplate_units.each do |floorplate_unit|
-              if floorplate_unit.building.present?
-                name = floorplate_unit.building + "-" + floorplate_unit.marketing_name
-                unit_name = name.gsub('-', '').gsub(' ', '')
-                # same_name_lock = community_locks.find_by(name: name) rescue nil
-
-                community_locks.each do |community_lock|
-                  community_lock_name = community_lock.name.gsub('-', '').gsub(' ', '')
-                  if (unit_name == community_lock_name)
-                    community_lock.update_attributes(stop_id: floorplate_unit.id, stop_type: "unit", stop_name: floorplate_unit.marketing_name)
-                  end
+    access_token = dwelo_client_credentials(community.dwelo)
+    if access_token.present? and community.dwelo.remote_locks.present?
+      if community.present?
+        if community.is_sitemap
+          community_units = community.units
+          community_locks = community.dwelo.remote_locks
+          community_units.each do |community_unit|
+            if community_unit.building.present?
+              name = community_unit.building + "-" + community_unit.marketing_name
+              unit_name = name.gsub('-', '').gsub(' ', '')
+              community_locks.each do |community_lock|
+                community_lock_name = community_lock.name.gsub('-', '').gsub(' ', '')
+                if (unit_name == community_lock_name)
+                  community_lock.update_attributes(stop_id: community_unit.id, stop_type: "unit", stop_name: community_unit.marketing_name)
                 end
-              else
-                unit_name = floorplate_unit.marketing_name.gsub('-', '').gsub(' ', '')
-                community_locks.each do |community_lock|
-                  community_lock_name = community_lock.name.gsub('-', '').gsub(' ', '')
-                  if (unit_name == community_lock_name)
-                    community_lock.update_attributes(stop_id: floorplate_unit.id, stop_type: "unit", stop_name: floorplate_unit.marketing_name)
-                  end
-                end
-                # same_name_lock = community_locks.find_by(name: floorplate_unit.marketing_name) rescue nil
               end
-              # if same_name_lock.present?
-              #   same_name_lock.update_attributes(stop_id: floorplate_unit.id, stop_type: "unit", stop_name: floorplate_unit.marketing_name)
-              # end
-
+            else
+              unit_name = community_unit.marketing_name.gsub('-', '').gsub(' ', '')
+              community_locks.each do |community_lock|
+                community_lock_name = community_lock.name.gsub('-', '').gsub(' ', '')
+                if (unit_name == community_lock_name)
+                  community_lock.update_attributes(stop_id: community_unit.id, stop_type: "unit", stop_name: community_unit.marketing_name)
+                end
+              end
+              # same_name_lock = community_locks.find_by(name: community_unit.marketing_name) rescue nil
             end
+            # if same_name_lock.present?
+            #   same_name_lock.update_attributes(stop_id: community_unit.id, stop_type: "unit", stop_name: community_unit.marketing_name)
+            # end
 
           end
+        else
+          community_locks = community.dwelo.remote_locks
+          community_floorplates = community.floorplates rescue nil
+          if community_floorplates.present?
+            community_floorplates.each do |community_floorplate|
+              community_floorplate_units = community_floorplate.units rescue nil
+              community_floorplate_units.each do |floorplate_unit|
+                if floorplate_unit.building.present?
+                  name = floorplate_unit.building + "-" + floorplate_unit.marketing_name
+                  unit_name = name.gsub('-', '').gsub(' ', '')
+                  # same_name_lock = community_locks.find_by(name: name) rescue nil
+
+                  community_locks.each do |community_lock|
+                    community_lock_name = community_lock.name.gsub('-', '').gsub(' ', '')
+                    if (unit_name == community_lock_name)
+                      community_lock.update_attributes(stop_id: floorplate_unit.id, stop_type: "unit", stop_name: floorplate_unit.marketing_name)
+                    end
+                  end
+                else
+                  unit_name = floorplate_unit.marketing_name.gsub('-', '').gsub(' ', '')
+                  community_locks.each do |community_lock|
+                    community_lock_name = community_lock.name.gsub('-', '').gsub(' ', '')
+                    if (unit_name == community_lock_name)
+                      community_lock.update_attributes(stop_id: floorplate_unit.id, stop_type: "unit", stop_name: floorplate_unit.marketing_name)
+                    end
+                  end
+                  # same_name_lock = community_locks.find_by(name: floorplate_unit.marketing_name) rescue nil
+                end
+                # if same_name_lock.present?
+                #   same_name_lock.update_attributes(stop_id: floorplate_unit.id, stop_type: "unit", stop_name: floorplate_unit.marketing_name)
+                # end
+
+              end
+
+            end
+
+          end
 
         end
-
       end
+      flash[:notice] = "Locks maped successfully."
+      render :js => "window.location = '/communities/#{community.id}/dwelos/new'"
+    else
+      flash[:error] = "Something went wrong, please check your credentials."
+        render :js => "window.location = '/communities/#{community.id}/dwelos/new'"
     end
-    flash[:notice] = "Locks maped successfully."
-    render :js => "window.location = '/communities/#{community.id}/dwelos/new'"
   end
 
   private
--- app/services/remote_lock_service.rb
+++ app/services/remote_lock_service.rb
@@ -26,7 +26,7 @@ class RemoteLockService < BaseService
     def get_all_deivces(access_token)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + "/devices"
             
@@ -45,7 +45,7 @@ class RemoteLockService < BaseService
     def get_device(access_token,device_id)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + "/devices/" + device_id 
             
@@ -64,7 +64,7 @@ class RemoteLockService < BaseService
     def update_device(access_token,device_id,updated_device_data)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + "/devices/" + device_id 
             
@@ -88,7 +88,7 @@ class RemoteLockService < BaseService
     def create_access_guest(access_token,tour_user,current_time)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + "/access_persons"
 
@@ -130,7 +130,7 @@ class RemoteLockService < BaseService
     def get_access_guest(access_token,guest_id)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + "/access_persons/" + guest_id 
             
@@ -149,7 +149,7 @@ class RemoteLockService < BaseService
     def delete_access_guest(access_token,guest_id)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + "/access_persons/" + guest_id 
             
@@ -168,7 +168,7 @@ class RemoteLockService < BaseService
     def update_access_guest(access_token,guest_id,tour_user)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + 
             
@@ -198,7 +198,7 @@ class RemoteLockService < BaseService
     def grant_access(access_token, access_person_id, accessible_id,accessible_type)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + "/access_persons/#{access_person_id}/accesses"
 
@@ -224,7 +224,7 @@ class RemoteLockService < BaseService
     def get_all_events(access_token,page)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
             url = page > 1 ? base_url + "/events/?page="+ page.to_s : base_url + "/events" 
             response = HTTParty.get(url,
                 :headers => { 'Authorization' => auth_header} )
@@ -241,7 +241,7 @@ class RemoteLockService < BaseService
         @dwelo_user = Dwelo.first
         if @dwelo_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
             url = "https://api-sandbox.dwelos.com/v4/integrations/pynwheel/events/?access_person_id=" + guest_id
             response = HTTParty.get(url,
                                     :headers => { 'Authorization' => auth_header} )
@@ -259,7 +259,7 @@ class RemoteLockService < BaseService
     def create_igloo_guests(access_token,tour_user,igloo_lock_id,current_time)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + "/igloo_guests"
             response = HTTParty.post(url,
@@ -288,7 +288,7 @@ class RemoteLockService < BaseService
     def delete_igloo_guests(access_token,igloo_guest_id)
         if @edge_state_user.present?
             token_type = "Bearer"
-            auth_header = token_type + " " + access_token
+            auth_header = token_type + " " + access_token rescue ''
 
             url = base_url + "/igloo_guests/" + igloo_guest_id           
             response = HTTParty.delete(url,
