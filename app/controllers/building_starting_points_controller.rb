1273a036f9aa27891d5e39350b700959370adec1
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-10-15 14:02:52 +0500

    added code

--- app/controllers/building_starting_points_controller.rb
+++ app/controllers/building_starting_points_controller.rb
@@ -1,4 +1,5 @@
 class BuildingStartingPointsController < ApplicationController
+  after_filter "previous_url", only: [:edit]
 	def edit
 		@community = Community.find params[:community_id]
     @floors = @community.floorplates.map{|x| x.floors}.flatten!.uniq.sort rescue nil
@@ -12,8 +13,13 @@ class BuildingStartingPointsController < ApplicationController
         if ts.present?
           ts.update_attributes(name: @building_starting_point.name)
         end
-        format.html { redirect_back(fallback_location: community_building_starting_point_path, notice: 'Building starting point was successfully updated.') }
-        format.js { render :show, status: :ok, location: @building_starting_point }
+        if params[:done_action] == "true"
+          format.html { redirect_to( session[:go_back] , notice: "Building starting point was successfully updated.")}
+          format.js { render :show, status: :ok, location: @building_starting_point }
+        else
+          format.html { redirect_back(fallback_location: community_building_starting_point_path, notice: 'Building starting point was successfully updated.') }
+          format.js { render :show, status: :ok, location: @building_starting_point }
+        end
       else
         @floors = @community.floorplates.map{|x| x.floors}.flatten!.uniq.sort rescue nil
         flash[:error] = @building_starting_point.errors.full_messages.join(',')
@@ -22,7 +28,6 @@ class BuildingStartingPointsController < ApplicationController
       end
     end
   end
-
 
   def destroy
     ts = TourStop.find_by(stop_type: "building_starting_point", stop_id: @building_starting_point.id)
@@ -41,4 +46,7 @@ class BuildingStartingPointsController < ApplicationController
   	params.require(:building_starting_point).permit(:name, :x_plot, :y_plot, :community_id, :floor, :building, :directional_text, :access_code)
   end
 
+  def previous_url
+    session[:go_back] = request.referer if request.referer != request.url
+  end
 end
