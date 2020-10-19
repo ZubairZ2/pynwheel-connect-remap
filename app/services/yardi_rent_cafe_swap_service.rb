4b013106a7d688199b69a9623d56b67e6ac3ab79
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-10-19 17:33:36 +0500

    updated with response

--- app/services/yardi_rent_cafe_swap_service.rb
+++ app/services/yardi_rent_cafe_swap_service.rb
@@ -54,8 +54,14 @@ class YardiRentCafeSwapService < BaseService
                 if unit.effective_rent <= 0
                   unit.effective_rent = 1.0
                 end
-                rentStr = yardi_rent_cafe_rent_matrix(api_token, property_code, r["ApartmentName"])
-                unit.lease_pricing = rentStr[0].to_s + ":" + rentStr[1] + "::" +  rentStr[2] + ":" + rentStr[3]
+
+                rentStrs = yardi_rent_cafe_rent_matrix(api_token, property_code, r["ApartmentName"])
+                leasing = ""
+                rentStrs.each do |rentStr|
+                  leasing = leasing + rentStr[1] + ":" + rentStr[0].to_s + "::" +  rentStr[2].split(" ")[0] + ":" + rentStr[3].split(" ")[0] + ';'
+                end
+                unit.lease_pricing = leasing
+
                 unit.save
               else
                 dup = Unit.find_by(community_id: credentials.community_id,provider_unit_id: r["ApartmentId"])
@@ -86,8 +92,14 @@ class YardiRentCafeSwapService < BaseService
                 if unit.effective_rent <= 0
                   unit.effective_rent = 1.0
                 end
-                rentStr = yardi_rent_cafe_rent_matrix(api_token, property_code, r["ApartmentName"])
-                unit.lease_pricing = rentStr[0].to_s + ":" + rentStr[1] + "::" +  rentStr[2] + ":" + rentStr[3]
+
+                rentStrs = yardi_rent_cafe_rent_matrix(api_token, property_code, r["ApartmentName"])
+                leasing = ""
+                rentStrs.each do |rentStr|
+                  leasing = leasing + rentStr[1] + ":" + rentStr[0].to_s + "::" +  rentStr[2].split(" ")[0] + ":" + rentStr[3].split(" ")[0] + ';'
+                end
+                unit.lease_pricing = leasing
+
                 unit.save
               end
 
@@ -226,8 +238,12 @@ class YardiRentCafeSwapService < BaseService
   def yardi_rent_cafe_rent_matrix(api_token, property_code, apartment_name)
     request_type = "pricingmatrix"
     url = "https://api.rentcafe.com/rentcafeapi.aspx?requestType=#{request_type}&APIToken=#{api_token}&propertycode=#{property_code}&ApartmentName=#{apartment_name}"
+    
     response = HTTParty.get(url)
     rent_matrix = JSON.parse(response.body)
-    rent_matrix.map{|r| [r["Rent"].to_i, r["Term"], r["Start_Date"], r["End_Date"]]}.min
+
+    uniq_terms = rent_matrix.map{|x| x["Term"].to_i }.uniq
+    distinct_data = uniq_terms.map{|term| rent_matrix.map{|data| data if data["Term"] == term.to_s}.compact}.compact
+    return distinct_data.map{|data| data.map{|r| [r["Rent"].to_i, r["Term"], r["Start_Date"], r["End_Date"]]}.min}
   end
 end
