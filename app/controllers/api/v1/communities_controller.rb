d98f1d7e4e1346af5f978e5e8a2200b65784e22a
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-10-26 16:28:27 +0500

    issue

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -137,7 +137,7 @@ class Api::V1::CommunitiesController < ActionController::Base
     @building_list = @building_list.map {|i| i.gsub(/\d+/) {|s| "%08d" % s.to_i } }.zip(@building_list).sort.map{|x,y| y}
     @community.tour.building_order.present? ? (@building_list =  @community.tour.building_order) : ""
 
-    @floor_list = @community.floorplates.map{|x| x.floors}.flatten!.uniq.sort rescue nil
+    @floor_list = @community.floorplates.map{|x| x.floors}.flatten!.uniq.sort rescue []
     #####
     @tours = Tour.where(community_id: params[:id])
 
