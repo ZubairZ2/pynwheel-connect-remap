ef797e41253030d03a671f4a0de069d768e4deec
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-11-13 19:26:02 +0500

    updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,10 @@ class HomeController < ApplicationController
   	if current_user.is_super_admin?
     	@communities = Community.select(:id,:name,:updated_at,:company_id,:data_provider).includes(:company)
     elsif current_user.is_dwelo_admin?
-      assigned_communities_ids = current_user.communities.ids
-      dwelo_communities_ids = Community.where(creator_id: User.all.map{|u| u.id if u.role == "Dwelo admin"}.compact).ids
-      ids = (assigned_communities_ids + dwelo_communities_ids).uniq
+      assigned_communities_ids = current_user.communities.ids # all assinged communities
+      dwelo_communities_ids = Community.where(creator_id: User.where(role: "Dwelo admin").ids).ids # all communities created by any dwelo admin
+      dwelo_companies_communities = Community.joins(:company).where(companies: {creator_id: User.where(role: "Dwelo admin").ids}).ids # all communities under dwelo_companies (either created by dwelo_admin or super_admin)
+      ids = (assigned_communities_ids + dwelo_communities_ids + dwelo_companies_communities).uniq
       @communities = Community.where(id: ids)
     else
     	@communities = current_user.communities
