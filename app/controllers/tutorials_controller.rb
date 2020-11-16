7e5f3af8f1a7350977c7826a7c2d5cd7602d3dfb
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-11-16 13:49:23 +0500

    changes

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -16,9 +16,9 @@ class TutorialsController < ApplicationController
   	@tutorial = Tutorial.find params[:id]
     # @tutorial.video_type = ([params[:video_type1].present? ? "pynwheel_touch" : nil ] + [params[:video_type2].present? ? "pynwheel_maps" : nil ] + [ params[:video_type3].present? ? "self_tour" : nil]).join(',') 
   	if @tutorial.update(tutorial_params)
-  		redirect_to edit_community_tutorial_path(@community,@tutorial), :notice => "Tutorial has been updated"
+  		redirect_to community_tutorials_path(current_community), :notice => "Tutorial has been updated"
   	else
-  		redirect_to edit_community_tutorial_path(@community,@tutorial), :notice => @tutorial.errors
+  		redirect_to community_tutorials_path(current_community), :notice => @tutorial.errors
   	end
   	
   end
@@ -44,7 +44,7 @@ class TutorialsController < ApplicationController
   	@tutorial = Tutorial.new(tutorial_params)
     # @tutorial.video_type = ([params[:video_type1].present? ? "pynwheel_touch" : nil ] + [params[:video_type2].present? ? "pynwheel_maps" : nil ] + [ params[:video_type3].present? ? "self_tour" : nil]).join(',') 
   	if @tutorial.save
-  		redirect_to edit_community_tutorial_path(@community,@tutorial), :notice => "Tutorial has been updated"
+  		redirect_to community_tutorials_path(current_community), :notice => "Tutorial has been updated"
   	else
   		render "new"
   	end
