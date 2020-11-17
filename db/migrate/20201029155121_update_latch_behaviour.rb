5b5d6995d840c7808fd998c14f0578824715bdfc
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-11-17 16:09:45 +0500

    migration updated latch v6 live

--- db/migrate/20201029155121_update_latch_behaviour.rb
+++ db/migrate/20201029155121_update_latch_behaviour.rb
@@ -1,6 +1,5 @@
 class UpdateLatchBehaviour < ActiveRecord::Migration[5.0]
     def change
-      drop_table :latch_allowed_accesses
       add_reference :latch_guests, :guest_of_stop, polymorphic: true
     end
   end
