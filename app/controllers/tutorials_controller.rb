39f6f023b51e7ad602e3e088c3dcfb187754145c
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-12-02 13:25:29 +0500

    change

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,6 @@
 class TutorialsController < ApplicationController
   def index
-  	@tutorials = Tutorial.where('pynwheel_touch = ? AND pynwheel_maps = ? AND self_tour = ?', (@community.touchscreen_app.present? ? (@community.touchscreen_app) : false) , (@community.show_map.present? ? (@community.show_map) : false) ,(@community.self_tour.present? ? (@community.self_tour) : false))
+  	@tutorials = Tutorial.where('pynwheel_touch = ? OR pynwheel_maps = ? OR self_tour = ?', (@community.touchscreen_app.present? ? (@community.touchscreen_app) : false) , (@community.show_map.present? ? (@community.show_map) : false) ,(@community.self_tour.present? ? (@community.self_tour) : false))
   end
   def new
   	@tutorial = Tutorial.new
