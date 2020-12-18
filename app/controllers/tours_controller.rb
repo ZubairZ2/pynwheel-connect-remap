f9604160de503ebb048098b3499f570d120aff4e
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2020-12-18 17:55:17 +0500

    almost done

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -354,9 +354,11 @@ class ToursController < ApplicationController
     @community_code = (JWT.encode ({"community_id" => @community.id}), ENV['SECRET_KEY_BASE_v2'], 'HS256')
   end
   def save_schedule_widget_btn_setting
-    @schedule_widget_setting = @community.tour.scheduler_widget_setting
-    @schedule_widget_setting.update_attributes(btn_text: params[:btn_text], btn_font: params[:btn_font], btn_font_size: params[:btn_font_size], btn_color: params[:btn_color])
     
+    @schedule_widget_setting = @community.tour.scheduler_widget_setting
+    @schedule_widget_setting.update_attributes(btn_text: params[:btn_text], btn_font: params[:btn_font], btn_font_size: params[:btn_font_size], btn_color: params[:btn_color], btn_width: params[:btn_width].to_i, btn_height: params[:btn_height].to_i)
+    flash[:notice] = "Tour settings updated successfully."
+    redirect_to settings_community_tours_path(@community)
   end
   def ajaxplotstartingpoint
     tour = Tour.find params[:tour_id]
--- db/migrate/20201127073818_create_scheduler_widget_settings.rb
+++ db/migrate/20201127073818_create_scheduler_widget_settings.rb
@@ -3,7 +3,9 @@ class CreateSchedulerWidgetSettings < ActiveRecord::Migration[5.0]
     create_table :scheduler_widget_settings do |t|
       t.string :btn_text, default: "Schedule a Visit"
       t.string :btn_color, default: "#20a345"
-      t.string :btn_font, default: "ms-appx:/DesignTemplates/Expressionist/CutomFonts/OpenSans-Regular.ttf#Open Sans"
+      t.integer :btn_width, default: "130"
+      t.integer :btn_height, default: "35"
+      t.string :btn_font, default: "Open Sans Regular"
       t.string :btn_font_size, default: "14px"
       t.references :tour, foreign_key: true
 
