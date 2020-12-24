8dc93c1cfb47ba6c65d95d7d4bdc2efdeb461aa3
Hamza Shamshad <hamza.shamshad@intagleo.com>
2020-12-24 14:33:49 +0500

    updated that was direct in staging

--- app/services/salesforce_services/tour_feedback.rb
+++ app/services/salesforce_services/tour_feedback.rb
@@ -50,7 +50,7 @@ module SalesforceServices
                             stop_detail[:stopName] = stop.name.present? ? stop.name : ((stop.stop_type.classify.constantize.find_by_id stop.stop_id).name rescue "")
                             stop_detail[:notes] = VisitedStop.where(tour_id: community.tour.id, tour_user_id: tour_user.id, tour_key: sf_user.tour_key, tour_stop_id: stop_id).pluck(:description).compact
                             stop_detail[:photos] = VisitedStop.where(tour_id: community.tour.id, tour_user_id: tour_user.id, tour_key: sf_user.tour_key, tour_stop_id: stop_id).pluck(:image).compact.count
-                            stop_detail[:chatHistory] = Chatroom.where(tour_id: community.tour.id, tour_user_id: tour_user.id).chats.where(tour_key: sf_user.tour_key).order(:id).map{|c| (c.name == "Support Team" ? ("Support:" + c.message + " :: ") : ("User:" + c.message + " :: "))}.flatten.join("") rescue ""
+                            stop_detail[:chatHistory] = Chatroom.find_by(tour_id: community.tour.id, tour_user_id: tour_user.id).chats.where(tour_key: sf_user.tour_key).order(:id).map{|c| (c.name == "Support Team" ? ("Support:" + c.message + " :: ") : ("User:" + c.message + " :: "))}.flatten.join("") rescue ""
                         rescue => exception
                             next
                         else
@@ -85,7 +85,7 @@ module SalesforceServices
                     rescue HTTParty::Error => e
                         OpenStruct.new({success?: false, error: e, payload: nil})
                     else
-                        unless response[:errorCode].present?
+                        if response.code == "200"
                             OpenStruct.new({success?: true, error: nil, payload: response})
                         else
                             OpenStruct.new({success?: false, error: response, payload: nil})
