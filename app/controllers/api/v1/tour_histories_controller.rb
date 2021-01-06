21e66a1523fcc701774dba456a17d9f3531b2e59
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2021-01-06 14:27:43 +0500

    issue

--- app/controllers/api/v1/tour_histories_controller.rb
+++ app/controllers/api/v1/tour_histories_controller.rb
@@ -21,9 +21,9 @@ class Api::V1::TourHistoriesController < ActionController::Base
         tour_history.tour_user_id = params[:tour_user_id]
         @tour = Tour.find params[:tour_id]
         tu = TourUser.find params[:tour_user_id]
-        @tour_history.is_virtual_tour = tu.is_virtual_tour rescue nil
-        @tour_history.latitude = tu.latitude rescue nil
-        @tour_history.longitude = tu.longitude rescue nil
+        tour_history.is_virtual_tour = tu.is_virtual_tour rescue nil
+        tour_history.latitude = tu.latitude rescue nil
+        tour_history.longitude = tu.longitude rescue nil
         if !@tour.visual_id_verification
           begin
             tu = TourUser.find params[:tour_user_id]
