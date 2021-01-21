b9eb041de79b846e5a164e8a16dd55da622a6296
Hamza Shamshad <hamza.shamshad@intagleo.com>
2021-01-21 14:34:08 +0500

    puts in live

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -469,6 +469,9 @@ class Api::V1::CommunitiesController < ActionController::Base
           if @community.credential.present? and @community.credential.crm_provider == "salesforce"
             if @community.id == 1240 # only enabled for "Metropolitan" for testing from Prometheus-salesforce
             response = SalesforceServices::GetBookingByNeighbor.call(community: @community, tour_user: @tour_user)
+            puts "###### ------------------------- #############"
+            puts response
+            puts "###### ------------------------- #############"
             if response.success?
               @scheduled_tours = response.payload.find_all{ |b| ( (b["Account__r"]["Name"].downcase.parameterize.gsub("-", "").gsub("_", "") == @community.name.downcase.parameterize.gsub("-", "").gsub("_", "")) and b["Status__c"] == "Scheduled" and b["Tour_Start_Time__c"].to_datetime.in_time_zone(timezone).strftime("%Y-%m-%d") == Time.now.in_time_zone(timezone).strftime("%Y-%m-%d")) }
               if @scheduled_tours.present?
