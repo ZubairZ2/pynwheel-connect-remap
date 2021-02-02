03070b698190c93abccddc9826896987bd6ec397
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2021-02-02 19:50:03 +0500

    change

--- db/migrate/20210202113347_add_portico_version_field_to_app_versions.rb
+++ db/migrate/20210202113347_add_portico_version_field_to_app_versions.rb
@@ -1,5 +1,5 @@
 class AddPorticoVersionFieldToAppVersions < ActiveRecord::Migration[5.0]
   def change
-    add_column :app_versions, :portico_version, :string
+    add_column :app_versions, :portico_version, :string, default: "1.0.0"
   end
 end
