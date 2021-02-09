cbc432fd659a51a7b7428596a4fa1413851a4e5b
Hamza Shamshad <hamza.shamshad@intagleo.com>
2021-02-09 19:55:40 +0500

    reverted last commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -101,21 +101,18 @@ class ApplicationController < ActionController::Base
   end
  
   def load_tour_users_chats
-    if current_user.present? and @community.present? and @community.chat_control and @community.tour.present?
-
-        if current_user.is_super_admin?
-          chat_communities = Community.joins(:community_users).where(community_users: {chat_enable: true}).includes(:tour)
-          @chatrooms = Chatroom.where(tour_id: chat_communities.map{|c| c.tour.id if c.tour.present?}).includes(:chats, :tour, :tour_user)
-          @listening_channels = Community.joins(:community_users).where(community_users: {chat_enable: true}).uniq.map{|c| (c.name + "_with_id_" + c.id.to_s).parameterize.gsub("-", "").gsub("_", "")}
-          @notifications =  @chatrooms.map{ |chatroom| notifications_by_chatroom_super_admin(chatroom) }
-        else
-          chat_enabled_communities = current_user.communities.where(community_users: {chat_enable: true}).includes(:tour)
-          @chatrooms = Chatroom.where(tour_id: chat_enabled_communities.map{|c| c.tour.id}).includes(:chats, :tour, :tour_user)
-          @listening_channels = chat_enabled_communities.map{|c| (c.name + "_with_id_" + c.id.to_s).parameterize.gsub("-", "").gsub("_", "")}
-          @notifications =  @chatrooms.map{ |chatroom| notifications_by_chatroom(@community, chatroom) }
-        end
+    # below code will not be executed if call made from browser is ajax
+    # request.xhr? => returns numeric or nil values not BOOLEAN values and 
+    # it works with unless condition as suited with our case
+    unless request.xhr?
+      if current_user.present? and @community.present? and @community.chat_control and @community.tour.present?
+        chat_enabled_communities = current_user.communities.where(community_users: {chat_enable: true}).uniq.includes(:tour)
+        @chatrooms = Chatroom.where(tour_id: chat_enabled_communities.map{|c| c.tour.id if c.tour.present?}).includes(:chats, :tour, :tour_user)
+        @listening_channels = chat_enabled_communities.map{|c| (c.name + "_with_id_" + c.id.to_s).parameterize.gsub("-", "").gsub("_", "")}
+        @notifications =  @chatrooms.map{ |chatroom| notifications_by_chatroom(@community, chatroom) }
         @chatroom_list = @chatrooms.map{|c| c.id}
         @default_user_image =  "/assets/chat-tour-user.jpg"
+      end
     end
   end
   
@@ -132,11 +129,6 @@ class ApplicationController < ActionController::Base
       min_count=0
     end 
     [chatroom.id , min_count]
-  end
-  
-  def notifications_by_chatroom_super_admin(chatroom)
-    count = Chat.where("chatroom_id = ? AND  name != ? ", chatroom.id, "Support Team").unread_by(current_user).count rescue 0
-    [chatroom.id , count]
   end
 
   def alphabetical_sort(company_or_community_or_community_groups)
