bc2e69ec620a0c35bf8429369d7b41ac8884e691
asrar.ahmad <asrar.ahmad@intagleo.com>
2021-02-18 16:27:17 +0500

    update and add migrate

--- db/migrate/20210209120418_community_locks_updates.rb
+++ db/migrate/20210209120418_community_locks_updates.rb
@@ -6,8 +6,6 @@ class CommunityLocksUpdates < ActiveRecord::Migration[5.0]
   	add_column :elevators, :lock_provider, :string, default: ""
   	add_column :tours, :lock_provider, :string, default: ""
     add_column :communities, :multiple_locks_provider, :string, array: true, default: []
-    add_column :tours, :access_code, :string
-    add_column :elevators, :access_code, :string
   end
 
   def down
@@ -17,7 +15,6 @@ class CommunityLocksUpdates < ActiveRecord::Migration[5.0]
   	remove_column :elevators, :lock_provider, :string, default: ""
   	remove_column :tours, :lock_provider, :string, default: ""
     remove_column :communities, :multiple_locks_provider, :string, array: true, default: []
-    remove_column :tours, :access_code, :string
-    remove_column :elevators, :access_code, :string
   end
+  
 end
