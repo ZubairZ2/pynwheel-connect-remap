f84998e6df33cd503a113fbf88ffe757ffc4dd3d
asrar.ahmad <asrar.ahmad@intagleo.com>
2021-02-25 23:53:45 +0500

    its also update in staging as well

--- app/helpers/assign_locks_helper.rb
+++ app/helpers/assign_locks_helper.rb
@@ -5,6 +5,8 @@ module AssignLocksHelper
 
             if lock_id.present?
                 latch_lock = LatchLock.find_by(door_uuid: lock_id, latch_id: community.latch.id) rescue nil
+                latch_lock.stop.update_column(:lock_provider, "") rescue nil
+                
                 if latch_lock.present? and stop.latch_locks.present? and stop.latch_locks.last.door_uuid != latch_lock.door_uuid
                     stop.latch_locks.update_all(stop_id: nil, stop_type: nil)
                     latch_lock.update_attributes(stop_id: stop.id, stop_type: stop.class.name) rescue nil
@@ -19,6 +21,8 @@ module AssignLocksHelper
 
             if lock_id.present?
                 remote_lock = stop.lock_provider.constantize.find_by(community_id: community.id).remote_locks.dwelo_locks.find_by(device_id: lock_id) rescue nil
+                remote_lock.stop_type.classify.constantize.find(remote_lock.stop_id).update_column(:lock_provider, "") rescue nil
+                
                 if remote_lock.present? and stop.remote_locks.dwelo_locks.present? and stop.remote_locks.dwelo_locks.last.device_id != remote_lock.device_id
                   stop.remote_locks.dwelo_locks.update_all(stop_id: nil, stop_type: nil, stop_name: nil)
                   remote_lock.update_attributes(stop_id: stop.id, stop_type: stop.class.name.snakecase, stop_name: stop.name) rescue nil
@@ -32,6 +36,8 @@ module AssignLocksHelper
 
             if lock_id.present?
                 remote_lock = stop.lock_provider.constantize.find_by(community_id: community.id).remote_locks.edgestate_locks.find_by(device_id: lock_id) rescue nil
+                remote_lock.stop_type.classify.constantize.find(remote_lock.stop_id).update_column(:lock_provider, "") rescue nil
+
                 if remote_lock.present? and stop.remote_locks.edgestate_locks.present? and stop.remote_locks.edgestate_locks.last.device_id != remote_lock.device_id
                   stop.remote_locks.edgestate_locks.update_all(stop_id: nil, stop_type: nil, stop_name: nil)
                   remote_lock.update_attributes(stop_id: stop.id, stop_type: stop.class.name.snakecase, stop_name: stop.name) rescue nil
@@ -45,6 +51,8 @@ module AssignLocksHelper
 
             if lock_id.present?
                 zerv_lock = ZervLock.find_by(mac_id: lock_id, zerv_id: community.zerv.id) rescue nil
+                zerv_lock.stop.update_column(:lock_provider, "") rescue nil
+              
                 if zerv_lock.present? and stop.zerv_locks.present? and stop.zerv_locks.last.mac_id != zerv_lock.mac_id
                     stop.zerv_locks.update_all(stop_id: nil, stop_type: nil)
                     zerv_lock.update_attributes(stop_id: stop.id, stop_type: stop.class.name) rescue nil
