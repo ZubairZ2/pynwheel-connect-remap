cf7fd8fd64153defeb060d0f4922663a6da83dcc
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2021-02-25 20:18:15 +0500

    check issue

--- app/helpers/tours_helper.rb
+++ app/helpers/tours_helper.rb
@@ -4,11 +4,11 @@ module ToursHelper
 			app_version = AppVersion.first
 			PorticoRequest.create(app_name: params[:appName], user_id: params[:tour_user_id], os_type: params[:appPlatform], app_version: params[:version].to_i)
 			app_version.update_attributes(portico_version: params[:version].to_i) if (params[:version].to_i > app_version.portico_version)
-			if params[:version].to_i < ENV["MINIMUM_SUPPORTED_VERSION"].to_i
-				false
-			else
-				true
-			end
+			if params[:version].to_i >= app_version.supported_version.to_i
+        true
+      else
+        false
+      end
 		else
 			true
 		end
