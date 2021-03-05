a4503ef90a9dabf1cf61b843d70e4c44310bf282
Muhammad Umer Sani <muhammad.umer@intagleo.com>
2021-03-05 18:14:24 +0500

    .last issue

--- app/controllers/api/v1/tour_histories_controller.rb
+++ app/controllers/api/v1/tour_histories_controller.rb
@@ -85,7 +85,7 @@ class Api::V1::TourHistoriesController < ActionController::Base
 
         check_length_stay(params[:tour_history_id], params[:lengthy_stay], tour.community, tu, params[:current_stop_id]) if (params[:tour_history_id].present? and params[:lengthy_stay].present?)
         chat_control = (tour.community.chat_control and tour.community.is_chat_available) ? tour.community.chat_control : false
-        has_tour_user_left(tour.community, tu, params[:lat_langs].last)
+        has_tour_user_left(tour.community, tu, params[:lat_langs].last) if params[:lat_langs].present?
         chatroom = Chatroom.find_by(tour_user_id: params[:tour_user_id], tour_id: params[:tour_id])
         if chatroom.present?
           if params[:last_msg_id].present?
