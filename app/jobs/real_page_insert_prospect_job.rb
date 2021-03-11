82458674879a74dbf71f5b1ac80be3d3cbc21e37
Hamza Shamshad <hamza.shamshad@intagleo.com>
2021-03-11 11:17:21 -0500

    updated realpage error Jennifer faced in call

--- app/jobs/real_page_insert_prospect_job.rb
+++ app/jobs/real_page_insert_prospect_job.rb
@@ -1,9 +1,9 @@
 class RealPageInsertProspectJob < ApplicationJob
     include SuckerPunch::Job
   
-    def perform(credentials, tour_user, appointment_time, marketing_source, desired_move_in_date)          # coming form scheduler widger, we are passing move-in-date for tour_time
+    def perform(credentials, tour_user, appointment_time, marketing_source, desired_move_in_date, community)          # coming form scheduler widger, we are passing move-in-date for tour_time
         real_page_insert_prospect_service = RealPageInsertProspectService.new(JSON.parse(credentials))
-        real_page_insert_prospect_service.perform(tour_user, appointment_time, marketing_source, desired_move_in_date)
+        real_page_insert_prospect_service.perform(tour_user, appointment_time, marketing_source, desired_move_in_date, community)
     end
 end
   
