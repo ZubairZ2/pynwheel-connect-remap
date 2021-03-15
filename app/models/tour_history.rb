3e9c2e783f38ba1272b9b2a3b803feda7a782199
Hamza Shamshad <hamza.shamshad@intagleo.com>
2021-03-15 15:46:47 +0500

    updated realpage guest card background job in getting visited stops marketing names

--- app/models/tour_history.rb
+++ app/models/tour_history.rb
@@ -325,13 +325,14 @@ class TourHistory < ApplicationRecord
 	end
 
 	def stop_marketing_names_visited_by_user
+    sleep 2
 		visited_stops = []
 
-		current_tour = VisitedStop.where(tour_key:  self.tour_user.tour_key)
+		current_tour = VisitedStop.where(tour_key:  self.tour_user.tour_key).order("id DESC").first
 		tour_key = current_tour.tour_key if current_tour.present?
 
 		if tour_key.present?
-			tour_stop_ids = VisitedStop.where(tour_key:  self.tour_user.tour_key).pluck(:tour_stop_id)
+			tour_stop_ids = VisitedStop.where(tour_key:  tour_key).pluck(:tour_stop_id)
 			unit_stops = TourStop.where(id: tour_stop_ids, stop_type: "unit").pluck(:stop_id)
 
       puts "unit_stops"
