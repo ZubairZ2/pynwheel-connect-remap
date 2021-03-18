633940e641aa9e22e93567cc11bc46214e5fd3a5
asrar.ahmad <asrar.ahmad@intagleo.com>
2021-03-18 16:18:24 +0500

    analytics view side changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,6 +56,7 @@
 //= require jquery.multi-select 
 //= require tags
 //= require multiple_tabs
+//= require Chart.min
 //made changes in jquery.multi-select according to our requirement in at library at line #488. Commented sanitize function
 // require_tree .
 //= require bootstrap-wysihtml5
--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,7 +1,19 @@
 class AnalyticsController < ApplicationController
   
   def index
-  	
+  	arr = [65, 59, 80, 81, 56, 55, 40]
+  	@data = {
+		  labels: ["January", "February", "March", "April", "May", "June"],
+		  datasets: [
+		    {
+		        label: "My First dataset",
+		        backgroundColor: "rgba(60,141,188,1)",
+		        borderColor: "rgba(220,220,220,1)",
+		        data: arr
+		    }
+		  ]
+		}
+		@options = { }
   end
 
 end
