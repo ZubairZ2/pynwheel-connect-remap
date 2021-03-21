08732ae56bca63e1808c6e52d57ba672d9f3d239
asrar.ahmad <m.khabbab@intagleo.co.uk>
2021-03-22 04:35:17 +0500

    For first 2 without filters

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,21 +1,103 @@
 class AnalyticsController < ApplicationController
-  
+  include AnalyticsHelper
   def index
-  	@track_session_count = TrackSession.count
-  	
-  	arr = [65, 59, 80, 81, 56, 55, 40]
-  	@data = {
-		  labels: ["January", "February", "March", "April", "May", "June"],
-		  datasets: [
-		    {
-		        label: "My First dataset",
-		        backgroundColor: "rgba(60,141,188,1)",
-		        borderColor: "rgba(220,220,220,1)",
-		        data: arr
-		    }
-		  ]
-		}
-		@options = { legend: {display: false} }
+
+    start_date , end_date = Date.today - 9.day , Date.today + 2.day
+    @days_count = return_total_days(start_date, end_date)
+    total_records = TrackSession.where.not(end_datetime: nil).where('start_datetime BETWEEN ? AND ?',start_date.yesterday, end_date.tomorrow)
+    collect_session_each_day_data(start_date, @days_count, total_records) # For Maps first bar graph
+    collect_session_each_day_data_in_minutes(start_date, @days_count, total_records) # For Maps second line graph
+
   end
 
+  private
+
+    def collect_session_each_day_data(start_date, days_count, total_records)
+
+      sessions_each_day_hash = return_empty_hash(days_count,start_date)
+
+      records_start_date = total_records.pluck(:start_datetime).map(&:to_date)
+      uniq_start_date = records_start_date.uniq
+      uniq_start_date_size = uniq_start_date.size
+
+      uniq_start_date_size.times do |i|
+        sessions_each_day_hash[uniq_start_date[i]] = records_start_date.count(uniq_start_date[i])
+        records_start_date = records_start_date - [uniq_start_date[i]]
+      end
+
+      @track_session_count = total_records.count
+      @avg_track_session = total_records.count / days_count
+      session_each_day_labels = sessions_each_day_hash.keys.map(&:to_s)
+      session_each_day_counts = sessions_each_day_hash.values
+    
+      make_sessions_graph(session_each_day_labels, session_each_day_counts)
+
+    end
+
+    def collect_session_each_day_data_in_minutes(start_date, days_count, total_records)
+      
+      sessions_each_day_hash = return_empty_hash(days_count,start_date)
+      start_end_datetime_arr = total_records.pluck(:start_datetime, :end_datetime)
+
+      records_start_date = total_records.pluck(:start_datetime).map(&:to_date)
+      uniq_start_date = records_start_date.uniq
+      uniq_start_date_size = uniq_start_date.size
+
+      total_minutes , total_count = 0, 0
+      uniq_start_date_size.times do |i|
+        minutes = 0
+        count = 0
+        remove_index = []
+        start_date_str = uniq_start_date[i].strftime("%y:%m:%d")
+        start_end_datetime_arr.each_with_index do |arr, ind|
+          if arr.first.strftime("%y:%m:%d") == start_date_str
+            mins = return_time_in_minutes(arr.first,arr.last)
+            minutes += mins
+            total_minutes += mins
+            count += 1
+            total_count += 1
+            remove_index << ind
+          end
+        end
+        sessions_each_day_hash[uniq_start_date[i]] = minutes / count
+        records_start_date = records_start_date - [uniq_start_date[i]]
+        remove_index.each_with_index {|removing_index,j| start_end_datetime_arr.delete_at(removing_index - j) }
+      end    
+
+      @average_duration_each_session_in_minutes = total_minutes / total_count
+      session_each_day_labels = sessions_each_day_hash.keys.map(&:to_s)
+      session_each_day_counts = sessions_each_day_hash.values
+
+      make_sessions_minutes_graph(session_each_day_labels, session_each_day_counts)
+    end
+
+    def make_sessions_graph(session_each_day_labels, session_each_day_counts)
+      @session_each_day_data = {
+        labels: session_each_day_labels,
+        datasets: [
+          {
+              label: "Total Sessions",
+              backgroundColor: "rgba(60,141,188,1)",
+              borderColor: "rgba(220,220,220,1)",
+              data: session_each_day_counts
+          }
+        ]
+      }
+      @session_each_day_options = { legend: {display: false} }
+    end
+
+    def make_sessions_minutes_graph(session_each_day_labels, session_each_day_counts)
+      @session_each_day_minutes_data = {
+        labels: session_each_day_labels,
+        datasets: [
+          {
+              label: "Sessions in minutes",
+              backgroundColor: "rgba(60,141,188,1)",
+              borderColor: "rgba(220,220,220,1)",
+              data: session_each_day_counts
+          }
+        ]
+      }
+      @session_each_day_minutes_options = { legend: {display: false} }
+    end
 end
--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,2 +1,27 @@
 module AnalyticsHelper
+
+  def return_total_days(start_date,end_date)
+    start_date = Time.new(start_date.strftime("%Y"), start_date.strftime("%m"), start_date.strftime("%d"))
+    end_date = Time.new(end_date.strftime("%Y"), end_date.strftime("%m"), end_date.strftime("%d"))
+    seconds = (end_date - start_date).to_i
+    dd, hh = seconds.divmod(60*60*24)
+    return dd;
+  end
+
+  def return_time_in_minutes(start_datetime,end_datetime)
+    start_datetime = Time.new(start_datetime.strftime("%Y"), start_datetime.strftime("%m"), start_datetime.strftime("%d"),start_datetime.strftime("%H"), start_datetime.strftime("%M"), start_datetime.strftime("%S"))
+    end_datetime = Time.new(end_datetime.strftime("%Y"), end_datetime.strftime("%m"), end_datetime.strftime("%d"),end_datetime.strftime("%H"), end_datetime.strftime("%M"), end_datetime.strftime("%S"))
+    seconds = (end_datetime - start_datetime).to_i
+    mm, ss = seconds.divmod(60)
+    return mm;
+  end
+
+  def return_empty_hash(days_count,start_date)
+    h = {}
+    days_count.times do |i|
+      h[start_date + (i.day)] = 0
+    end
+    return h
+  end
+
 end
