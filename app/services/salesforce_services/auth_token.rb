199cdc615beffc3eeb5803a3647ec49e022c03af
Hamza Sohail <hamza.sohail@intagleo.com>
2021-04-07 12:34:18 +0500

    Oauth API fix for sales force

--- app/services/salesforce_services/auth_token.rb
+++ app/services/salesforce_services/auth_token.rb
@@ -2,16 +2,8 @@ module SalesforceServices
     class AuthToken < SalesforceServices::BaseService
 
         def execute(args)
-            url = "https://prometheusreg.my.salesforce.com/services/oauth2/token"
-            response = HTTParty.post(url,
-                body: {
-                    username: @sales_force.salesforce_username,
-                    password: @sales_force.salesforce_password,
-                    client_id: @sales_force.salesforce_client_id,
-                    client_secret: @sales_force.salesforce_secret_id,
-                    grant_type: "password"
-                }
-            )
+            url = "https://prometheusreg.my.salesforce.com/services/oauth2/token?client_id=#{@sales_force.salesforce_client_id}&client_secret=#{@sales_force.salesforce_secret_id}&username=#{@sales_force.salesforce_username}&password=#{@sales_force.salesforce_password}&grant_type=password"
+            response = HTTParty.post(url, body: {})
             puts "---"*50
             puts url
             puts response
