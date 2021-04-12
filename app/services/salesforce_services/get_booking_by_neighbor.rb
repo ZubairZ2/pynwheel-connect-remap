0968bf8c2e1a7731a1d8f4174d2a6ddf880a54d2
Salahudin Sallu <salahudin@intagleo.com>
2021-04-12 23:15:28 +0500

    Puts for testing

--- app/services/salesforce_services/get_booking_by_neighbor.rb
+++ app/services/salesforce_services/get_booking_by_neighbor.rb
@@ -17,8 +17,7 @@ module SalesforceServices
                                     'Content-Type' => 'application/json' }
                     )
                     
-                    sleep(10)
-                rescue HTTParty::Error => e
+                    rescue HTTParty::Error => e
                     OpenStruct.new({success?: false, error: e, payload: nil})
                 else
                     if response.code == "200" or response.code == 200
