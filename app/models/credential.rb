9d4b0ee22c0d2808c28231907db4809835edad1e
Umer Sani <muhammad.umer@intagleo.com>
2021-05-05 11:24:05 +0500

    remove code

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -187,9 +187,6 @@ class Credential < ApplicationRecord
     if self.use_different_crm_provider && community.crm_credential.crm_provider == "salesforce"
       self.community.tour.update_columns(only_scheduled_tour: true)
       self.community.update_columns(scheduler_widget: false)
-    else
-      self.community.tour.update_columns(only_scheduled_tour: false)
-      self.community.update_columns(scheduler_widget: false)
     end
   end
 end
