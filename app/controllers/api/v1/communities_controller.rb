dbf18c50b147e2a8ecf98ee4e98c7f40d8763731
Umer Sani <muhammad.umer@intagleo.com>
2021-05-06 11:25:57 +0500

    code was removed

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -185,6 +185,7 @@ class Api::V1::CommunitiesController < ActionController::Base
       @community.save
       charge_for_id_verfication(@tour_user, 200) if (do_verfication params[:verfied_by_provider], @community)
       @tour_user.verified_by = params[:verfied_by_provider]
+      @tour_user.lock_access_time = Time.now.utc
       @tour_user.save
       
       unless @tour_user.email == "Removed at Consumer Request"
