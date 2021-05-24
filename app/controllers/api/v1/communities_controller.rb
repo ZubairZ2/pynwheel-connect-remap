6e4dffa07e887e8ac7af443f86b51143d5ae78b6
asrar.ahmad <asrar.ahmad@intagleo.com>
2021-05-24 12:41:43 +0500

    apply check of site map true for temporary

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -95,7 +95,7 @@ class Api::V1::CommunitiesController < ActionController::Base
   end
 
   def list_communities
-    @communities = Community.where(touchscreen_app: true).select(:id,:name,:company_id,:locked,:latitude,:longitude,:address,:logo,:state,:city).includes(:company)
+    @communities = Community.where(touchscreen_app: true, is_sitemap: true).select(:id,:name,:company_id,:locked,:latitude,:longitude,:address,:logo,:state,:city).includes(:company)
   end
 
   def portico_list_communities
