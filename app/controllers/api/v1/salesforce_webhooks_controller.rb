ef46f2d97f5cd032579d9b582a8aaac25dfe0a17
Salahudin Sallu <salahudin@intagleo.com>
2021-06-22 13:55:52 +0500

    Saleforce tour response

--- app/controllers/api/v1/salesforce_webhooks_controller.rb
+++ app/controllers/api/v1/salesforce_webhooks_controller.rb
@@ -5,5 +5,6 @@ class Api::V1::SalesforceWebhooksController < ActionController::Base
     puts "Salesforce Webhook"
     puts params.inspect
     puts "------------------------------"*50
+    render :json => {:success=>true, :message => "Salesforce tour submitted successfully", :status => 200}
   end
 end
