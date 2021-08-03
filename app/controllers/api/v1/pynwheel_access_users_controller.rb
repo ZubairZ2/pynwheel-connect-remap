60bafbd459b5081a4dd42d6c035856b9bc406970
Salahudin Sallu <salahudin@intagleo.com>
2021-08-03 19:23:14 +0500

    Access token within user json

--- app/controllers/api/v1/pynwheel_access_users_controller.rb
+++ app/controllers/api/v1/pynwheel_access_users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::PynwheelAccessUsersController < ActionController::Base
   def pynwheel_access_user_authentication
     if @pynwheel_access_user.present?
       if @pynwheel_access_user.is_verified
-        render json: {message: "Varified User", success_code: 200, status: true, access_token: encode_jwt_token(@pynwheel_access_user)}
+        @access_token = encode_jwt_token(@pynwheel_access_user)
       else
         render json: {message: "Non Varified User", success_code: 404, status: false}
       end
@@ -49,11 +49,9 @@ class Api::V1::PynwheelAccessUsersController < ActionController::Base
     if @pynwheel_access_user.present?
       if @pynwheel_access_user.pin_code == params[:pin_code]
         verify_user(true)
-
-        render json: {message: "Pynwheel access user is verified successfully", success_code: 200, status: true, access_token: encode_jwt_token(@pynwheel_access_user), user_data: @pynwheel_access_user}
+        @access_token = encode_jwt_token(@pynwheel_access_user)
       else
         verify_user(false)
-
         render json: {message: "OTP is wrong or expired", success_code: 404, status: false}
       end
     else
---
+++ app/views/api/v1/pynwheel_access_users/verify_otp.json.jbuilder
@@ -0,0 +1,18 @@
+json.status true
+json.success_code 200
+json.message "Pynwheel access user is verified successfully"
+json.user_data {
+  json.access_token @access_token
+  json.id @pynwheel_access_user.id
+  json.name @pynwheel_access_user.name
+  json.first_name @pynwheel_access_user.first_name
+  json.last_name @pynwheel_access_user.last_name
+  json.email @pynwheel_access_user.email
+  json.phone_number @pynwheel_access_user.phone_number
+  json.user_type @pynwheel_access_user.user_type
+  json.move_in_date @pynwheel_access_user.move_in_date
+  json.move_out_date @pynwheel_access_user.move_out_date
+  json.lease_in_date @pynwheel_access_user.lease_in_date
+  json.lease_out_date @pynwheel_access_user.lease_out_date
+  json.is_varified @pynwheel_access_user.is_verified
+}
