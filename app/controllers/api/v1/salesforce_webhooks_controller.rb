5fc2d5a0373b6d5467f3957940c9a93849634379
abubakar <abubakar@intagleo.com>
2021-09-10 01:14:54 +0500

    Fixed exception

--- app/controllers/api/v1/salesforce_webhooks_controller.rb
+++ app/controllers/api/v1/salesforce_webhooks_controller.rb
@@ -24,7 +24,7 @@ class Api::V1::SalesforceWebhooksController < ActionController::Base
       neighborhoodId = params[:neighborhoodId] if params[:neighborhoodId].present?
       if neighborhoodId.present?
         crm_credential = CrmCredential.find_by(salesforce_property_id: neighborhoodId) rescue "" 
-        community = crm_credential.community
+        community = crm_credential.community rescue "" 
       end
       # binding.pry
       if neighborhoodId.present? && community&.crm_credential&.salesforce_property_id == neighborhoodId        
