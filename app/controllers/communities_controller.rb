7eacc79dd707855066760eec25e157538cee5df1
abubakar <abubakar@intagleo.com>
2021-09-24 17:27:14 +0500

    Removed unused check from import action

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -293,22 +293,18 @@ class CommunitiesController < ApplicationController
   def import
     Thread.current[:errors] = []
     @community = Community.find params[:community_id]
-    if @community.pynwheel_access
-      if @community.credentials_are_present? && @community.check_credentials
-        if @community.data_is_imported and Thread.current[:errors].empty?
-          flash[:notice] = "Good job! You have successfully imported this property's data."
-          PaperTrail::Version.create(item_type: "ImportData",item_id: @community.id,event: "update",whodunnit: current_user.id,community_id: current_community.id, company_id: current_company.id,object: "name: #{@community.name} community_id: '#{@community.id}'")
-          redirect_to community_settings_path(:community_id=>@community.id)
-        else
-          flash[:error] = Thread.current[:errors].join(',')
-          redirect_to community_settings_path(:community_id=>@community.id)
-        end
+    if @community.credentials_are_present? && @community.check_credentials
+      if @community.data_is_imported and Thread.current[:errors].empty?
+        flash[:notice] = "Good job! You have successfully imported this property's data."
+        PaperTrail::Version.create(item_type: "ImportData",item_id: @community.id,event: "update",whodunnit: current_user.id,community_id: current_community.id, company_id: current_company.id,object: "name: #{@community.name} community_id: '#{@community.id}'")
+        redirect_to community_settings_path(:community_id=>@community.id)
       else
-        flash[:error] = "Please enter valid credentials in settings before importing data."
+        flash[:error] = Thread.current[:errors].join(',')
         redirect_to community_settings_path(:community_id=>@community.id)
       end
     else
-      redirect_to root_path
+      flash[:error] = "Please enter valid credentials in settings before importing data."
+      redirect_to community_settings_path(:community_id=>@community.id)
     end
   end
 
