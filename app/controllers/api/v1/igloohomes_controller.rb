842f17b0a65dba4f65ae5c5a9c780025bb0df161
Salahudin Sallu <salahudin@intagleo.com>
2021-10-07 14:19:25 +0500

    Timezone percent encoding

--- app/controllers/api/v1/igloohomes_controller.rb
+++ app/controllers/api/v1/igloohomes_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::IgloohomesController < ActionController::Base
 
 
   def timezone
-    response = get_device_timezone(params[:timezone]) if params[:timezone].present?
+    response = get_device_timezone(timezone_making(params[:timezone])) if params[:timezone].present?
 
-    if response.present? && response["payload"].present? && response["payload"]["ranges"].present? &&  response["payload"]["gmtOffset"].present?
+    if response.present? && response["payload"].present? &&  response["payload"]["gmtOffset"].present?
       render :json=> {status: true, respnse: response["payload"]}
     else
       render :json=> {status: false, respnse: "Failed to fetch the timezone"}
@@ -12,7 +12,7 @@ class Api::V1::IgloohomesController < ActionController::Base
   end
 
   def pairing
-    response = get_paired_device(params[:timezone], params[:payload]) if params[:timezone].present? && params[:payload].present?
+    response = get_paired_device(timezone_making(params[:timezone]), params[:payload]) if params[:timezone].present? && params[:payload].present?
 
     if response.present? && response["payload"].present? && response["payload"]["bluetoothAdminKey"].present? &&  response["payload"]["masterPin"].present?
       render :json=> {status: true, respnse: response["payload"]}
@@ -23,6 +23,10 @@ class Api::V1::IgloohomesController < ActionController::Base
 
 
   private
+
+  def timezone_making time_zone
+    URI.escape(time_zone, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  end
 
   def get_paired_device time_zone,  payload
     url = "#{ENV["IGLOOHOME_API_BASE_URL"]}/v2/locks"
