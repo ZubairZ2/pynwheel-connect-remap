7b964366edddc95638e0ebd0c0f1b5dc3239daec
Salahudin Sallu <salahudin@intagleo.com>
2021-10-20 14:10:53 +0500

    Code improvements

--- app/services/psi_connection_service.rb
+++ app/services/psi_connection_service.rb
@@ -6,10 +6,12 @@ class PsiConnectionService < BaseService
       else
         url = "https://"+credentials.entrata_url+".entrata.com/api/v1/propertyunits"
       end
+      
       password = credentials.password
       username = credentials.username
       property_ids = credentials.property_id.split(',') rescue []
       property_id = property_ids[0]
+
       response = HTTParty.post(url,
         :body => {
           "auth": {
@@ -27,26 +29,12 @@ class PsiConnectionService < BaseService
           }
         }.to_json,
         :headers => { 'Content-Type' => 'application/json' } )
+
       response =  response.body.gsub('@','')
       hash = JSON.parse(response)
-#       ############################### Converting to own xml format
-#       psi_floorplan = "<Floorplans>"
-#       psi_unit = "Units"
-#
-#       hash["response"]["result"]["Properties"]["Property"][0]["Floorplans"]["Floorplan"].each do |floor|
-#         psi_floorplan = psi_floorplan + "<floorplan><IDValue>"+floor['Identification']['IDValue'].to_s+"</IDValue><Name>"+floor['Name'].to_s+"</Name><UnitCount>"+floor['UnitCount'].to_s+"</UnitCount><UnitAvailable>"+floor['UnitsAvailable'].to_s+"</UnitAvailable><DisplayedUnitsAvailable>"+floor['DisplayedUnitsAvailable'].to_s+"</DisplayedUnitsAvailable><bedroom>"+floor['Room'][0]['Count'].to_s+"</bedroom><bathroom>"+floor['Room'][1]['Count'].to_s+"</bathroom><SquareFeet>"+floor['SquareFeet']['@attributes']['Min'].to_s+"</SquareFeet><MarketRent>"+floor['MarketRent']['@attributes']['Min'].to_s+"</MarketRent></floorplan>"
-#
-#       end
-#       psi_floorplan = psi_floorplan + "</Floorplans>"
-#
-#       hash["response"]["result"]["Properties"]["Property"][0]["PropertyUnits"]["PropertyUnit"].each do |u|
-#         psi_unit = psi_unit+ " <Id>"+u['@attributes']['Id']+"</Id><UnitNumber>"+u['@attributes']['UnitNumber']+"</UnitNumber><FloorplanId>"+u['@attributes']['FloorplanId']+"</FloorplanId><UnitTypeId>"+u['@attributes']['UnitTypeId']+"</UnitTypeId>
-# <PropertyId>"+u['@attributes']['PropertyId']+"</PropertyId><FloorPlanName>"+u['@attributes']['FloorPlanName']+"</FloorPlanName><FloorId>"+u['@attributes']['FloorId']+"
-# </FloorId><BuildingId>"+u['@attributes']['BuildingId']+"</BuildingId><BuildingName>"+u['@attributes']['BuildingName']+"</BuildingName>"
-#
-#       end
-#       psi_floorplan
-    hash.to_xml
+      
+      hash.to_xml
+    
     rescue => e
       false
     end    
