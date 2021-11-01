be66a42ea2f1251c6af979c2f2deddc1d11fdf14
abubakar <abubakar@intagleo.com>
2021-11-01 16:03:40 +0500

    Fixed sorting issue

--- app/datatables/schedual_tours_datatable.rb
+++ app/datatables/schedual_tours_datatable.rb
@@ -130,7 +130,7 @@ private
   end
 
   def fetch_scheduled_tours
-    unless params[:sSearch]
+    if params[:sSearch].blank?
       scheduled_tours = SchedualTour.where(community_id: @community).where.not(tour_user_id: nil).desc_tour_date rescue ""
       # scheduled_tours = scheduled_tours.joins(joins_relation(sort_column)).order("#{sort_column} #{sort_direction}")
       scheduled_tours = scheduled_tours.page(page).per_page(per_page)
