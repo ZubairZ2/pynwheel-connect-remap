1d65c935420b1ba91838856395759849871e94af
abubakar <abubakar@intagleo.com>
2021-11-05 14:38:56 +0500

    Fixed one day and one hour reminder email notifications

--- app/jobs/delayed_scheduler_mailer_job.rb
+++ app/jobs/delayed_scheduler_mailer_job.rb
@@ -1,15 +1,16 @@
 class DelayedSchedulerMailerJob < ApplicationJob
   include SuckerPunch::Job
-
   def perform(subject, msg, to,community, community_subject = nil, community_msg = nil, community_to = nil, email_from = nil,show_html,schedule_tour)
     if email_from.present?
       NotificationMailer.tour_history_mail(subject, msg, to, email_from,community,show_html,schedule_tour).deliver_now
       if community_subject.present?
-        NotificationMailer.tour_history_mail(community_subject, community_msg, community_to, email_from,community,show_html,schedule_tour).deliver_now unless community_subject == nil
+        NotificationMailer.tour_history_mail(community_subject, community_msg, community_to,email_from,community,show_html,schedule_tour).deliver_now unless community_subject == nil
       end
     else
       NotificationMailer.tour_history_mail(subject, msg, to,email_from,community,show_html,schedule_tour).deliver_now
-      NotificationMailer.tour_history_mail(community_subject, community_msg, community_to,email_from,community,show_html,schedule_tour).deliver_now unless community_subject == nil
+      if community_subject.present?
+        NotificationMailer.tour_history_mail(community_subject, community_msg, community_to,email_from,community,show_html,schedule_tour).deliver_now unless community_subject == nil
+      end    
     end
   end
 end
