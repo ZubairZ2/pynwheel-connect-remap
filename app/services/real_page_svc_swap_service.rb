2cfe601c1b8d40cafbf5a50943704e9263feb2f2
Salahudin Sallu <salahudin@pynwheel.com>
2021-11-10 14:50:31 +0500

    Addition of the building fix

--- app/services/real_page_svc_swap_service.rb
+++ app/services/real_page_svc_swap_service.rb
@@ -262,7 +262,7 @@ class RealPageSvcSwapService < BaseService
                 end
                 unit.marketing_name = u[:UnitNumber]
 
-                unit.building = u[:BuildingID]
+                # unit.building = u[:BuildingID]
                 unit.floorplan_id = u[:FloorplanID]
                 unit.market_rent = u[:BaseRentAmount]
                 unit.effective_rent = u[:BaseRentAmount].to_f > 0 ? u[:BaseRentAmount] : 1
