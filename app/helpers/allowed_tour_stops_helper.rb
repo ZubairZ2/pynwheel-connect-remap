4da79b7166c2f66c3b279d715601f6a88dd5b0c6
Salahudin Sallu <salahudin@pynwheel.com>
2021-11-12 17:13:30 +0500

    Fix Igloohome lock

--- app/helpers/allowed_tour_stops_helper.rb
+++ app/helpers/allowed_tour_stops_helper.rb
@@ -4,28 +4,31 @@ module AllowedTourStopsHelper
 
     visible_stops.each do |stop|
       tour_stop = (stop[0].classify.constantize.find_by_id stop[1])
-      
-      if tour_stop.lock_provider == "Igloohome" || (tour_stop.doors.present? && tour_stop.doors.last.lock_provider === "Igloohome")
-        if tour_stop.class.name === "Amenity"
-          if tour_stop.present? && tour_stop.doors.present? && tour_stop.doors.last.lock_provider === "Igloohome"
-            allowed_stops << tour_stop.doors.last.id
-          else
-            allowed_stops << tour_stop.id
-          end
 
-        elsif tour_stop.class.name === "Unit"
-          if tour_stop.present? && tour_stop.door.present? && tour_stop.doors.last.lock_provider === "Igloohome"
-            allowed_stops << tour_stop.door.id
-          else
-            allowed_stops << tour_stop.id
-          end
+     unless tour_stop.class.name === "Amenity" || tour_stop.class.name === "Unit"
+      if tour_stop.lock_provider == "Igloohome"
+        allowed_stops << tour_stop.id
+      end
+     end
 
+      if tour_stop.class.name === "Amenity" && ( tour_stop.lock_provider == "Igloohome" || (tour_stop.doors.present? && tour_stop.doors.last.lock_provider === "Igloohome") )
+        if tour_stop.present? && tour_stop.doors.present? && tour_stop.doors.last.lock_provider === "Igloohome"
+          allowed_stops << tour_stop.doors.last.id
         else
           allowed_stops << tour_stop.id
         end
-        
       end
+        
+      if tour_stop.class.name === "Unit" && ( tour_stop.lock_provider == "Igloohome" || (tour_stop.door.present? && tour_stop.door.lock_provider === "Igloohome") )
+        if tour_stop.present? && tour_stop.door.present? && tour_stop.door.lock_provider === "Igloohome"
+          allowed_stops << tour_stop.door.id
+        else
+          allowed_stops << tour_stop.id
+        end
+      end
+
     end
+      
 
     allowed_stops << community.tour.id if community.tour.lock_provider == "Igloohome"
     
