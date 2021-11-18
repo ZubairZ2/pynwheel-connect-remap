5a91cec8e93aaefbf7036d99befd41ba31dba696
abubakar <abubakar@intagleo.com>
2021-11-18 16:18:06 +0500

    Added arrived attribute on tour history creation

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -235,7 +235,7 @@ class Api::V1::CommunitiesController < ActionController::Base
   #TODO:: Incase if you need to create tourhistory here otherwise remove it later.
   def create_tour_history(tour_user,tour_type,community)
     tour_history = TourHistory.find_or_create_by(tour_user_id: tour_user.id) rescue TourHistory.new
-    tour_history.update_columns(community_id: community.id, tour_type: tour_type, tour_user_id: tour_user.id, tour_id: community.tour.id)
+    tour_history.update_columns(community_id: community.id, tour_type: tour_type, tour_user_id: tour_user.id, tour_id: community.tour.id, arrived: Time.now)
   end
 
   def send_access_code_email subj, body, community
