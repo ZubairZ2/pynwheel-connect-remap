9706557cc5fe48c81cafa7ed26ad6fe807c7743f
abubakar <abubakar@intagleo.com>
2021-12-23 17:16:13 +0500

    PYN-211/Added token authorization before hiting the API

--- app/controllers/api/v1/floorplans_controller.rb
+++ app/controllers/api/v1/floorplans_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::FloorplansController < ActionController::Base
+  include ApplicationHelper
 
   def index
-    @community = Community.find params[:community_id]
-    floorplans = FloorplanUnitsService.new(@community).get_floorplans
-    @floorplans = Kaminari.paginate_array(floorplans).page(params[:page]).per(params[:per_page])
+    access = grant_access (decoded(params[:token])) rescue false
+    if api_access or access == true
+      @community = Community.find params[:community_id]
+      floorplans = FloorplanUnitsService.new(@community).get_floorplans
+      @floorplans = Kaminari.paginate_array(floorplans).page(params[:page]).per(params[:per_page])
+    end
   end
 
 end
