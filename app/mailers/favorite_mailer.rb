cd5636717b21a79bc2052ad043590706ea158c10
Salahudin Sallu <salahudin@pynwheel.com>
2022-01-10 17:57:47 +0500

    Touch app brochure email fix

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,5 +1,4 @@
 class FavoriteMailer < ApplicationMailer
-
 	def email_favorites(email_from,email_to,email_bcc,email_body,favorites,units,ios,community)
     @favorites = favorites
     @email_body = email_body
@@ -9,6 +8,7 @@ class FavoriteMailer < ApplicationMailer
     @to_email = email_to
     mail(to: email_to, from: email_from, bcc: email_bcc, subject: 'My Favorites')
   end
+
   def email_favorites_text(email_from,email_to,email_bcc,email_body,favorites,units,ios,community)
       @favorites = favorites
       @email_body = email_body
