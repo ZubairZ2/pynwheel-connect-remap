51eeab3f43772f42717bac8d6402100f6401f887
subhanabid-int <subhan.abid@intagleo.com>
2022-01-26 21:50:15 +0500

    Get enpoint updates

--- app/controllers/api/v2/community_property_map_controller.rb
+++ app/controllers/api/v2/community_property_map_controller.rb
@@ -8,18 +8,28 @@ class Api::V2::CommunityPropertyMapController < Api::V2::ApiApplicationControlle
     else
       property_map = @community.floorplates
     end
-    render :json => {data: property_map.as_json}
+    if property_map
+      render :json => {:success =>  true, data: property_map.as_json}
+    else
+      render :json => {:success => false , :message => "Record not found."}
+    end
   end
 
-  def update
+  def set_community_floorplates
+
     if @community.is_sitemap
+      if params["sitemap"]["id"].present?
+        @community.sitemap.find_by_id(params["sitemap"]["id"])
+      else
+        @community.sitemap.create(site_params)
+      end
     sitemap = @community.sitemap
       if sitemap.update(property_map_params)
         @property_map = sitemap
       end
     else
       params[:floorplate].values.each do |f|
-        @community.floorplates.find_or_create_by!(id: f["id"]).update_attributes(name: f["name"], range: f["range"], image: f["image"])
+        @community.floorplates.find_or_create_by!(id: f["id"],name: f["name"], range: f["range"], image: f["image"])
       end
       @property_map = @community.floorplates
     end
