c40c2d1389284f403dfb1c738ceab726c58c9b2c
Muhammad Fahad Umer <fahad.umer@intagleo.com>
2022-02-01 17:48:11 +0500

    Changes single params to multi params endpoint

--- app/controllers/api/v2/community_floor_plans_controller.rb
+++ app/controllers/api/v2/community_floor_plans_controller.rb
@@ -1,70 +1,60 @@
 class Api::V2::CommunityFloorPlansController < Api::V2::ApiApplicationController
-  before_action :load_community, only: [:index, :show, :check_floorplan]
+  before_action :load_community, only: [:index, :show, :add_floorplan]
   before_action :doorkeeper_authorize!
 
   def index
     floorplans = @community.floorplans.order(id: :desc)
     if floorplans.present?
-      render json: {succcess: true, data: floorplans.as_json}
+      render json: { succcess: true, data: floorplans.as_json }
     else
-      render json: {success: false, data: "No floorplans found"}
+      render json: { success: false, data: "No floorplans found" }
     end
   end
 
   def show
     floorplan = @community.floorplans.find_by_id(params[:id])
     if floorplan.present?
-      render json: {succcess: true, data: floorplan.as_json}
+      render json: { succcess: true, data: floorplan.as_json }
     else
-      render json: {success: false, data: "No floorplan found"}
+      render json: { success: false, data: "No floorplan found" }
     end
   end
 
-  def check_floorplan
-    if params["floorplan"]["id"].nil? || params["floorplan"]["id"] == ""
-      create_floorplan
-    else
-      update_floorplan
-    end
-  end
-
-  def create_floorplan
-    if params["floorplan"]["name"].nil?
-      data = "Required field: name* must be filled."
-    elsif params["floorplan"]["image"].nil?
-      data = "Required field: image* must be selected."
-    else
-      if @community.floorplans.where(name: params["floorplan"]["name"]).present?
-        data = "Required field: name* must be unique."
-      elsif @community.floorplans.where(provider_floorplan_id: params["floorplan"]["provider_floorplan_id"]).present?
-        data = "Required Field: provider floorplan id => #{params["floorplan"]["provider_floorplan_id"]} is already used."
+  def add_floorplan
+    @floorplan = []
+    floorplans_params = params["floorplan"].values
+    floorplans_params.each do |floorplan|
+      if floorplan["id"].present? && @community.floorplans.find_by_id(floorplan["id"]).present?
+        find_floorplan = @community.floorplans.find_by_id(floorplan["id"])
+        if find_floorplan.update(name: floorplan["name"], image: floorplan["image"], secondary_image: floorplan["secondary_image"])
+          PaperTrail::Version.create(item_type: "Floorplan", item_id: find_floorplan.id, event: "update", whodunnit: current_pynwheel_user.id, community_id: @community.id, company_id: @community.company.id, object: "name: '#{find_floorplan.name}' community_id: '#{@community.id}'")
+          @floorplan << { success: true, data: find_floorplan.as_json }
+        else
+          @floorplan << { success: false, data: find_floorplan.errors.full_messages.join(',') }
+        end
       else
-        floorplan = @community.floorplans.create!(floorplan_post_params)
-        data = floorplan.as_json
+        new_floorplan = @community.floorplans.new(name: floorplan["name"], image: floorplan["image"], secondary_image: floorplan["secondary_image"])
+        if new_floorplan.save
+          PaperTrail::Version.create(item_type: "Floorplan", item_id: new_floorplan.id, event: "create", whodunnit: current_pynwheel_user.id, community_id: @community.id, company_id: @community.company.id, object: "name: '#{new_floorplan.name}' community_id: '#{@community.id}'")
+          @floorplan << { success: true, data: new_floorplan.as_json }
+        else
+          @floorplan << { success: false, data: new_floorplan.errors.full_messages.join(',') }
+        end
       end
     end
-    render json: {success: true, data: data}
-  end
-
-  def update_floorplan
-    floorplan = @community.floorplans.where(id: params["floorplan"]["id"])
-    if floorplan.present?
-      if floorplan.update(floorplan_post_params)
-        render json: {success: true, data: floorplan.as_json}
-      end
-    else
-      render json: {success: false, data: "Id not found! Please try again."}
+    if @floorplan.any?
+      render json: @floorplan
     end
   end
-
 
   private
+
   def load_community
     @community = Community.find params[:community_id]
   end
 
   def floorplan_post_params
-    params.require(:floorplan).permit(:id, :name, :image, :secondary_image, :market_rent, :provider_floorplan_id)
+    params.require(:floorplan).permit!
   end
 
   def floorplan_params
--- app/models/floorplan.rb
+++ app/models/floorplan.rb
@@ -53,29 +53,30 @@ class Floorplan < ApplicationRecord
   belongs_to :community
   has_many :amenities, as: :amenityable
   has_one :status, as: :statusable
-  validates_uniqueness_of :name, scope: :community, on: :create
-  validates_uniqueness_of :provider_floorplan_id, scope: :community
-  after_commit :populate_image_urls, on: [:create,:update]
-  validates :market_rent, :numericality => { greater_than_or_equal_to: -1 }
+  validates_uniqueness_of :name, scope: :community, on: [:create, :update]
+  # validates_uniqueness_of :provider_floorplan_id, scope: :community
+  after_commit :populate_image_urls, on: [:create, :update]
+  # validates :market_rent, :numericality => { greater_than_or_equal_to: -1 }
   # before_create :set_image_name
   after_update :crop_image
   after_update :crop_secondary_image
 
   def as_json
     super(
-      :only => [:id , :name, :community_id, :provider_floorplan_id, :provider,:market_rent, :image, :secondary_image]
+      :only => [:id, :name, :community_id, :image, :secondary_image]
     )
   end
 
   def populate_image_urls
     if image.present?
-      set_standard_url('Floorplan',id)
+      set_standard_url('Floorplan', id)
     end
   end
 
   def crop_secondary_image
     secondary_image.recreate_versions! if (crop_x_secondary.present? && !image_bit && do_crop_secondary)
   end
+
   def crop_image
     image.recreate_versions! if (crop_x.present? && image_bit && do_crop)
   end
