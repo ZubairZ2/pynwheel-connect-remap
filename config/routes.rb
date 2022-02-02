ea4f8d43ce9169e2f0fe4e858c2b8befe9b1510d
abubakar <abubakar@intagleo.com>
2022-02-02 19:09:33 +0500

    Implemented GET API to fetch the existing data provider and crm provider

--- config/routes.rb
+++ config/routes.rb
@@ -549,30 +549,29 @@ Rails.application.routes.draw do
   namespace :api, constraints: { format: 'json' } do
     namespace :v2 do
       post '/communities/:community_id/update_galleries', to: 'galleries#update_galleries'
+      get '/communities/:community_id/community_data_provider', to: 'data_providers#get_community_data_provider'
       resources :user_details do
         member do
           put :update_company
         end
       end
-
       resources :community_property_map do
         member do
           post :add_property_images
         end
       end
-
       resources :communities do
         member do
           post :add_comment
           post :get_products
           put :update_products
         end
-
         resources :galleries do
           member do
             delete :delete_gallery_image
           end
         end
+        resources :data_providers
 
       end
       resources :access_token do
