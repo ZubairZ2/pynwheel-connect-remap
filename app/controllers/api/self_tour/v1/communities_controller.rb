79a0f73f0f5ce644957ea3c9e556d2297fe21b83
Salahudin Sallu <salahudin@pynwheel.com>
2022-02-11 18:09:19 +0500

    Cutomize tour stop list

--- app/controllers/api/self_tour/v1/communities_controller.rb
+++ app/controllers/api/self_tour/v1/communities_controller.rb
@@ -1,14 +1,14 @@
 class Api::SelfTour::V1::CommunitiesController < ActionController::Base
   
-  before_action :set_community, only: [:customize_stops_list, :initialize_tour]
-  before_action :set_community_tour, only: [:customize_stops_list, :initialize_tour]
-  before_action :set_tour_user, only: [:customize_stops_list, :initialize_tour]
+  before_action :set_community, only: [:customize_tour, :initialize_tour]
+  before_action :set_community_tour, only: [:customize_tour, :initialize_tour]
+  before_action :set_tour_user, only: [:customize_tour, :initialize_tour]
   before_action :random_string_generator, only: [:initialize_tour]
 
   include ApplicationHelper
   include StripeServices
   
-  def customize_stops_list
+  def customize_tour
     access = grant_access (decoded(params[:token])) rescue false
     if api_access or access == true
       @building_list = Buildings.new(@community).get_community_buildings
@@ -19,7 +19,6 @@ class Api::SelfTour::V1::CommunitiesController < ActionController::Base
 
   def initialize_tour
     @floorplans = get_floorplans_with_required_filter()
-    @tour = @tours.last
     @tour_type = params[:tour_status] rescue @tour_user.tour_type
     charge_for_id_verfication(@tour_user, 200) if (do_verfication params[:verfied_by_provider], @community)
     update_verification_attributes()
@@ -49,9 +48,7 @@ class Api::SelfTour::V1::CommunitiesController < ActionController::Base
   end
 
   def set_community_tour
-    @tours = []
-    @tours <<  @community.tour
-    @tours
+    @tour = @community.tour
   end
 
   def get_floorplans_with_required_filter
