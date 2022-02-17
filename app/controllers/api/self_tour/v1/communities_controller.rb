1095a6dd35d7f9087395be354a65d4bdd6f1a7f0
Salahudin Sallu <salahudin@pynwheel.com>
2022-02-17 14:07:57 +0500

    Fix virtual tour

--- app/controllers/api/self_tour/v1/communities_controller.rb
+++ app/controllers/api/self_tour/v1/communities_controller.rb
@@ -26,6 +26,8 @@ class Api::SelfTour::V1::CommunitiesController < ActionController::Base
   def initialize_tour
     @floorplans = get_floorplans_with_required_filter()
     @tour_type = params[:tour_status] rescue @tour_user.tour_type
+    @tour_user.update(tour_type: params[:tour_status], tour_key: @random_string)
+    
     charge_for_id_verfication(@tour_user, 200) if (do_verfication params[:verfied_by_provider], @community)
     update_verification_attributes()
     PropertyAccessCode.new(@community ,@tour_user, @tour_type).restrict_property_access_with_code
