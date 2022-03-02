9e9f821047508b128e1322ddc74ec2019f99b8a8
Salahudin Sallu <salahudin@pynwheel.com>
2022-03-02 20:24:41 +0500

    PYN-368 statuses and remarks object created

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,8 @@ class Status < ApplicationRecord
     belongs_to :statusable, polymorphic: true
 
     enum status: [:in_progress, :submitted, :approved, :rejected , :re_submitted]
+
+    def status_and_remarks_obj
+        {name: self.status, remarks: self.remarks}
+    end
 end
