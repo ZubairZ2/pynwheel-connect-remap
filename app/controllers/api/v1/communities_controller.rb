81409dac97fda3a9caf2238946bfddde1b4db760
Salahudin Sallu <salahudin@pynwheel.com>
2022-03-15 20:16:34 +0500

    Tour summary button fix

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -420,7 +420,8 @@ class Api::V1::CommunitiesController < ActionController::Base
       @community = Community.find_by_id params[:id]
       @tour_user = TourUser.find_by_id params[:tour_user_id]
       if @community.present? and @tour_user.present?
-        @visited_history = VisitedStop.exists?(tour_user_id:  @tour_user.id ,tour_id: @community.community_tour.id)
+        @tour = CustomizeTourService.new(@community, @tour_user).get_user_tour
+        @visited_history = VisitedStop.exists?(tour_user_id:  @tour_user.id ,tour_id: @tour.id)
         data = {visited_history: @visited_history, tour_user: @tour_user}
         render :json=> {data: data, :status=>true, :message => "data retuned succesfully", code: 200}
       else
