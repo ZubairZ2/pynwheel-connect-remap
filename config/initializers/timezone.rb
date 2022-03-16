9b9ecbc44780308b6c4e11cb9568790f1c000538
Salahudin Sallu <salahudin@pynwheel.com>
2022-03-16 15:57:44 +0500

    auto way finding fix for tour custumization

--- config/initializers/timezone.rb
+++ config/initializers/timezone.rb
@@ -1,7 +1,3 @@
 Timezone::Lookup.config(:geonames) do |c|
-    puts "---------------------------------"*100
-    puts "The hourly limit of 1000 credits for this IP has been exceeded. Please throttle your requests or use the commercial service."
-    puts "---------------------------------"*100
-
     c.username = 'hamza.shamshad'
 end
--- db/migrate/20210201102941_add_details_in_community_users.rb
+++ db/migrate/20210201102941_add_details_in_community_users.rb
@@ -1,6 +1,6 @@
 class AddDetailsInCommunityUsers < ActiveRecord::Migration[5.0]
   def change
-    rename_column :communities, :is_chat_login, :is_chat_available
+    # rename_column :communities, :is_chat_login, :is_chat_available
     add_column :community_users, :is_logged_in, :boolean, default: false
     change_column_default :community_users, :chat_enable, false
   end
