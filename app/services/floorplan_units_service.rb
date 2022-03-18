acd46b38fbae178ca5193dc9f4dd20035d966a9b
Salahudin Sallu <salahudin@pynwheel.com>
2022-03-18 15:44:42 +0500

    Fix for amenities

--- app/services/floorplan_units_service.rb
+++ app/services/floorplan_units_service.rb
@@ -15,14 +15,18 @@ class FloorplanUnitsService < BaseService
   end
 
   def get_floorplan_units(floorplan)
-    Unit.where('floorplan_id = ? AND community_id = ? AND available = ?', floorplan.provider_floorplan_id, @community.id, true).available_units.where.not(floor: not_floor, building: not_building) if floorplan.present?
+    unless @community.is_sitemap
+      Unit.where('floorplan_id = ? AND community_id = ? AND available = ?', floorplan.provider_floorplan_id, @community.id, true).available_units.where.not(floor: not_floor, building: not_building) if floorplan.present?
+    else
+      Unit.where('floorplan_id = ? AND community_id = ? AND available = ?', floorplan.provider_floorplan_id, @community.id, true).available_units if floorplan.present?
+    end
   end
 
   def get_floorplate_amenities
     unless @community.is_sitemap
       @community.amenities.where(amenityable_type: "Floorplate").where.not(x_plot: [0,nil], y_plot: [0,nil], floor: not_floor, building: not_building).sort_by { |a| a&.floor } 
     else
-      @community.amenities.where(amenityable_type: "Sitemap").where.not(x_plot: [0,nil], y_plot: [0,nil], floor: nil, building: nil)
+      @community.amenities.where(amenityable_type: "Sitemap").where.not(x_plot: [0,nil], y_plot: [0,nil])
     end
   end
 
