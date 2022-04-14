e91f3cdfda056b61343f302a8dcee9b2d4dcaa59
Muhammad Fahad Umer <fahad.umer@intagleo.com>
2022-04-14 22:18:32 +0500

    pyn-407 visiting hours integration

--- app/controllers/api/v2/opening_hours_controller.rb
+++ app/controllers/api/v2/opening_hours_controller.rb
@@ -18,7 +18,8 @@ class Api::V2::OpeningHoursController < Api::V2::ApiApplicationController
   def create_opening_hours
     begin
       if @community.self_tour
-        tour_params = params["data"]
+        values = params["data"]
+        tour_params = JSON.parse(values)
         if tour_params.present?
           tour_params.values.each_with_index do |tour, index|
             self_tour_method(tour) if tour_params.keys[index].eql?(SELF_TOUR)
@@ -28,6 +29,12 @@ class Api::V2::OpeningHoursController < Api::V2::ApiApplicationController
       end
       visiting_hours = get_all_hours
       @community.set_visiting_hours_status(current_pynwheel_user)
+      email = PynwheelLaunch::Communities::FollowUpEmails.new(@community).send_emails
+      email[:data].each do |mail|
+        if mail[:name].eql?(VISITING_HOURS) && mail[:status].eql?("Submitted")
+          FollowUpMailer.send_submitted_form(@community, VISITING_HOURS, email[:data]).deliver_later
+        end
+      end
       if visiting_hours.any?
         render json: { success: true, data: visiting_hours.as_json }
       end
@@ -55,12 +62,12 @@ class Api::V2::OpeningHoursController < Api::V2::ApiApplicationController
   private
 
   def delete_self_visiting_hours(tour)
-    delete_hour = @community.opening_hours.find_by(id: tour)
+    delete_hour = OpeningHour.find_by(id: tour)
     delete_hour.destroy
   end
 
   def delete_guided_visiting_hours(tour)
-    delete_hour = @community.guided_opening_hours.find_by(id: tour)
+    delete_hour = GuidedOpeningHour.find_by(id: tour)
     delete_hour.destroy
   end
 
@@ -78,11 +85,17 @@ class Api::V2::OpeningHoursController < Api::V2::ApiApplicationController
       tour_hour_id = tour_hour["id"]
       if tour_hour_id.present?
         update_hour = @community.guided_opening_hours.find_by(id: tour_hour_id)
-        if update_hour.present?
-          update_hour.update(day: tour_hours.keys[index], opening_time: time_24_hours(tour_hour[:opening_time]), closing_time: time_24_hours(tour_hour[:closing_time]))
+        if tour_hour["opening_time"].eql?("Invalid Date")
+          delete_guided_visiting_hours(tour_hour_id)
+        else
+          if update_hour.present?
+            update_hour.update(day: tour_hours.keys[index], opening_time: time_24_hours(tour_hour["opening_time"]), closing_time: time_24_hours(tour_hour["closing_time"]))
+          end
         end
       else
-        @community.guided_opening_hours.create(day: tour_hours.keys[index], opening_time: time_24_hours(tour_hour[:opening_time]), closing_time: time_24_hours(tour_hour[:closing_time]))
+        if !tour_hour["opening_time"].eql?("Invalid Date")
+          @community.guided_opening_hours.create(day: tour_hours.keys[index], opening_time: time_24_hours(tour_hour["opening_time"]), closing_time: time_24_hours(tour_hour["closing_time"]))
+        end
       end
     end
   end
@@ -92,11 +105,17 @@ class Api::V2::OpeningHoursController < Api::V2::ApiApplicationController
       tour_hour_id = tour_hour["id"]
       if tour_hour_id.present?
         update_hour = @community.opening_hours.find_by(id: tour_hour_id)
-        if update_hour.present?
-          update_hour.update(day: tour_hours.keys[index], opening_time: time_24_hours(tour_hour[:opening_time]), closing_time: time_24_hours(tour_hour[:closing_time]))
+        if tour_hour["opening_time"].eql?("Invalid Date")
+          delete_self_visiting_hours(tour_hour_id)
+        else
+          if update_hour.present?
+            update_hour.update(day: tour_hours.keys[index], opening_time: time_24_hours(tour_hour["opening_time"]), closing_time: time_24_hours(tour_hour["closing_time"]))
+          end
         end
       else
-        @community.opening_hours.create(day: tour_hours.keys[index], opening_time: time_24_hours(tour_hour[:opening_time]), closing_time: time_24_hours(tour_hour[:closing_time]))
+        if !tour_hour["opening_time"].eql?("Invalid Date")
+          @community.opening_hours.create(day: tour_hours.keys[index], opening_time: time_24_hours(tour_hour["opening_time"]), closing_time: time_24_hours(tour_hour["closing_time"]))
+        end
       end
     end
   end
