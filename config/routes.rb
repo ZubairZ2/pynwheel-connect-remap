eefec3657fb49e073664f3f1d21558b856dec057
Salahudin Sallu <94007791+Salahudin-Sallu@users.noreply.github.com>
2022-04-26 13:45:04 +0500

    Revert "pyn-413 added two endpoints for email verification and user signup"

--- config/routes.rb
+++ config/routes.rb
@@ -571,8 +571,6 @@ Rails.application.routes.draw do
       post :perq_tour_webhook, to: 'perq_webhooks#perq_tour_webhook'
       post :save_data, to: 'dwelo_devices#load_data'
       post :device_lock_unlock, to: 'dwelo_devices#device_lock_or_unlock'
-      get :get_tour_user_by_tour, to: 'communities#get_tour_user_by_tour'
-      get :get_user_by_email, to: 'communities#get_user_by_email'
       resources :communities, only: :index do
         member do
           get :data
