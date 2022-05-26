e1ef3e6577aded5be343cacc264f7c745dc79e32
Muhammad Fahad Umer <fahad.umer@intagleo.com>
2022-05-26 22:03:58 +0500

    pyn-413 modify get-filtered-data

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -506,6 +506,7 @@ class Api::V1::CommunitiesController < ActionController::Base
         upcoming_tours = []
         completed_tours = []
         expired_tours = []
+        last_visit = nil
         if @scheduled_tours.present?
           @scheduled_tours.each do |tour|
             if tour.present?
@@ -515,10 +516,11 @@ class Api::V1::CommunitiesController < ActionController::Base
             end
           end
           last_visit = get_last_visited_community(@scheduled_tours)
-          data = { tour_user: @tour_user, last_visit: last_visit, upcoming: upcoming_tours.uniq, completed: completed_tours.uniq, exipred: expired_tours.uniq }
+          data = { tour_user: @tour_user, last_visit: last_visit, upcoming: upcoming_tours, completed: completed_tours, exipred: expired_tours }
           render :json=> { data: data.as_json, :status=> true, :message => "data returned succesfully", code: 200 }
         else
-          render :json=> { data: data, :status=>false, :message => "Tours not found", code: 400 }
+          data = { tour_user: @tour_user, last_visit: last_visit, upcoming: upcoming_tours.uniq, completed: completed_tours.uniq, exipred: expired_tours.uniq }
+          render :json=> { data: data.as_json, :status=>true, code: 200 }
         end
       else
         render :json=> { data: data, :status=>false, :message => "Invalid or Missing tour_user_id", code: 400 }
