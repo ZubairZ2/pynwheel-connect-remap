6281642de8e7df23552a9bbc2702f9e4494a938a
Muhammad Fahad Umer <fahad.umer@intagleo.com>
2022-05-31 19:13:04 +0500

    pyn-413 uncomment image uploader

--- app/models/community.rb
+++ app/models/community.rb
@@ -4,7 +4,7 @@ class Community < ApplicationRecord
   mount_base64_uploader :logo, AvatarUploader
   mount_base64_uploader :secondary_logo, AvatarUploader
   mount_base64_uploader :self_tour_logo, AvatarUploader
-  # mount_base64_uploader :brand_details_pdf , PdfUploader  // will required in stagging
+  mount_base64_uploader :brand_details_pdf , PdfUploader
 
   belongs_to :company
   belongs_to :community_group
