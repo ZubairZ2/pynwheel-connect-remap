58cb742813aae16f7a57cd49ca7e69398dd2f2d8
Salahudin Sallu <salahudin@pynwheel.com>
2022-06-22 14:41:27 +0500

    pyn-484 fix spreadsheet issue

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -119,136 +119,29 @@ class Credential < ApplicationRecord
   end
 
   def import_data_from_spreadsheet(file)
-    xlsx = Roo::Spreadsheet.open(file.open)
-    units = xlsx.sheet(0)
-    units.each_with_index do |u,index|
-      unless index == 0 
-        unit = Unit.where(provider: "spreadsheet",community_id: community_id,provider_unit_id: u[0].to_s.gsub(".","")).first_or_initialize
-        unit.provider_unit_id = u[0].to_s.gsub(".","")
-        unit.marketing_name = u[0].to_i == 0 ? u[0] : u[0] rescue u[0]
-        unit.unit_type = u[0]
-        unit.floorplan_id = u[1]
-        unit.floor = u[3]
-        unit.availability = u[4] == true ? "Unoccupied" : "Occupied"
-        unit.available = u[4] == true ? true : false
-        unit.available_date = u[5]
-        unit.market_rent = u[6].present? ? u[6] : 1
-        unit.effective_rent = u[6].present? ? u[6] : 1
-        unit.save(validate: false)
-      end
-    end
-    floorplans = xlsx.sheet(1)
-
-    floorplans.each_with_index do |f,index|
-      unless index == 0
-        floorplan = Floorplan.where(provider: "spreadsheet",community_id: community_id,provider_floorplan_id: f[0]).first_or_initialize  
-        floorplan.name = f[1]
-        floorplan.square_feet = f[2]
-        floorplan.bedrooms = f[3]
-        floorplan.bathrooms = f[4]
-        floorplan.save(validate: false)
-      end
-    end
+    spreadsheet_data_update_or_import(file)
   end
 
   def swap_data_from_spreadsheet(file)
+    spreadsheet_data_update_or_import(file)
+  end
+  
+  def spreadsheet_data_update_or_import file
     xlsx = Roo::Spreadsheet.open(file.open)
+   
     units = xlsx.sheet(0)
-    units.each_with_index do |u,index|
-      unless index == 0
-        unit_name = u[0].to_i == 0 ? u[0] : u[0] rescue u[0]
-        unit = Unit.where(community_id: community_id,marketing_name: unit_name).first
-        if unit.present?
-          unit.provider_unit_id = u[0].to_s.gsub(".","")
-          unit.provider = "spreadsheet_new"
-          unit.unit_type = u[0]
-          unit.floorplan_id = u[1]
-          unit.floor = u[3]
-          unit.availability = u[4] == true ? "Unoccupied" : "Occupied"
-          unit.available = u[4] == true ? true : false
-          unit.available_date = u[5]
-          unit.market_rent = u[6].present? ? u[6] : 1
-          unit.effective_rent = u[6].present? ? u[6] : 1
-          unit.save(validate: false)
-        else
-          dup = Unit.find_by(community_id: community_id, provider_unit_id: u[0].to_s.gsub(".",""))
-          if dup.present?
-            dup.destroy
-          end
-          unit = Unit.new
-          unit.community_id = community_id
-          unit.marketing_name = u[0].to_i == 0 ? u[0] : u[0] rescue u[0]
-          unit.provider_unit_id = u[0].to_s.gsub(".","")
-          unit.provider = "spreadsheet_new"
-          unit.unit_type = u[0]
-          unit.floorplan_id = u[1]
-          unit.floor = u[3]
-          unit.availability = u[4] == true ? "Unoccupied" : "Occupied"
-          unit.available = u[4] == true ? true : false
-          unit.available_date = u[5]
-          unit.market_rent = u[6].present? ? u[6] : 1
-          unit.effective_rent = u[6].present? ? u[6] : 1
-          unit.save(validate: false)
-        end
-      end
-    end
-    unit = Unit.where(community_id: community_id)
-    unit.each do |d|
-      unless d.provider == "spreadsheet_new" || d.provider == "manually"
-        d.destroy
-      end
-    end
-    unit = Unit.where(community_id: community_id)
-    unit.each do |d|
-      if d.provider == "spreadsheet_new"
-        d.provider = "spreadsheet"
-      end
-    end
 
+    units.each_with_index do |u, index|
+      create_or_update_unit(community_id, u) unless index == 0
+    end
+    
     floorplans = xlsx.sheet(1)
 
     floorplans.each_with_index do |f,index|
-      unless index == 0
-        floorplan = Floorplan.where(community_id: community_id,name: f[1]).first
-        if floorplan.present?
-          floorplan.provider = "spreadsheet_new"
-          floorplan.provider_floorplan_id = f[0]
-          floorplan.square_feet = f[2]
-          floorplan.bedrooms = f[3]
-          floorplan.bathrooms = f[4]
-          floorplan.manual_override = false
-          floorplan.save(validate: false)
-        else
-          dup = Floorplan.find_by(community_id: community_id, provider_floorplan_id: f[0])
-          if dup.present?
-            dup.destroy
-          end
-          floorplan = Floorplan.new
-          floorplan.community_id = community_id
-          floorplan.name = f[1]
-          floorplan.provider = "spreadsheet_new"
-          floorplan.provider_floorplan_id = f[0]
-          floorplan.square_feet = f[2]
-          floorplan.bedrooms = f[3]
-          floorplan.bathrooms = f[4]
-          floorplan.save(validate: false)
-        end
-      end
-    end
-    fp = Floorplan.where(community_id: community_id)
-    fp.each do |d|
-      unless d.provider == "spreadsheet_new"
-        d.destroy
-      end
-    end
-    fp = Floorplan.where(community_id: community_id)
-    fp.each do |d|
-      if d.provider == "spreadsheet_new"
-        d.provider = "spreadsheet"
-      end
+      create_or_update_floorplan(community_id, f) unless index == 0
     end
   end
-  
+
   def set_https_in_url
     if self.url.present?
       unless url.include?('https')
@@ -264,4 +157,44 @@ class Credential < ApplicationRecord
       self.community.update_columns(scheduler_widget: false)
     end
   end
+
+  private
+
+  def create_or_update_unit community_id, u
+    unit = Unit.where(provider: "spreadsheet", community_id: community_id, provider_unit_id: u[0].to_s.gsub(".","") ).first_or_initialize
+
+    unit.provider_unit_id = u[0].to_s.gsub(".","")
+    unit.marketing_name = get_integer_value(u[0])
+    unit.unit_type = get_integer_value(u[0])
+    unit.floorplan_id = u[1]
+    unit.floor = get_integer_value(u[3])
+    unit.availability = u[4] == true ? "Unoccupied" : "Occupied"
+    unit.available = u[4] == true  ? true : false
+    unit.available_date = u[5]
+    unit.market_rent = u[6].present? ? u[6] : 0
+    unit.effective_rent = u[6].present? ? u[6] : 0
+
+    unit.save(validate: false)
+  end
+
+  def create_or_update_floorplan community_id, f
+    floorplan = Floorplan.where(provider: "spreadsheet", community_id: community_id, provider_floorplan_id: f[0] ).first_or_initialize
+
+    floorplan.name = get_integer_value(f[1])
+    floorplan.provider_floorplan_id = f[0]
+    floorplan.square_feet = f[2]
+    floorplan.bedrooms = get_integer_value(f[3])
+    floorplan.bathrooms = get_integer_value(f[4])
+
+    floorplan.save(validate: false)
+  end
+
+  def get_integer_value value
+    if value.class == Integer || value.class == Float
+      value.to_i
+    else
+      value
+    end
+  end
+
 end
