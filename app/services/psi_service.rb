75070509ef2de7e6480f0a8d786fad9254064399
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2022-08-30 12:12:44 +0500

    added to_s method

--- app/services/psi_service.rb
+++ app/services/psi_service.rb
@@ -349,7 +349,7 @@ class PsiService < BaseService
         floorplan.provider = "psi"
 
         unless floorplan.name_is_updated.present? && floorplan.name_is_updated
-          floorplan.name = f["Name"] + " " + f["Identification"]["IDValue"]
+          floorplan.name = f["Name"] + " " + f["Identification"]["IDValue"].to_s
         end
 
         floorplan.unit_count = f["UnitsAvailable"]
