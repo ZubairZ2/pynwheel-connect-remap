740e2af8144a61c86e99b31a7a439f74b93eb545
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2022-09-06 18:52:16 +0500

    pyn-545 added json method for design direction

--- app/models/design_direction.rb
+++ app/models/design_direction.rb
@@ -1,3 +1,9 @@
 class DesignDirection < ApplicationRecord
   belongs_to :comunity
+
+  def as_json
+    super(
+      :only => [:id, :image, :hex_colors, :direction, :additional_direction]
+    )
+  end
 end
