f2ebdefbbee1068ff933212144bf8b7aa79db699
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2022-09-20 18:08:36 +0500

    pyn-545 roperty management issue

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -152,7 +152,7 @@ class Credential < ApplicationRecord
 
   def change_to_scheduled_tours_for_sf
     community = self.community
-    if self.use_different_crm_provider && community.crm_credential.crm_provider == "salesforce"
+    if self.use_different_crm_provider && community&.crm_credential&.crm_provider == "salesforce"
       self.community.community_tour.update_columns(only_scheduled_tour: true)
       self.community.update_columns(scheduler_widget: false)
     end
