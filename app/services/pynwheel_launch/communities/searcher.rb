c1b63dd3707902b8e4fc311e51ed671eee5f195e
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2022-09-22 21:18:21 +0500

    remove comment

--- app/services/pynwheel_launch/communities/searcher.rb
+++ app/services/pynwheel_launch/communities/searcher.rb
@@ -16,7 +16,6 @@ attr_reader :user , :params
       communities = user&.community_users
     elsif user.is_super_admin?
       communities = CommunityUser.all
-      # communities = CommunityUser.where(community_id: 1618)
     elsif user.is_regional_admin?
       ids = user&.region&.communities&.ids
       communities = CommunityUser.where(community_id: ids)
