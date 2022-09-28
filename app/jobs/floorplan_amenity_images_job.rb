0bd1e908f3a8ed92c0bc2d1ebd9166a301e89a79
Salahudin Sallu <salahudin@pynwheel.com>
2022-09-28 17:07:46 +0500

    Logs

--- app/jobs/floorplan_amenity_images_job.rb
+++ app/jobs/floorplan_amenity_images_job.rb
@@ -13,7 +13,19 @@ class FloorplanAmenityImagesJob < ApplicationJob
     else
       floorplan_units = Unit.all.where(floorplan_id: floorplan.provider_floorplan_id, community_id: community_id) rescue nil
       floorplan_units.each do |floorplan_unit|
-        floorplan_unit.amenities.create!(image: source, name: name, floorplan_amenity_id: amenity.id)
+        
+        begin
+          puts "\n\n\n\n -------------------------------------"
+          puts success
+          puts "\n\n\n\n -------------------------------------"
+
+          floorplan_unit.amenities.create!(image: source, name: name, floorplan_amenity_id: amenity.id)
+        rescue => error
+          puts "\n\n\n\n -------------------------------------"
+          puts error.inspect
+          puts "\n\n\n\n -------------------------------------"
+        end
+
       end
     end
   end
