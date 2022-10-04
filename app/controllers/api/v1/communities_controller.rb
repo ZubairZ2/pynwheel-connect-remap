8ead0e34a99982750975d2ec72359b87532beff2
Salahudin Sallu <salahudin@pynwheel.com>
2022-10-04 19:46:41 +0500

    pyn-546 CMS favorite settings keword updated

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -74,12 +74,12 @@ class Api::V1::CommunitiesController < ActionController::Base
   def email_favorites
     begin
       if !@community.favorite_setting.present? || @community.favorite_setting.email_from.blank?
-        render :json=> {:success=>false, :message => "Please specify sender email address in CMS first. Email from can't be empty.", :operation => "email favorites"}
+        render :json=> {:success=>false, :message => "Please specify sender email address in CMS first. Email from can't be empty.", :operation => "email #{@community.favorites_page_name}"}
       else
         if @community.email_favorites(params)
-          render :json=> {:success=>true, :message => "success", :operation => "email favorites"}
+          render :json=> {:success=>true, :message => "success", :operation => "email #{@community.favorites_page_name}"}
         else
-          render :json=> {:success=>false, :message => "No valid favorites present"}
+          render :json=> {:success=>false, :message => "No valid #{@community.favorites_page_name} present"}
         end
       end
     rescue Exception => e   
--- app/controllers/favorite_images_controller.rb
+++ app/controllers/favorite_images_controller.rb
@@ -13,7 +13,7 @@ class FavoriteImagesController < ApplicationController
   def create
     @favorite = @community.build_favorite_setting(favorite_params)
     if @favorite.save
-      flash[:notice] = "Favorite settings created successfully."
+      flash[:notice] = "#{@community.favorites_page_name} settings created successfully."
       redirect_to community_favorite_settings_path(@community)
     else
       flash[:error] = @favorite.errors.full_messages.join(',')
@@ -24,7 +24,7 @@ class FavoriteImagesController < ApplicationController
   def update
     @favorite = @community.favorite_setting
     if @favorite.update(favorite_params)
-      flash[:notice] = "Favorite settings updated successfully."
+      flash[:notice] = "#{@community.favorites_page_name} settings updated successfully."
       redirect_to community_favorite_settings_path(@community)
     else
       flash[:error] = @favorite.errors.full_messages.join(',')
