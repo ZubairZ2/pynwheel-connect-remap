2d73f34e1b45c9a6c10140f88c01bd3ddf098a4b
Muhammad Fahad Umer <fahad@pynwheel.com>
2022-10-12 15:13:27 +0500

    Adjust ths size of logo in webpages

--- app/assets/javascripts/webpages.js
+++ app/assets/javascripts/webpages.js
@@ -235,7 +235,7 @@ $(window).bind('load', function () {
     webpagePanZoom = panzoom($area, 
       {
         bounds: true, contain: 'automatic', smoothScroll: true,
-        boundsPadding: 0.325,
+        boundsPadding: 0.4,
         maxZoom: 5,
         minZoom: 1,
         minScale: 1,
