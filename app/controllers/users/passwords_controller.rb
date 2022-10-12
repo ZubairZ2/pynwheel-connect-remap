b2b0dfd250ccc5a153e46ec25eeb6d1f524bf420
Muhammad Fahad Umer <fahad@pynwheel.com>
2022-10-12 16:26:08 +0500

    Restrict user not reset password if account is not setup

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,5 +1,22 @@
 class Users::PasswordsController < Devise::PasswordsController
 
+  def create
+    email = params[:user][:email]
+    if email.present?
+      user = User.find_by(email: email)
+      if user.present?
+        if user.first_name.nil?
+          redirect_to :back
+          flash[:alert] = 'You account is not setup yet.'
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+  end
+
   def after_resetting_password_path_for(resource)
     if resource.pynwheel_launch_access && resource.pynwheel_connect_access
       "#{root_url}access_selection.html"
