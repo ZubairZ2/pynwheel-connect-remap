457d60818eb06cc29a89d98f91c93199e592f79d
Salahudin Sallu <salahudin@pynwheel.com>
2022-11-02 16:16:48 +0500

    pyn-582 data priorities updates

--- app/services/data_providers_service.rb
+++ app/services/data_providers_service.rb
@@ -1,8 +1,9 @@
 class DataProvidersService
   def initialize provider
-    @communities = Community.where.not(data_provider: nil, company_id: 44)
+    # remove pynwheel Inc and Prometheous company
+    @communities = Community.where.not(data_provider: nil, company_id: [44, 423])
     @communities = @communities.where(data_provider: provider, locked: false)
-    @communities = @communities.where(data_provider_updated_on: "Never") + @communities.where.not(data_provider_updated_on: "Never").order(:data_provider_updated_on) #first + second priority
+    @communities = @communities.where.not(data_provider_updated_on: "Never").order(:data_provider_updated_on) + @communities.where(data_provider_updated_on: "Never")
   end
 
   def sync_psi_data
