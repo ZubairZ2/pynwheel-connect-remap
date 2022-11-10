1bef3fb53236854bc99a4f2b76dc5f9d0c78fb23
Salahudin Sallu <salahudin@pynwheel.com>
2022-11-10 17:01:00 +0500

    pyn-582 update priority

--- app/services/data_providers_service.rb
+++ app/services/data_providers_service.rb
@@ -3,7 +3,7 @@ class DataProvidersService
     # remove pynwheel Inc and Prometheous company
     @communities = Community.where.not(data_provider: nil, company_id: [44, 423])
     @communities = @communities.where(data_provider: provider, locked: false)
-    # @communities = @communities.where.not(data_provider_updated_on: "Never").order(:data_provider_updated_on) + @communities.where(data_provider_updated_on: "Never")
+    @communities = @communities.where(data_provider_updated_on: "Never") + @communities.where.not(data_provider_updated_on: "Never").order(:data_provider_updated_on)
   end
 
   def sync_psi_data
@@ -11,7 +11,7 @@ class DataProvidersService
     @communities.each do |community|
       puts "\n\n --------- Started updating for community: #{community.id}, Last updated on:  #{community.data_provider_updated_on.to_s} ------------- \n\n"
       ImportPsiDataJob.perform_async(community.credential.attributes.to_json)
-      sleep 60
+      ENV["SLEEP_TIME"].to_i
     end
   end
 
@@ -20,7 +20,7 @@ class DataProvidersService
     @communities.each do |community|
       puts "\n\n --------- Started updating for community: #{community.id}, Last updated on:  #{community.data_provider_updated_on.to_s} ------------- \n\n"
       ((community.credential.resman_api_version === "GetMarketing4_0") ? ImportResman4DataJob.perform_async(community.credential.attributes.to_json) : ImportResmanDataJob.perform_async(community.credential.attributes.to_json))
-      sleep 60
+      ENV["SLEEP_TIME"].to_i
     end
   end
 
@@ -29,7 +29,7 @@ class DataProvidersService
     @communities.each do |community|
       puts "\n\n --------- Started updating for community: #{community.id}, Last updated on:  #{community.data_provider_updated_on.to_s} ------------- \n\n"
       community.credential.url.include?("20") ? ImportYardi2DataJob.perform_async(community.credential.attributes.to_json) : ImportYardi4DataJob.perform_async(community.credential.attributes.to_json)
-      sleep 60
+      ENV["SLEEP_TIME"].to_i
     end
   end
 
@@ -38,7 +38,7 @@ class DataProvidersService
     @communities.each do |community|
       puts "\n\n --------- Started updating for community: #{community.id}, Last updated on:  #{community.data_provider_updated_on.to_s} ------------- \n\n"
       ImportYardirentcafeDataJob.perform_async community.credential.attributes.to_json
-      sleep 60
+      ENV["SLEEP_TIME"].to_i
     end
   end
   
@@ -47,7 +47,7 @@ class DataProvidersService
     @communities.each do |community|
       puts "\n\n --------- Started updating for community: #{community.id}, Last updated on:  #{community.data_provider_updated_on.to_s} ------------- \n\n"
       ImportRealpageSvcDataJob.perform_async community.credential.attributes.to_json
-      sleep 60
+      ENV["SLEEP_TIME"].to_i
     end
   end
 
@@ -69,6 +69,4 @@ class DataProvidersService
       ImportXmlDataJob.perform_async community.credential.attributes.to_json
     end
   end
-
-
 end
