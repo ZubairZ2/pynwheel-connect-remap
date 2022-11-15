14bbc88edfa2cead5b3cf7f5952e6e83317f1dd7
Salahudin Sallu <salahudin@pynwheel.com>
2022-11-15 14:07:24 +0500

    pyn-582 remove today comm

--- app/services/data_providers_service.rb
+++ app/services/data_providers_service.rb
@@ -3,8 +3,7 @@ class DataProvidersService
     # remove pynwheel Inc and Prometheous company
     @communities = Community.where.not(data_provider: nil, company_id: [44, 423])
     @communities = @communities.where(data_provider: provider, locked: false)
-    @communities = @communities.where("data_provider_updated_on < ? ", Date.today)
-    @communities = @communities.where.not(data_provider_updated_on: "Never").order(:data_provider_updated_on) + @communities.where(data_provider_updated_on: "Never")
+    @communities = @communities.where.not(data_provider_updated_on: "Never").where("data_provider_updated_on < ? ", Date.today).order(:data_provider_updated_on) + @communities.where(data_provider_updated_on: "Never")
   end
 
   def sync_psi_data
