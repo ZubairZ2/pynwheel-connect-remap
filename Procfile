4cad7667bcb9d3aaa7124fcd65e7e3674e4503d6
Salahudin Sallu <salahudin@pynwheel.com>
2022-11-19 17:49:27 +0500

    pyn-582 sidekiq and proc updated

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-bundle exec puma -p $PORT -t 1:5 -w 2
+# bundle exec puma -p $PORT -t 1:5 -w 2
+web: bundle exec rails server -p ${PORT:-3000}
+redis: redis-server /usr/local/etc/redis.conf
+sidekiq_worker: bundle exec sidekiq -C ./config/sidekiq.yml
--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,16 @@ module PynwheelCms
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.active_job.queue_adapter = :sucker_punch
+
+    # config.active_jobs.running_on[:sucker_punch] = [ MyJob, YourJob, HerJob ]
+    # config.active_jobs.running_on[:resque] = [RealPageDataUpdateWorker, YardirentcafeDataUpdateWorker, EntrataDataUpdateWorker, YardiDataUpdateWorker ]
+
+    # config.active_job.queue_adapter = :sucker_punch
+    config.active_job.queue_adapter = :sidekiq
+
+    config.action_dispatch.rack_cache = true
+    config.cache_store = :redis_store, ENV["REDIS_URL"], { expires_in: 120.minutes }
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
