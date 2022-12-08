9c6df728f638e1f56225652829167d6c0c85ef26
Salahudin Sallu <salahudin@pynwheel.com>
2022-12-08 17:55:34 +0500

    pyn-664 fixed grace period issue

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -527,8 +527,8 @@ class Api::V1::CommunitiesController < ActionController::Base
             end
           end
           last_visit = get_last_visited_community(@scheduled_tours)
-          data = { tour_user: @tour_user, last_visit: last_visit, upcoming: upcoming_tours.uniq, completed: completed_tours.uniq, exipred: expired_tours.uniq }
-          render :json=> { data: data.as_json, :status=> true, :message => "data returned succesfully", code: 200 }
+          data = {tour_user: @tour_user, last_visit: last_visit, upcoming: upcoming_tours.uniq, completed: completed_tours.uniq, exipred: expired_tours.uniq }
+          render :json=> {data: data.as_json, :status=> true, :message => "data returned succesfully", code: 200 }
         else
           data = { tour_user: @tour_user, last_visit: last_visit, upcoming: upcoming_tours, completed: completed_tours, exipred: expired_tours }
           render :json=> { data: data.as_json, :status=>true, code: 200 }
@@ -1388,6 +1388,7 @@ class Api::V1::CommunitiesController < ActionController::Base
   private
 
   def get_community_tour(tour)
+
     if !tour.tour_date.nil?
       d = tour.tour_date
       t = tour.tour_time
@@ -1395,8 +1396,9 @@ class Api::V1::CommunitiesController < ActionController::Base
     else
       dt = DateTime.now
     end
+
     tour_type = tour.tour_type.eql?("") ? tour.property_tour_type : tour.tour_type
-    return {schedule_tour_id: tour.id, tour_type: tour_type, tour_time: dt, community: tour.community}
+    return {grace_period: tour&.community&.tour&.grace_period, schedule_tour_id: tour.id, tour_type: tour_type, tour_time: dt, community: tour.community}
   end
 
   def get_last_visited_community(scheduled_tours)
@@ -1416,7 +1418,7 @@ class Api::V1::CommunitiesController < ActionController::Base
   def date_compare(tour)
     if tour.present?
       if (tour.tour_date && tour.tour_time).present?
-        (tour.tour_date.to_s + " " + tour.tour_time.strftime("%I:%M%p")).in_time_zone(tour&.community&.get_time_zone()) < Time.now.in_time_zone(tour&.community&.get_time_zone())
+        ( (tour.tour_date.to_s + " " + tour.tour_time.strftime("%I:%M%p")).in_time_zone(tour&.community&.get_time_zone()) + (tour.community.tour.grace_period.minutes) + 1.minute) <= (Time.now.in_time_zone(tour&.community&.get_time_zone()))
       else
         return false
       end
