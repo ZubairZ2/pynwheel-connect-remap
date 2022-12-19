c80ac72662c211e810c148b9bbf0c831c7663d91
Salahudin Sallu <salahudin@pynwheel.com>
2022-12-19 15:59:13 +0500

    pyn-667 rollback swap data for yardi provider

--- app/services/yardi4_swap_service.rb
+++ app/services/yardi4_swap_service.rb
@@ -69,7 +69,7 @@ class Yardi4SwapService < BaseService
           end
           update_yardi4_floorplans(floorplans)
           update_yardi4_units(ils_units,external_property_id)
-          # rename_provider
+          rename_provider
           #else
           #Thread.current[:errors] << "Invalid credentials.Please enter correct one and try again."
           #ExceptionNotifier.notify_exception(Exception.new,data: {message: "Invalid credentials.Please enter correct one and try again.",community_id: credentials.community_id})
@@ -90,9 +90,10 @@ class Yardi4SwapService < BaseService
       if unit.count > 1
         unit = Unit.where(community_id: credentials.community_id, property_id: property_id, marketing_name: (u[:Units][:Unit][:Identification][0][:IDValue] rescue u[:Units][:Unit][:Identification][0][0][:IDValue]) ,floorplan_id: Floorplan.find_by(name: u[:Units][:Unit][:FloorplanName]).provider_floorplan_id)
       end
+
       if unit.present?
         unit = unit.first
-        unit.provider = "yardi"
+        unit.provider = "yardi_new"
         unit.provider_unit_id = "#{u[:Units][:Unit][:Identification][0][:IDValue]}-#{property_id}" rescue "#{u[:Units][:Unit][:Identification][0][0][:IDValue]}-#{property_id}"
         unit.property_id = property_id
         #unit.provider_unit_id = u["Units"]["Unit"]["Identification"]["IDValue"]
@@ -140,12 +141,14 @@ class Yardi4SwapService < BaseService
         # unit = Unit.where(community_id: credentials.community_id).first
         provider_unit_id = "#{u[:Units][:Unit][:Identification][0][:IDValue]}-#{property_id}" rescue "#{u[:Units][:Unit][:Identification][0][0][:IDValue]}-#{property_id}"
         dup = Unit.find_by(community_id: credentials.community_id, property_id: property_id, provider_unit_id: provider_unit_id)
+        
         if dup.present?
           dup.destroy
         end
+
         unit = Unit.new
         unit.community_id = credentials.community_id
-        unit.provider = "yardi"
+        unit.provider = "yardi_new"
         unit.property_id = property_id
         unit.provider_unit_id = provider_unit_id
         unit.unit_type = (u[:Units][:Unit][:Identification][0][:IDValue] rescue u[:Units][:Unit][:Identification][0][0][:IDValue])
@@ -207,7 +210,7 @@ class Yardi4SwapService < BaseService
         fp = fp.first
         rooms = []
         floorplan.each do |f|
-          fp.provider = "yardi"
+          fp.provider = "yardi_new"
           fp.provider_floorplan_id = floorplan[0][:IDValue]
           if f.key?(:Room)
             rooms << f
@@ -250,7 +253,7 @@ class Yardi4SwapService < BaseService
         # fp = Floorplan.where(community_id: credentials.community_id).first
         fp = Floorplan.new
         fp.community_id = credentials.community_id
-        fp.provider = "yardi"
+        fp.provider = "yardi_new"
         rooms = []
         floorplan.each do |f|
           fp.provider_floorplan_id = floorplan[0][:IDValue]
