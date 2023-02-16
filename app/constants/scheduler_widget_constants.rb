0d1b42c6f3707eb5047c84b9d4b6488a2d8090da
Salahudin Sallu <salahudin@pynwheel.com>
2023-02-16 16:29:28 +0500

    pyn-713 syntax fix

--- app/constants/scheduler_widget_constants.rb
+++ app/constants/scheduler_widget_constants.rb
@@ -76,7 +76,7 @@ class SchedulerWidgetConstants
       key: 4,
       image: "instruction-4.png",
       title: "Apply for a Lease or Start Chat",
-      text: "Find the home you want? Apply for a lease, or talk to property staff directly via live chat."
+      text: "Find the home you want? Apply for a lease, or talk to property staff directly via live chat.",
       background: "#fcf0ff"
     }
   ]
