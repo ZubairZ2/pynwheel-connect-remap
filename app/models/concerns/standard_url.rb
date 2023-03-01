cf4aae4024e147b8c50d344b6acabccd694a5b34
Salahudin Sallu <salahudin@pynwheel.com>
2023-03-01 13:00:22 +0500

    pyn-725 uploading svg

--- app/models/concerns/standard_url.rb
+++ app/models/concerns/standard_url.rb
@@ -5,7 +5,7 @@ module StandardUrl
     model = klass.constantize.find(id)
     model.update_attribute(:standard_image_url, model.image.url) 
     if klass == "Floorplate"
-      # model.update_attribute(:svg_image_url, model.image.url(:svg_for_metro)) if model.image.url(:svg_for_metro).present?
+      model.update_attribute(:svg_image_url, model.image.url(:svg_for_metro)) if model.image.url(:svg_for_metro).present?
     end
 
     if klass == "HomePageImage"
