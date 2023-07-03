e50707302edeb9824896bcc7540b83b5ca860d73
Salahudin Sallu <salahudin@pynwheel.com>
2023-07-03 13:19:52 +0500

    Redis TLS added for heroku mini plan

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,11 @@
 if Rails.env == 'production'
-  uri = URI.parse(ENV["REDIS_URL"])
+  uri = URI.parse(ENV["REDIS_TLS_URL"])
   Resque.redis = Redis.new(
     host:     uri.host,
     port:     uri.port,
     password: uri.password,
   )
 elsif Rails.env == 'development'
-  uri = URI.parse(ENV["REDIS_URL"])
+  uri = URI.parse(ENV["REDIS_TLS_URL"])
   Resque.redis = Redis.new(host: uri.host)
 end
