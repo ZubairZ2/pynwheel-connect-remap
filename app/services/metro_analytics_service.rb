1a30ef3971aeafd74411a5f8d0cefb633ce06988
Salahudin Sallu <salahudin@pynwheel.com>
2023-07-18 19:58:30 +0500

    pyn-831 favorites spelling fix for american english

--- app/services/metro_analytics_service.rb
+++ app/services/metro_analytics_service.rb
@@ -25,7 +25,7 @@ class MetroAnalyticsService
     visited_pages.concat( (1..params["ApartmentsPageVisited"]).map{ "Apartments Page".upcase } )
     visited_pages.concat( (1..params["GalleryPageVisited"]).map{ "Gallery Page".upcase } )
     visited_pages.concat( (1..params["NeighbourhoodPageVisited"]).map{ "Neighbourhood Page".upcase } )
-    visited_pages.concat( (1..params["FavouritesPageVisited"]).map{ "Favourites Page".upcase } )
+    visited_pages.concat( (1..params["FavouritesPageVisited"]).map{ "Favorites Page".upcase } )
 
     visited_pages
   end
