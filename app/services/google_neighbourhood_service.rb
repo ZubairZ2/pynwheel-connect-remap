c7eed4f0cee79c92d57e659b667a0ff1723895f0
Salahudin Sallu <salahudin@pynwheel.com>
2023-08-16 19:40:55 +0500

    pyn-843 nearby api updated to rankby

--- app/services/google_neighbourhood_service.rb
+++ app/services/google_neighbourhood_service.rb
@@ -50,7 +50,7 @@ class GoogleNeighbourhoodService
   end
 
   def fetch_neighbourhood_data
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?types=#{@params[:cat]}&location=#{@params[:latitude]},#{@params[:longitude]}&radius=#{@params[:radius]}&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?types=#{@params[:cat]}&location=#{@params[:latitude]},#{@params[:longitude]}&rankby=distance&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
     fetch_recursive(url, 1)
     @results
 
