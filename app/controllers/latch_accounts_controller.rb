5638dc7335003c63e1c0b064e3cb9ce69a2d3eda
Salahudin Sallu <salahudin@pynwheel.com>
2023-08-17 16:40:58 +0500

    pyn-847 latch accounts controller code improvements

--- app/controllers/latch_accounts_controller.rb
+++ app/controllers/latch_accounts_controller.rb
@@ -1,77 +1,61 @@
 class LatchAccountsController < ApplicationController
-  # include Error::ErrorHandler
-  before_action :set_user
+  before_action :set_latch_account, except: [:new, :remove_latch_locks]
 
   def new
     @latch = Latch.new
   end
-  
+
   def create
-    locks_provider = current_community.multiple_locks_provider
-    locks_provider << "Latch" unless locks_provider.include?("Latch")
-    unless @is_already_exists
-      @latch = Latch.new(latch_params)
-      result = parse_csv(params[:file]) if params[:file].present?
-      if @latch.save
-        current_community.update_columns(:multiple_locks_provider => locks_provider)
-        flash[:notice] = "Latch credentails saved successfully"
-        redirect_to new_community_dwelo_path
-      else
-        flash[:error] = @latch.errors.full_messages.join(',')
-        redirect_to new_community_dwelo_path
-      end
+    if @latch.update(latch_params)
+      add_lock_provider
+      flash[:notice] = "Latch credentials saved successfully"
     else
-      @latch = Latch.find_by(community_id: current_community.id)
-      result = parse_csv(params[:file]) if params[:file].present?
-      if @latch.update_attributes(latch_params)
-        current_community.update_columns(:multiple_locks_provider => locks_provider)
-        flash[:notice] = "Latch credentails updated successfully"
-        redirect_to new_community_dwelo_path
-      else
-        flash[:error] = @latch.errors.full_messages.join(',')
-        redirect_to new_community_dwelo_path
-      end
+      flash[:error] = @latch.errors.full_messages.join(', ')
     end
+    redirect_to new_community_dwelo_path
   end
 
   def upload_lock_image
     return unless params[:lock_image].present?
-    @latch = current_community.latch || Latch.new(community_id: current_community.id)
+
     @latch.update(lock_image: params[:lock_image])
   end
 
   def remove_latch_locks
-    if current_community.latch.present?
-      if current_community.latch.latch_locks.present?
-        current_community.latch.latch_locks.destroy_all
-        flash[:notice] = "Locks deleted successfully"
-        redirect_to new_community_dwelo_path(current_community)
-      else
-        flash[:error] = "No locks are present"
-        redirect_to new_community_dwelo_path(current_community)
-      end
+    if current_community.latch&.latch_locks&.destroy_all
+      flash[:notice] = "Locks deleted successfully"
     else
-      flash[:error] = "Credentials for Latch are missing"
-      redirect_to new_community_dwelo_path(current_community)
+      flash[:error] = current_community.latch.present? ? "No locks are present" : "Credentials for Latch are missing"
     end
-  end
 
-  def parse_csv(file)
-    @latch.import_data(file)
+    redirect_to new_community_dwelo_path(current_community)
   end
 
   def destroy
-    @latch  = EdgeState.find(params[:id])
+    @latch = Latch.find(params[:id])
     @latch.destroy
+    redirect_to new_community_dwelo_path(current_community), notice: 'Latch account successfully destroyed.'
   end
 
   private
 
-  def latch_params
-    params.require(:latch).permit(:passwordless_client_id, :passwordless_client_secret,:client_id, :client_secret, :community_id, :lock_instruction_text)
+  def add_lock_provider
+    locks_provider = current_community.multiple_locks_provider
+
+    unless locks_provider.include?("Latch")
+      locks_provider << "Latch"
+      current_community.update_columns(multiple_locks_provider: locks_provider)
+    end
   end
 
-  def set_user
-    @is_already_exists = Latch.find_by(community_id: current_community.id).present?
+  def set_latch_account
+    @latch = Latch.find_or_initialize_by(community_id: current_community.id)
+  end
+
+  def latch_params
+    params.require(:latch).permit(
+      :passwordless_client_id, :passwordless_client_secret,
+      :client_id, :client_secret, :community_id, :lock_instruction_text
+    )
   end
 end
--- app/models/latch.rb
+++ app/models/latch.rb
@@ -16,110 +16,110 @@ class Latch < ApplicationRecord
     return LATCH
   end
 
-  def import_data(file)
-      Thread.new do
-          execution_context = Rails.application.executor.run!
-          clear_locks_provider(community)
-          begin
-              if file.path.split('.').last.include?("csv")
-                  names_with_ids = Community.pluck(:id,:name).map{|x| [x[0], x[1].downcase.gsub(/([-() ])/, '')]}
-                  CSV.foreach(file.path, headers: true) do |row|
-                      community = Community.find_by_name row[0]
-                      if community.present?
-                          save_lock_info(community,row)
-                      else
-                          id = names_with_ids.map{|n| n[0] if n[1] == row[0].downcase.gsub(/([-() ])/, '')}.compact
-                          community = Community.find_by_id id.first
-                          if community.present?
-                              save_lock_info(community, row)
-                          end
-                      end
-                  end
-                  notify_pusher({success: "Locks are imported successfully."})
-              else
-                  notify_pusher({error: "Invalid File"})
-              end
-          rescue => exception
-              puts "--------------------------- an error occured -----------------------------"
-              notify_pusher({error: "Error in parsing " + "the uploaded CSV" + " file."})
-          end
-      ensure
-          execution_context.complete! if execution_context
-      end
-  end
+#   def import_data(file)
+#       Thread.new do
+#           execution_context = Rails.application.executor.run!
+#           clear_locks_provider(community)
+#           begin
+#               if file.path.split('.').last.include?("csv")
+#                   names_with_ids = Community.pluck(:id,:name).map{|x| [x[0], x[1].downcase.gsub(/([-() ])/, '')]}
+#                   CSV.foreach(file.path, headers: true) do |row|
+#                       community = Community.find_by_name row[0]
+#                       if community.present?
+#                           save_lock_info(community,row)
+#                       else
+#                           id = names_with_ids.map{|n| n[0] if n[1] == row[0].downcase.gsub(/([-() ])/, '')}.compact
+#                           community = Community.find_by_id id.first
+#                           if community.present?
+#                               save_lock_info(community, row)
+#                           end
+#                       end
+#                   end
+#                   notify_pusher({success: "Locks are imported successfully."})
+#               else
+#                   notify_pusher({error: "Invalid File"})
+#               end
+#           rescue => exception
+#               puts "--------------------------- an error occured -----------------------------"
+#               notify_pusher({error: "Error in parsing " + "the uploaded CSV" + " file."})
+#           end
+#       ensure
+#           execution_context.complete! if execution_context
+#       end
+#   end
 
-  def save_lock_info(community, lock_data)
-      lock_name = lock_data[1]; lock_id = lock_data[2]; stop_data = lock_data[3]; door_uuid = lock_data[4];
+#   def save_lock_info(community, lock_data)
+#       lock_name = lock_data[1]; lock_id = lock_data[2]; stop_data = lock_data[3]; door_uuid = lock_data[4];
 
-      data = parse_stop(stop_data)
-      lock = community.latch.latch_locks.find_by(door_uuid: door_uuid) rescue nil
+#       data = parse_stop(stop_data)
+#       lock = community.latch.latch_locks.find_by(door_uuid: door_uuid) rescue nil
 
-      if lock_name.present? and lock_id.present? and door_uuid.present?
-          if lock.nil? and data.nil?
-              LatchLock.create(door_uuid: door_uuid, lock_name: lock_name, latch_id: community.latch.id, lock_id: lock_id)
-          elsif lock.present? and data.nil?
-              lock.update_attributes(lock_id: lock_id, lock_name: lock_name, latch_id: community.latch.id)
-          else  # (lock.nil? and data.present?) or (lock.present? and data.present?)
-              data.latch_locks.map{|l| l.update_attributes(stop_type: nil, stop_id: nil)}
-              lock.destroy if lock.present?
-              data.latch_locks.create(door_uuid: door_uuid, lock_name: lock_name, latch_id: community.latch.id, lock_id:lock_id)
-              data.update_column(:lock_provider, "Latch")
-          end
-      end
-  end
-
-
-    def clear_locks_provider(community)
-        community.units.where(lock_provider: "Latch").update_all(lock_provider: "")
-        community.amenities.where(lock_provider: "Latch").update_all(lock_provider: "")
-        community.elevators.where(lock_provider: "Latch").update_all(lock_provider: "")
-        community.building_starting_point.where(lock_provider: "Latch").update_all(lock_provider: "")
-        community.community_tour.update(lock_provider: "") if community.community_tour.lock_provider === "Latch"
-        community.doors.where(lock_provider: "Latch").update_all(lock_provider: "")
-    end
+#       if lock_name.present? and lock_id.present? and door_uuid.present?
+#           if lock.nil? and data.nil?
+#               LatchLock.create(door_uuid: door_uuid, lock_name: lock_name, latch_id: community.latch.id, lock_id: lock_id)
+#           elsif lock.present? and data.nil?
+#               lock.update_attributes(lock_id: lock_id, lock_name: lock_name, latch_id: community.latch.id)
+#           else  # (lock.nil? and data.present?) or (lock.present? and data.present?)
+#               data.latch_locks.map{|l| l.update_attributes(stop_type: nil, stop_id: nil)}
+#               lock.destroy if lock.present?
+#               data.latch_locks.create(door_uuid: door_uuid, lock_name: lock_name, latch_id: community.latch.id, lock_id:lock_id)
+#               data.update_column(:lock_provider, "Latch")
+#           end
+#       end
+#   end
 
 
-  def parse_stop(stop_name)
-      data=nil
+#     def clear_locks_provider(community)
+#         community.units.where(lock_provider: "Latch").update_all(lock_provider: "")
+#         community.amenities.where(lock_provider: "Latch").update_all(lock_provider: "")
+#         community.elevators.where(lock_provider: "Latch").update_all(lock_provider: "")
+#         community.building_starting_point.where(lock_provider: "Latch").update_all(lock_provider: "")
+#         community.community_tour.update(lock_provider: "") if community.community_tour.lock_provider === "Latch"
+#         community.doors.where(lock_provider: "Latch").update_all(lock_provider: "")
+#     end
 
-      if stop_name.include?('-')
-          building_name, unit_name = stop_name.split('-',2)
-      else
-          building_name = nil
-          unit_name = stop_name
-      end
 
-      if building_name == nil
-          building_name_ = ''
-      else
-          building_name_ = building_name
-      end
+#   def parse_stop(stop_name)
+#       data=nil
 
-      data = community.units.where('(marketing_name = ? or provider_unit_id = ?) and (building = ? or building = ?)', unit_name, unit_name, building_name, building_name_).first rescue nil
-      data = community.units.where('(marketing_name = ? or provider_unit_id = ?) and (building = ? or building = ?)', stop_name, stop_name, nil, '').first rescue nil unless data.present?
+#       if stop_name.include?('-')
+#           building_name, unit_name = stop_name.split('-',2)
+#       else
+#           building_name = nil
+#           unit_name = stop_name
+#       end
 
-      data = community.doors.where(name: stop_name).first if data.nil?
-      data = community.amenities.where(name: stop_name).first if data.nil?
-      data = community.elevators.where(name: stop_name).first if data.nil?
-      data = community.building_starting_point.where(name: stop_name).first if data.nil?
-      data = community.community_tour.name == stop_name ? community.community_tour : nil if data.nil?
+#       if building_name == nil
+#           building_name_ = ''
+#       else
+#           building_name_ = building_name
+#       end
 
-      if data.nil? and stop_name.count('-') > 1
-          (stop_name.count('-')+1).times.each do |i|
-              if i >= 2
-                  building = (stop_name.split('-', i+1).first ((stop_name.split('-', i+1).length) -1)).join('-')
-                  unit_name = stop_name.split('-', i+1).last
-                  data = community.units.where('(marketing_name = ? or provider_unit_id = ?) and building = ?', unit_name, unit_name, building).first rescue nil
-              end
-              break if data.present?
-          end
-      end
+#       data = community.units.where('(marketing_name = ? or provider_unit_id = ?) and (building = ? or building = ?)', unit_name, unit_name, building_name, building_name_).first rescue nil
+#       data = community.units.where('(marketing_name = ? or provider_unit_id = ?) and (building = ? or building = ?)', stop_name, stop_name, nil, '').first rescue nil unless data.present?
 
-      return data
-  end
+#       data = community.doors.where(name: stop_name).first if data.nil?
+#       data = community.amenities.where(name: stop_name).first if data.nil?
+#       data = community.elevators.where(name: stop_name).first if data.nil?
+#       data = community.building_starting_point.where(name: stop_name).first if data.nil?
+#       data = community.community_tour.name == stop_name ? community.community_tour : nil if data.nil?
 
-  def notify_pusher(response)
-      channel_name = "latch_channel_for_" + (self.community.name.gsub(/[^0-9a-z ]/i, '') + "_with_id_" + self.community.id.to_s).gsub(' ', '_')
-      Pusher.trigger(channel_name, 'data-import', response.as_json)
-  end
+#       if data.nil? and stop_name.count('-') > 1
+#           (stop_name.count('-')+1).times.each do |i|
+#               if i >= 2
+#                   building = (stop_name.split('-', i+1).first ((stop_name.split('-', i+1).length) -1)).join('-')
+#                   unit_name = stop_name.split('-', i+1).last
+#                   data = community.units.where('(marketing_name = ? or provider_unit_id = ?) and building = ?', unit_name, unit_name, building).first rescue nil
+#               end
+#               break if data.present?
+#           end
+#       end
+
+#       return data
+#   end
+
+#   def notify_pusher(response)
+#       channel_name = "latch_channel_for_" + (self.community.name.gsub(/[^0-9a-z ]/i, '') + "_with_id_" + self.community.id.to_s).gsub(' ', '_')
+#       Pusher.trigger(channel_name, 'data-import', response.as_json)
+#   end
 end
