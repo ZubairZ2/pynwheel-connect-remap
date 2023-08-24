40882d7e4cdd881b90548e48b242c501f3df3bd3
Salahudin Sallu <salahudin@pynwheel.com>
2023-08-24 19:58:17 +0500

    pyn-858 save favorites on share link fix

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -222,10 +222,18 @@ class WebpagesController < ActionController::Base
   end
 
   def favorites_share_link
-    @favorite = Favorite.find_by_session_id(params[:session_id])
-    @units = Unit.where(id: @favorite&.unit_ids,community_id: params[:community_id]).where.not(available_date: nil)
-    @fav_units_info = @units.to_json
-    @floorplans = Floorplan.where(provider_floorplan_id: @units && @units.map(&:floorplan_id),community_id: params[:community_id])
+    # @favorite = Favorite.find_by_session_id(params[:session_id])
+    # @units = Unit.where(id: @favorite&.unit_ids,community_id: params[:community_id]).where.not(available_date: nil)
+    # @fav_units_info = @units.to_json
+    # @floorplans = Floorplan.where(provider_floorplan_id: @units && @units.map(&:floorplan_id),community_id: params[:community_id]) 
+    begin
+      @scheduler_widget_link = get_scheduler_link
+      @favorite = Favorite.find_by_session_id(cookies[:webpages_session_id])
+      @units = Unit.where(id: JSON.parse(cookies[:favorite_unit_ids]),community_id: params[:community_id]).where.not(available_date: nil)
+      @fav_units_info = @units.to_json
+      @floorplans = Floorplan.where(provider_floorplan_id: @units.map(&:floorplan_id),community_id: params[:community_id])
+    rescue => ex
+    end
   end
 
   def clear_favorites
