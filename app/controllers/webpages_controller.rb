424e6b282fc0f838363a5609186446823f3eff15
Salahudin Sallu <salahudin@pynwheel.com>
2023-09-04 19:59:46 +0500

    fix webpages_session_id issue

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -13,6 +13,7 @@ class WebpagesController < ActionController::Base
     # cookies.permanent[:webpages_session_id] = SecureRandom.hex(8) if is_cookies_session_nil
     if webpages_session_id.nil?
       webpages_session_id = SecureRandom.hex(8)
+      cookies.permanent[:webpages_session_id] = webpages_session_id
       Rails.cache.write('webpages_session_id', webpages_session_id)
     end
     # Favorite.create(session_id: cookies[:webpages_session_id],unit_ids: []) if is_cookies_session_nil
@@ -375,7 +376,7 @@ class WebpagesController < ActionController::Base
     Time.zone.now.utc.in_time_zone(@timezone)
   end
   def set_webpages_session_id
-    @webpages_session_id = Rails.cache.read('webpages_session_id') || cookies[:webpages_session_id]
+    @webpages_session_id = cookies[:webpages_session_id].present? ? cookies[:webpages_session_id] : Rails.cache.read('webpages_session_id')
   end
   def return_community_datetime(datetime)
     Time.zone.parse(datetime).in_time_zone(@timezone).to_datetime if datetime.present? && @timezone.present?
