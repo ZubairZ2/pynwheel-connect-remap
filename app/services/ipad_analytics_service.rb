925ca0a3c407fb5c6b88a858e57baa0bcf89304a
Salahudin Sallu <salahudin@pynwheel.com>
2023-09-19 16:09:41 +0500

    pyn-872 spelling correction

--- app/services/ipad_analytics_service.rb
+++ app/services/ipad_analytics_service.rb
@@ -25,7 +25,7 @@ class IpadAnalyticsService
       visited_pages = params["additionalPage"]
       visited_pages.concat( (1..params["ApartmentsPageVisited"]).map{ "Apartments Page".upcase } )
       visited_pages.concat( (1..params["GalleryPageVisited"]).map{ "Gallery Page".upcase } )
-      visited_pages.concat( (1..params["NeighbourhoodPageVisited"]).map{ "Neighbourhood Page".upcase } )
+      visited_pages.concat( (1..params["NeighbourhoodPageVisited"]).map{ "Neighborhood Page".upcase } )
       visited_pages.concat( (1..params["FavouritesPageVisited"]).map{ "Favorites Page".upcase } )
 
       visited_pages
