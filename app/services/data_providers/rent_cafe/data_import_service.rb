0aab848f6b4c2c6cdf72373f6df886fd1f9d0480
Salahudin Sallu <salahudin@pynwheel.com>
2023-11-15 17:23:04 +0500

    pyn-899 bad url fix

--- app/services/data_providers/rent_cafe/data_import_service.rb
+++ app/services/data_providers/rent_cafe/data_import_service.rb
@@ -169,7 +169,8 @@ module DataProviders
 
         def image_base64(image_url)
           return unless image_url.present?
-          uri = URI.parse(image_url)
+          encoded_url = URI.encode(image_url)
+          uri = URI.parse(encoded_url)
           file = uri.open
           image_data = file.read
           encoded_image = Base64.strict_encode64(image_data)
