a2034a68dd5b0790230308cc43d846f0a233c224
Salahudin Sallu <salahudin@pynwheel.com>
2023-11-21 18:53:59 +0500

    pyn-920 server error fixed

--- app/controllers/scheduler_widget/widgets_controller.rb
+++ app/controllers/scheduler_widget/widgets_controller.rb
@@ -66,21 +66,28 @@ class SchedulerWidget::WidgetsController < ApplicationController
     
     if @use_yardi_as_lead
       @yardi_time_slots = @community.available_slots(@schedule_tour)
-      if @yardi_time_slots.present?
-        if @community.credential.rentcafe_api_version == "RentCafe V2"
+      if @community.credential.rentcafe_api_version == "RentCafe V2"
+        if @yardi_time_slots.present?
           @yardi_self_time_slots = @yardi_time_slots.map{|x|  [x["startTime"].split(' ')[0],"#{x["startTime"].split(' ')[1]} #{x["startTime"].split(' ')[2]}","#{x["endTime"].split(' ')[1]} #{x["endTime"].split(' ')[2]}" ] if x['slotType'] == "SelfTour"}.compact
           @yardi_guided_time_slots = @yardi_time_slots.map{|x| [x["startTime"].split(' ')[0],"#{x["startTime"].split(' ')[1]} #{x["startTime"].split(' ')[2]}","#{x["endTime"].split(' ')[1]} #{x["endTime"].split(' ')[2]}" ] if x['slotType'] == "AgentGuided"}.compact
+          @time_slots = @community.collect_time_slots_for_yardi(@stepping, @yardi_self_time_slots, @yardi_guided_time_slots)
+          @yardi_enable_days = @time_slots.keys
         else
+          @time_slots = @reschedule_tour ? @community.collect_time_slots_for_rechedule_tours(@stepping,@tour_type) : @community.collect_time_slots(@stepping)
+          @yardi_enable_days = []
+          @use_yardi_as_lead = false
+        end
+      else
+        if @yardi_time_slots["Response"].present?
           @yardi_self_time_slots = @yardi_time_slots["Response"][0]["AvailableSlots"].map{|x| [x["dtStart"].split(' ')[0],x["dtStart"].split(' ')[1],x["dtEnd"].split(' ')[1]  ] if x['TypeofSlot'] == "SelfTour"}.compact
           @yardi_guided_time_slots = @yardi_time_slots["Response"][0]["AvailableSlots"].map{|x| [x["dtStart"].split(' ')[0],x["dtStart"].split(' ')[1],x["dtEnd"].split(' ')[1]  ] if x['TypeofSlot'] == "GuidedTour"}.compact
+          @time_slots = @community.collect_time_slots_for_yardi(@stepping, @yardi_self_time_slots, @yardi_guided_time_slots)
+          @yardi_enable_days = @time_slots.keys
+        else
+          @time_slots = @reschedule_tour ? @community.collect_time_slots_for_rechedule_tours(@stepping,@tour_type) : @community.collect_time_slots(@stepping)
+          @yardi_enable_days = []
+          @use_yardi_as_lead = false
         end
-        
-        @time_slots = @community.collect_time_slots_for_yardi(@stepping, @yardi_self_time_slots, @yardi_guided_time_slots)
-        @yardi_enable_days = @time_slots.keys
-      else
-        @time_slots = @reschedule_tour ? @community.collect_time_slots_for_rechedule_tours(@stepping,@tour_type) : @community.collect_time_slots(@stepping)
-        @yardi_enable_days = []
-        @use_yardi_as_lead = false
       end
     else
       @time_slots = @reschedule_tour ? @community.collect_time_slots_for_rechedule_tours(@stepping,@tour_type) : @community.collect_time_slots(@stepping)
