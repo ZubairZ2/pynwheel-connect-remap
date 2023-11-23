12afeedb4ac720fe66f0afaa77c1e550faf1f734
Salahudin Sallu <salahudin@pynwheel.com>
2023-11-23 18:16:51 +0500

    pyn-907 RentCafe floorplan images added

--- app/services/yardi_rent_cafe_static_service.rb
+++ app/services/yardi_rent_cafe_static_service.rb
@@ -184,6 +184,7 @@ class YardiRentCafeStaticService < BaseService
               end
 
               fp.deposit = r["MinimumDeposit"]
+              add_floorplan_images(fp, r['FloorplanImageURL'])
               fp.save(validate: false)
             end
           end
@@ -193,6 +194,29 @@ class YardiRentCafeStaticService < BaseService
         #ExceptionNotifier.notify_exception(e,data: {community_id: credentials.community_id})
       end
     end
+  end
+
+  def add_floorplan_images fp, image_urls
+    primary_image = fetch_floorplan_image_url(image_urls, 0)
+    secondary_image = fetch_floorplan_image_url(image_urls, 1)
+    fp.image = image_base64(primary_image) if primary_image.present?
+    fp.secondary_image = image_base64(secondary_image) if secondary_image.present?
+  end
+
+  def fetch_floorplan_image_url image_urls, index
+    return unless image_urls.present?
+    urls = image_urls&.split(",")&.reverse  
+    urls[index]
+  end
+
+  def image_base64(image_url)
+    return unless image_url.present?
+    encoded_url = URI.encode(image_url)
+    uri = URI.parse(encoded_url)
+    file = uri.open
+    image_data = file.read
+    encoded_image = Base64.strict_encode64(image_data)
+    "data:image/png;base64,#{encoded_image}"
   end
 
   def set_availabilty_date(available_date)
