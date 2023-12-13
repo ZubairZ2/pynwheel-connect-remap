5fb3b0c8e302ae34f4bc214edfdbb8b764032ca5
Salahudin Sallu <salahudin@pynwheel.com>
2023-12-13 21:11:45 +0500

    pynwheel company destroy

--- app/jobs/delete_company_job.rb
+++ app/jobs/delete_company_job.rb
@@ -3,13 +3,19 @@ class DeleteCompanyJob < ApplicationJob
   include SuckerPunch::Job
 
   def perform(company)
-    tours = Tour.where(community_id: company.communities.pluck(:id) )
+    community_ids = company.communities.pluck(:id)
+    tours = Tour.where(community_id: community_ids )
+    OpeningHour.where(community_id: community_ids).destroy_all
+    GuidedOpeningHour.where(community_id: community_ids).destroy_all
+    Gallery.where(community_id: community_ids).destroy_all
+    CommunityUser.where(community_id: community_ids).destroy_all
     Feedback.where(tour_id: tours.pluck(:id) ).destroy_all
-    Credential.where(community_id: company.communities.pluck(:id)).destroy_all
+    Credential.where(community_id: community_ids).destroy_all
+    PynwheelAccessUser.where(community_id: community_ids ).destroy_all
+    tours.destroy_all
+    
     Credential.where(company_id: company.id).destroy_all
     CompanySetting.where(company_id: company.id).destroy_all
-    pynwheeel_access_users = PynwheelAccessUser.where(community_id: company.communities.pluck(:id) )
-    pynwheeel_access_users.destroy_all
     company.destroy
   end
 end
