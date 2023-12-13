6cea5e6b7aab8a3fbaaa6a33824b6245857ad51a
Salahudin Sallu <salahudin@pynwheel.com>
2023-12-13 19:54:34 +0500

    pyn-937 fix company delete issue

--- app/jobs/delete_company_job.rb
+++ app/jobs/delete_company_job.rb
@@ -6,6 +6,8 @@ class DeleteCompanyJob < ApplicationJob
     tours = Tour.where(community_id: company.communities.pluck(:id) )
     Feedback.where(tour_id: tours.pluck(:id) ).destroy_all
     Credential.where(community_id: company.communities.pluck(:id)).destroy_all
+    Credential.where(company_id: company.id).destroy_all
+    CompanySetting.where(company_id: company.id).destroy_all
     pynwheeel_access_users = PynwheelAccessUser.where(community_id: company.communities.pluck(:id) )
     pynwheeel_access_users.destroy_all
     company.destroy
