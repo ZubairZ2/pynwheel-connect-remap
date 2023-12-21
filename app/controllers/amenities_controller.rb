5f77df04d39034279bcd731dad95ce62d5519a60
Salahudin Sallu <salahudin@pynwheel.com>
2023-12-21 17:08:18 +0500

    pyn-938 shifted to background job

--- app/controllers/amenities_controller.rb
+++ app/controllers/amenities_controller.rb
@@ -19,7 +19,7 @@ class AmenitiesController < ApplicationController
       @amenity.save
       redirect_to edit_community_amenity_path(current_community, @amenity)
     else
-      UploadAmenitiesWorker.perform_async(current_community&.id, params[:src], params[:name])
+      AmenityImagesJob.perform_async current_community&.id, params[:src], params[:name]
       @amenities = current_community.amenities.order(id: :desc)
     end
   end
---
+++ app/jobs/amenity_images_job.rb
@@ -0,0 +1,13 @@
+class AmenityImagesJob < ApplicationJob
+  include SuckerPunch::Job
+
+  def perform community_id, source, amenity_name
+    return unless community_id.present?
+    begin
+      community = Community.find_by_id community_id
+      community.amenities.create(image: source, name: amenity_name)
+    rescue => error
+      puts "\ne.message\b" 
+    end
+  end
+end
