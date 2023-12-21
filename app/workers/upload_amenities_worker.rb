ac6f4b8a554dca092a8c2ef9559a7acb4a1a3f12
Salahudin Sallu <salahudin@pynwheel.com>
2023-12-21 16:51:03 +0500

    pyn-938 queue name updated

--- app/workers/upload_amenities_worker.rb
+++ app/workers/upload_amenities_worker.rb
@@ -1,11 +1,15 @@
 class UploadAmenitiesWorker
   include Sidekiq::Worker
-  sidekiq_options queue: 'create_amenity', retry: 3
+  sidekiq_options queue: 'amenity_image', retry: 3
 
   def perform(community_id, image_src, amenity_name)
     return unless community_id.present?
     community = Community.find_by_id community_id
 
-    community.amenities.create(image: image_src, name: amenity_name)
+    begin
+      community.amenities.create(image: image_src, name: amenity_name)
+    rescue => e
+      raise e
+    end
   end
 end
