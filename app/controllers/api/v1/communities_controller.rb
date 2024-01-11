197663e1a2f45b0036e355e2c74d0beefd3c82da
Salahudin Sallu <salahudin@pynwheel.com>
2024-01-11 17:55:51 +0500

    pyn-952 Allowed emails fix

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -117,11 +117,26 @@ module Api
     
       def portico_list_communities
         @allow_usage, @redirect_url = get_version_access params
-        if @tour_user.present?
-          @communities = Community.select(:id,:name,:email,:phone,:company_id,:locked,:latitude,:longitude,:address,:logo,:state,:city).includes(:company, :allowed_emails).self_tour_enabled_only.where(allowed_emails: {email: [nil, @tour_user.email]})
-        else
-          @communities = Community.select(:id,:name,:email,:phone,:company_id,:locked,:latitude,:longitude,:address,:logo,:state,:city).includes(:company).self_tour_enabled_only
-        end
+        # if @tour_user.present?
+        #   @communities = Community.select(:id,:name,:email,:phone,:company_id,:locked,:latitude,:longitude,:address,:logo,:state,:city).includes(:company, :allowed_emails).self_tour_enabled_only.where(allowed_emails: {email: [nil, @tour_user.email]})
+        # else
+        #   @communities = Community.select(:id,:name,:email,:phone,:company_id,:locked,:latitude,:longitude,:address,:logo,:state,:city).includes(:company).self_tour_enabled_only
+        # end
+
+        @communities = Community.select(:id, :name, :email, :phone, :company_id, :locked, :latitude, :longitude, :address, :logo, :state, :city, :restrict_access)
+                                .includes(:company, :allowed_emails)
+                                .self_tour_enabled_only
+
+        unrestricted_properties = @communities.where(restrict_access: false)
+        restricted_properties = @communities.where(restrict_access: true)
+
+        user_allowed_properties = restricted_properties.joins(:allowed_emails)
+                                                       .where(allowed_emails: { email: @tour_user.email.downcase })
+                                                       .distinct
+
+        @communities = unrestricted_properties + user_allowed_properties
+
+
       end
     
       def lincoln_list_communities
