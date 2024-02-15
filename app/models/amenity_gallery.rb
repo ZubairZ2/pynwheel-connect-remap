687a8d79dd18465dff0309616bcc55eb4a1a8a1a
Salahudin Sallu <salahudin@pynwheel.com>
2024-02-15 17:29:33 +0500

    pyn-820 list view formatting

--- app/models/amenity_gallery.rb
+++ app/models/amenity_gallery.rb
@@ -27,12 +27,19 @@ class AmenityGallery < ApplicationRecord
 
   def stop_description_formatting stop_description
     return "" unless stop_description.present?
-    
-    if stop_description.match?(/<ul\b.*?>|<ol\b.*?>/)
-      doc = Nokogiri::HTML(stop_description)
-      items = doc.css('ul li').map(&:text)
-      items.join(', ')
-    else
+
+    begin
+      if stop_description.match?(/<ul\b.*?>|<ol\b.*?>/)
+        doc = Nokogiri::HTML(stop_description)
+        items = doc.css('ul li, ol li').map(&:text)
+        list_text = items.join(",\n")
+        list_text = "\n#{list_text}\n"
+        formatted_string = stop_description.gsub(/<ul\b.*?>.*?<\/ul>|<ol\b.*?>.*?<\/ol>/, list_text)
+        ActionView::Base.full_sanitizer.sanitize(formatted_string)
+      else
+        ActionView::Base.full_sanitizer.sanitize(stop_description)
+      end
+    rescue
       ActionView::Base.full_sanitizer.sanitize(stop_description)
     end
   end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -23,15 +23,22 @@ class Gallery < ApplicationRecord
 		DeleteGalleryJob.perform_async self
 	end
 
-	def stop_description_formatting stop_description
-	    return "" unless stop_description.present?
-	    
-	    if stop_description.match?(/<ul\b.*?>|<ol\b.*?>/)
-	      doc = Nokogiri::HTML(stop_description)
-	      items = doc.css('ul li').map(&:text)
-	      items.join(', ')
-	    else
-	      ActionView::Base.full_sanitizer.sanitize(stop_description)
-	    end
-	end
+  def stop_description_formatting stop_description
+    return "" unless stop_description.present?
+
+    begin
+      if stop_description.match?(/<ul\b.*?>|<ol\b.*?>/)
+        doc = Nokogiri::HTML(stop_description)
+        items = doc.css('ul li, ol li').map(&:text)
+        list_text = items.join(",\n")
+        list_text = "\n#{list_text}\n"
+        formatted_string = stop_description.gsub(/<ul\b.*?>.*?<\/ul>|<ol\b.*?>.*?<\/ol>/, list_text)
+        ActionView::Base.full_sanitizer.sanitize(formatted_string)
+      else
+        ActionView::Base.full_sanitizer.sanitize(stop_description)
+      end
+    rescue
+      ActionView::Base.full_sanitizer.sanitize(stop_description)
+    end
+  end
 end
