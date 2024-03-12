7c670b2b76b8c57bb29cb8c4cc0f19e58c015ec6
Salahudin Sallu <salahudin@pynwheel.com>
2024-03-12 11:57:15 +0500

    pyn-939 company level token storage

--- app/services/data_providers/rent_manager/base_service.rb
+++ app/services/data_providers/rent_manager/base_service.rb
@@ -3,35 +3,67 @@ require 'httparty'
 module DataProviders
   module RentManager
     class BaseService
-      TOKEN_CACHE_KEY = 'rent_manager_api_token'.freeze
-      TOKEN_EXPIRY_THRESHOLD = 24.hours
-      INACTIVITY_THRESHOLD = 15.minutes
 
       def initialize(community_id)
         @batch_size = 10
         @community_id = community_id
         @community = Community.find_by_id(community_id)
         @credential = @community&.credential if @community
-
-        @api_auth_token = cached_api_token || generate_and_cache_api_token if valid_community?
+        @company = @community.company
       end
 
       def get_property_details(property_code)
-        refresh_token_if_necessary
+        return unless is_user_authorized?
         fetch_property_details(property_code)
       end
 
       def get_units_list(property_code)
-        refresh_token_if_necessary
+        return unless is_user_authorized?
         fetch_units_details(property_code)
       end
 
       def get_floorplans_list(property_code)
-        refresh_token_if_necessary
+        return unless is_user_authorized?
         fetch_floorplans_details(property_code)
       end
 
       private
+
+      def is_user_authorized?
+        begin
+          if (token_expired? || token_inactive?) 
+            renew_token 
+          else
+            @company.update(rentmanager_token_inactivity: (Time.now + 15.minutes))
+            return true
+          end
+        rescue
+          return false
+        end
+      end
+
+      def token_inactive?
+        @company&.rentmanager_token_inactivity.nil? || Time.now >= @company&.rentmanager_token_inactivity
+      end
+
+      def token_expired?
+        @company&.rentmanager_auth_token.nil? || Time.now >= @company&.rentmanager_token_expiry
+      end
+
+      def renew_token
+        access_token = fetch_authentication_token()
+        access_token = JSON.parse(access_token.body)
+
+        return false unless access_token.present?
+        
+        @company.update(
+          rentmanager_auth_token: access_token, 
+          rentmanager_token_expiry: (Time.now + 1.day),
+          rentmanager_token_inactivity: (Time.now + 15.minutes)
+        )
+
+        return true
+      end
 
       def update_attribute_if_blank(object, attribute, value, diff_name = nil)
         updated_column = diff_name.present? ? diff_name : attribute
@@ -48,10 +80,6 @@ module DataProviders
         return if units.empty?
 
         ProvidersDataUpdationService.new.update_or_create_units_records(units)
-      end
-
-      def valid_community?
-        @community && @credential
       end
 
       def fetch_authentication_token
@@ -132,7 +160,7 @@ module DataProviders
       def fetch_request_header
         {
           'Content-Type' => 'application/json',
-          'X-RM12Api-ApiToken' => @api_auth_token
+          'X-RM12Api-ApiToken' => api_auth_token()
         }
       end
 
@@ -144,42 +172,12 @@ module DataProviders
         @credential.rentmanager_password
       end
 
+      def api_auth_token
+        @company.rentmanager_auth_token
+      end
+
       def location_id
         1
-      end
-
-      def cached_api_token
-        Rails.cache.read(TOKEN_CACHE_KEY)
-      end
-
-      def generate_and_cache_api_token
-        access_token = fetch_authentication_token
-        access_token = JSON.parse(access_token.body)
-        puts "\n\nAccess Token: #{access_token}\n\n"
-
-        Rails.cache.write(TOKEN_CACHE_KEY, access_token, expires_in: TOKEN_EXPIRY_THRESHOLD)
-        Rails.cache.write("#{TOKEN_CACHE_KEY}_expiry",(Time.now + TOKEN_EXPIRY_THRESHOLD), expires_in: TOKEN_EXPIRY_THRESHOLD)
-        Rails.cache.write("#{TOKEN_CACHE_KEY}_inactivity", (Time.now + INACTIVITY_THRESHOLD), expires_in: INACTIVITY_THRESHOLD)
-
-        access_token
-      end
-
-      def refresh_token_if_necessary
-        if token_expired? || token_inactive?
-          @api_auth_token = generate_and_cache_api_token
-        else
-          Rails.cache.write("#{TOKEN_CACHE_KEY}_inactivity", (Time.now + INACTIVITY_THRESHOLD), expires_in: INACTIVITY_THRESHOLD)
-        end
-      end
-
-      def token_expired?
-        token_expiry = Rails.cache.read("#{TOKEN_CACHE_KEY}_expiry")
-        token_expiry.nil? || Time.now >= token_expiry
-      end
-
-      def token_inactive?
-        last_activity = Rails.cache.read("#{TOKEN_CACHE_KEY}_inactivity")
-        last_activity.nil? || Time.now >= last_activity
       end
     end
   end
