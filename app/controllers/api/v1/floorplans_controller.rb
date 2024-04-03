20c15f09021afdfc983e1079a58b532ebda25947
Salahudin Sallu <salahudin@pynwheel.com>
2024-04-03 14:55:26 +0500

    pyn-1002 avoid from removing stops

--- app/controllers/api/v1/floorplans_controller.rb
+++ app/controllers/api/v1/floorplans_controller.rb
@@ -101,12 +101,13 @@ module Api
           # end
         
           add_remove_stop_into_sort_hash(params[:building], params[:floor], tour_stop, "remove")
+          render json: { success: true, error_code: 200, message: "Tour stop has been deleted successfully", is_unit_already_available: TourStop.where(stop_id: params[:stop_id], tour_id: @tour.id ).last.present?}, status: 200
 
-          if tour_stop.destroy
-            render json: { success: true, error_code: 200, message: "Tour stop has been deleted successfully", is_unit_already_available: TourStop.where(stop_id: params[:stop_id], tour_id: @tour.id ).last.present?}, status: 200
-          else
-            render json: { success: false, status_code: 400, message: "Something went wrong, please try again later", data: nil }, status: 400
-          end
+          # if tour_stop.destroy
+          #   render json: { success: true, error_code: 200, message: "Tour stop has been deleted successfully", is_unit_already_available: TourStop.where(stop_id: params[:stop_id], tour_id: @tour.id ).last.present?}, status: 200
+          # else
+          #   render json: { success: false, status_code: 400, message: "Something went wrong, please try again later", data: nil }, status: 400
+          # end
 
         elsif stops_count == 1
           render json: { success: true, error_code: 200, message: "Last stop can not be removed", is_unit_already_available: TourStop.where(stop_id: params[:stop_id], tour_id: @tour.id).last.present?}, status: 200
