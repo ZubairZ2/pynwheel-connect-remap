7d177663951b9468aead79bcf2344586a56f3034
Salahudin Sallu <salahudin@pynwheel.com>
2024-04-18 14:54:12 +0500

    Server errror fix on live while opening community group

--- app/controllers/community_groups_controller.rb
+++ app/controllers/community_groups_controller.rb
@@ -1,9 +1,6 @@
 class CommunityGroupsController < ApplicationController
   # include Error::ErrorHandler
   def index
-    puts "\n\n\n\n #{params} \n\n\n\n"
-    puts "\n\n\n\n Current Company: #{current_company.inspect} \n\n\n\n"
-    
     @community_groups = alphabetical_sort(CommunityGroup.where(company_id: current_company.id)) rescue []
   end
   def new
