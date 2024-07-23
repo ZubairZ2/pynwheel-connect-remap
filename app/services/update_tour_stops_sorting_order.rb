738d2799d26e974eb9d76eaa3e143e6fe51949c7
Salahudin Sallu <salahudin@pynwheel.com>
2024-07-23 19:47:16 +0500

    pyn-1102 fix server error

--- app/services/update_tour_stops_sorting_order.rb
+++ app/services/update_tour_stops_sorting_order.rb
@@ -1,9 +1,9 @@
 class UpdateTourStopsSortingOrder
-  def initialize(community, tour_user, stop_ids = [])
+  def initialize(community, tour_user, stop_ids)
     @community = community
     @tour_user = tour_user
     @tour = get_tour
-    @ignore_stop_ids = stop_ids
+    @ignore_stop_ids = stop_ids || []
     puts "******"*30
     puts "\n\n"
     puts @ignore_stop_ids.inspect
@@ -203,7 +203,7 @@ class UpdateTourStopsSortingOrder
 
   def get_tour_stops
     ids = @tour.tour_stops.ids - @ignore_stop_ids
-    @tour.tour_stops.where(id: ids).plotted_stops.visible.map{|s|}
+    @tour.tour_stops.where(id: ids).plotted_stops.visible
   end
 
   def get_floor_stops stop_ids
