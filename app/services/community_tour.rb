f88c89b72e4cc0a0f2508968668314f4abef5e7d
Salahudin Sallu <salahudin@pynwheel.com>
2024-09-19 23:10:35 +0500

    pyn-1146 customize tour API update

--- app/services/community_tour.rb
+++ app/services/community_tour.rb
@@ -130,7 +130,7 @@ class CommunityTour
                 "primary_floorplan": primary_floorplan,
                 "secondary_floorplan": secondary_floorplan,
                 "stop_type": stop.stop_type,
-                "floor": new_stop&.floor,
+                "floor": stop.stop_floor_name(new_stop),
                 "building": new_stop&.building
               }
             end
