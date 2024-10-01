3f92cb80dc4b4c43c7c4c64319d8ba2d0aae1125
Salahudin Sallu <salahudin@pynwheel.com>
2024-10-01 12:59:53 +0500

    Redis fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,12 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.cache_store = :redis_store, ENV['REDIS_URL'], { expires_in: 1.year }
+  # config.cache_store = :redis_store, ENV['REDIS_URL'], { expires_in: 1.year }
+  config.cache_store = :redis_store, {
+    url: ENV['REDIS_URL'],
+    expires_in: 1.year,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,23 @@
-if Rails.env == 'production'
+require 'uri'
+require 'redis'
+
+if Rails.env.production?
   uri = URI.parse(ENV["REDIS_URL"])
+
   Resque.redis = Redis.new(
     host:     uri.host,
     port:     uri.port,
+    password: uri.password,
+    ssl:      uri.scheme == 'rediss', # Enable SSL if using rediss://
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # Skip SSL verification (optional)
+  )
+  
+elsif Rails.env.development?
+  uri = URI.parse(ENV["REDIS_URL"])
+
+  Resque.redis = Redis.new(
+    host: uri.host,
+    port: uri.port,
     password: uri.password
   )
-elsif Rails.env == 'development'
-  uri = URI.parse(ENV["REDIS_URL"])
-  Resque.redis = Redis.new(host: uri.host)
 end
