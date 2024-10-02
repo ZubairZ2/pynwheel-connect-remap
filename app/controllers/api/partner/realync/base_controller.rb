e63550dfddc58306f9c12c9dff0d9ccc3a7509d1
Salahudin Sallu <salahudin@pynwheel.com>
2024-10-02 15:23:28 +0500

    enable loggin on live

--- app/controllers/api/partner/realync/base_controller.rb
+++ app/controllers/api/partner/realync/base_controller.rb
@@ -2,20 +2,20 @@ module Api
   module Partner
     module Realync
       class BaseController < ActionController::Base
-        # around_action :logging_trail 
+        around_action :logging_trail 
 
-        # def logging_trail
-        #   begin
-        #     # Replace with worker
-        #     ::Log::Impression.new(requester: request).request_filler
-        #     yield
-        #     # Replace with worker
-        #     ::Log::Impression.new(requester: response).response_filler
-        #   rescue Exception => ex
-        #     # Replace with worker
-        #     ::Log::Impression.new(requester: response, exception: ex).exception_filler
-        #   end
-        # end 
+        def logging_trail
+          begin
+            # Replace with worker
+            ::Log::Impression.new(requester: request).request_filler
+            yield
+            # Replace with worker
+            ::Log::Impression.new(requester: response).response_filler
+          rescue Exception => ex
+            # Replace with worker
+            ::Log::Impression.new(requester: response, exception: ex).exception_filler
+          end
+        end
       end
     end
   end
