d1287b1bd0a29e6c2368f9e32ef12f6b50a48f0a
Salahudin Sallu <salahudin@pynwheel.com>
2024-10-10 19:27:04 +0500

    pyn-1136 Reports order change

--- app/services/sessions_report_service.rb
+++ app/services/sessions_report_service.rb
@@ -18,7 +18,7 @@ class SessionsReportService < BaseService
   private
 
   def generate_months
-    (@start_date..@end_date).map { |d| d.strftime("%B %Y") }.uniq
+    (@start_date..@end_date).map { |d| "Sessions Count ( #{d.strftime("%B %Y")} )" }.uniq
   end
 
   def generate_headers
