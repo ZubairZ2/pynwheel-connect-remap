1120e7e55d253aa7e34cf017ee73b8eac127d98c
Salahudin Sallu <salahudin@pynwheel.com>
2024-10-23 18:42:37 +0500

    pyn-1156 resman version 4 floorplan images added

--- app/services/resman4_static_service.rb
+++ app/services/resman4_static_service.rb
@@ -201,9 +201,31 @@ class Resman4StaticService < BaseService
         end
       end
 
+      add_floorplan_images(floorplan, f['File'])
+
       floorplan.save(validate: false)
 
     end
+  end
+
+  def add_floorplan_images fp, image_urls
+    primary_image = fetch_floorplan_image_url(image_urls)
+    fp.image = image_base64(primary_image) if primary_image.present?
+  end
+
+  def fetch_floorplan_image_url image_urls
+    return unless image_urls.present?
+    image_urls['Src'] rescue nil
+  end
+
+  def image_base64(image_url)
+    return unless image_url.present?
+    encoded_url = URI.encode(image_url)
+    uri = URI.parse(encoded_url)
+    file = uri.open
+    image_data = file.read
+    encoded_image = Base64.strict_encode64(image_data)
+    "data:image/png;base64,#{encoded_image}"
   end
 
   def get_unit_lease_prising unit, leasing = ""
