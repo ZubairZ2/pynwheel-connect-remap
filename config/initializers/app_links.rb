df3e591334e19e095eb194ac1319862ed16f4e78
Salahudin Sallu <salahudin@pynwheel.com>
2024-10-31 16:36:31 +0500

    pyn-1160 code indentation

--- config/initializers/app_links.rb
+++ config/initializers/app_links.rb
@@ -2,11 +2,15 @@ module AppLinks
   LINKS = {
     lincoln: {
       app: "https://apps.apple.com/us/app/lincoln-property-self-tour/id1508997129",
-      android: "https://play.google.com/store/apps/details?id=com.pynwheel.lincolnselftour"
+      android: "https://play.google.com/store/apps/details?id=com.pynwheel.lincolnselftour",
+      one_link: "http://onelink.to/6fsxvq",
+      app_identification_text: "Lincoln Property Company Pynwheel Tour"
     },
     default: {
       app: "https://apps.apple.com/us/app/self-tour/id1488907392",
-      android: "https://play.google.com/store/apps/details?id=com.pynwheel.selftour"
+      android: "https://play.google.com/store/apps/details?id=com.pynwheel.selftour",
+      one_link: "http://onelink.to/m5vuhn",
+      app_identification_text: "Pynwheel Tour"
     }
   }
 
@@ -19,6 +23,18 @@ module AppLinks
   end
 
   def self.one_link company_name
-    company_name == "lincoln" ? "http://onelink.to/6fsxvq" : "http://onelink.to/m5vuhn"
+    if company_name == "lincoln"
+      LINKS.fetch(company_name.to_sym, LINKS[:lincoln])[:one_link]
+    else
+      LINKS.fetch(company_name.to_sym, LINKS[:default])[:one_link]
+    end
+  end
+
+  def self.app_identification_text
+    if company_name == "lincoln"
+      LINKS.fetch(company_name.to_sym, LINKS[:lincoln])[:app_identification_text]
+    else
+      LINKS.fetch(company_name.to_sym, LINKS[:default])[:app_identification_text]
+    end
   end
 end
