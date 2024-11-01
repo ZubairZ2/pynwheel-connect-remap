67c1191f5e6e18ff6a84f600ea4d38dac293f14c
Salahudin Sallu <salahudin@pynwheel.com>
2024-11-01 23:09:37 +0500

    pyn-1138 enum updates

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,7 +1,7 @@
 class Status < ApplicationRecord
     belongs_to :statusable, polymorphic: true
 
-    enum status: [:in_progress, :in_review, :submitted, :approved, :rejected , :re_submitted, :completed, :deployed, :application_in_qa, :released, :form_approved, 
+    enum :status, [:in_progress, :in_review, :submitted, :approved, :rejected , :re_submitted, :completed, :deployed, :application_in_qa, :released, :form_approved, 
     :in_production]
 
     def status_and_remarks_obj
