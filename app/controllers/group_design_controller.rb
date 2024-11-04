4c539b18eb6551601dad66ef2fdb35c9d7b0b177
Salahudin Sallu <salahudin@pynwheel.com>
2024-11-04 20:20:03 +0500

    pyn-1138 commented code remove and fixes

--- app/controllers/group_design_controller.rb
+++ app/controllers/group_design_controller.rb
@@ -1,15 +1,13 @@
 class GroupDesignController < ApplicationController
-  # include Error::ErrorHandler
   def index
-
     @community_group = CommunityGroup.find(params[:community_group_id])
     @group_design = @community_group.group_design
 
     @uploader = GroupHomepageVideo.new.video
     @uploader.success_action_redirect = upload_video_direct_community_group_group_design_url(@community_group.id,@group_design.id)
   end
-  def update
 
+  def update
     @community_group = CommunityGroup.find params[:community_group_id]
     @group_design = GroupDesign.find params[:id]
     if @group_design.update(group_design_params)
@@ -19,42 +17,47 @@ class GroupDesignController < ApplicationController
       render "edit"
     end
   end
+
   def delete_home_page_video
     @homepage_video = GroupHomepageVideo.find params[:home_page_video_id].to_i
     @homepage_video.destroy
     redirect_to community_group_group_design_index_path(@community_group.id)
   end
+
   def destroy
     @community_group = CommunityGroup.find params[:community_group_id]
     @homepage_video = GroupHomepageVideo.find params[:home_page_video_id].to_i
     @homepage_video.destroy
     redirect_to community_group_group_design_index_path(@community_group.id)
   end
-  def save_home_page_images
 
+  def save_home_page_images
     @group_design = GroupDesign.find params[:id].to_i
     image = MiniMagick::Image.open(params[:file].path)
     @group_design.group_homepage_images.create(image: params[:file],is_small: (image.width < 800 && image.height < 600) ? true : false)
-    #@home_page_images = current_community.design.home_page_images.order(:sort).all
     render :json=>{"status"=>"sucdess"}
   end
+
   def update_home_page_images
     @community_group = CommunityGroup.find params[:community_group_id]
     @homepage_image = GroupHomepageImage.find params[:home_page_image_id].to_i
     @homepage_image.update(name: params[:group_homepage_image][:name])
     redirect_to community_group_group_design_index_path(@community_group.id)
   end
+
   def show_image_in_modal
     @community_group = CommunityGroup.find params[:community_group_id]
     @group_design = GroupDesign.find params[:id]
     @group_homepage_image = GroupHomepageImage.find(params[:home_page_image])
   end
+
   def delete_home_page_image
     @community_group = CommunityGroup.find params[:community_group_id]
     @homepage_image = GroupHomepageImage.find params[:home_page_image_id].to_i
     @homepage_image.destroy
     redirect_to community_group_group_design_index_path(@community_group.id)
   end
+
   def set_loop_type
     @group_design = GroupDesign.find params[:id]
     @group_design.loop_type = (params['loop_type'] == "true" ? "images" : "videos")
@@ -62,32 +65,13 @@ class GroupDesignController < ApplicationController
     render :json=>{"status"=>"sucdess"}
   end
   def upload_video_direct
-
-    # home = HomePageVideo.where(design_id: current_community.design.id).first
-    # if home.present?
-    #   home.destroy
-    # end
     @uploader =  GroupHomepageVideo.new(params[:home_page_video])
     @group_design = GroupDesign.find params[:id]
-    if @group_design.group_homepage_video.present?
-      @group_design.group_homepage_video.destroy
-    end
-    # "https://images-pynwheel-cms-v2.s3.amazonaws.com/uploads/home_page_video/video/11/1565361189-1565352260-Short_funny_clips.mp4"
-    # file_key = "uploads/home_page_video/video/1403/#{SecureRandom.uuid}-#{params[:doc][:video]}"
-    # @document = HomePageVideo.create(video: params[:doc][:video])
-    # byebug
-    # render :json => {
-    #     :policy => s3_upload_policy_document(file_key),
-    #     :signature => s3_upload_signature(file_key),
-    #     :key => file_key,
-    #     :success_action_redirect => upload_video_direct_community_home_page_index_url
-    # }
+    @group_design.group_homepage_video.destroy if @group_design.group_homepage_video.present?
+
     if @uploader.save
-      # binding.pry
-      # Resque.enqueue(AvatarProcessor, @uploader.id, params[:key])
       @uploader.remote_video_url = @uploader.video.direct_fog_url + params[:key]
       @uploader.group_design_id = @group_design.id
-
       @uploader.save
       redirect_to community_group_group_design_index_path, notice: 'Video has been uploaded'
     else
@@ -97,9 +81,11 @@ class GroupDesignController < ApplicationController
 
   private
 
-  def group_design_params
-    params.require(:group_design).permit!  end
-  def set_community
-    @community = Community.find params[:id]
-  end
+    def group_design_params
+      params.require(:group_design).permit!  
+    end
+    
+    def set_community
+      @community = Community.find params[:id]
+    end
 end
--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -8,7 +8,6 @@ class UnitsController < ApplicationController
   before_action :load_all_locks, only: [:new, :create, :edit, :update]
 
   def index
-    #@units = @community.units.page(params[:page]).per(10)
     @community_info = Community.includes(:floorplans, :units).find(params[:community_id])
     @communities = current_company.communities
     add_breadcrumb "Units", community_units_path(@community)
@@ -47,7 +46,6 @@ class UnitsController < ApplicationController
     # #PaperTrail::Version.create(item_type: "Unit",item_id: @unit.id,event: "update",whodunnit: current_user.id,community_id: current_community.id, company_id: current_company.id,object: "name: #{@floorplan.name} community_id: '#{@floorplan.community_id}'")
 
     redirect_to edit_community_unit_path(@community, @unit)
-    # render :json=> {:success=>false}
   end
 
   def show_unit_secondary_image_in_modal
@@ -159,14 +157,6 @@ class UnitsController < ApplicationController
 
       if (params[:unit][:marketing_name].present? && params[:unit][:marketing_name] != @unit.marketing_name)
         @unit.name_is_updated = true
-        # begin
-        #   ts = TourStop.find_by(stop_id: @unit.id)
-        #   if ts.present?
-        #     ts.name = params[:unit][:marketing_name]
-        #     ts.save
-        #   end
-        # rescue => ex
-        # end
         TourStop.where(stop_id: @unit.id).update_all(name: params[:unit][:marketing_name])
       end
 
@@ -216,7 +206,7 @@ class UnitsController < ApplicationController
         @unit.availability = "Occupied"
         @unit.available = false
       end
-      ########
+
       if @unit.manual_override
         if params[:unit][:description].present?
           params[:unit][:description] = add_padding_description params[:unit][:description]
@@ -302,18 +292,12 @@ class UnitsController < ApplicationController
             end
             format.json { respond_with_bip(@unit) }
           end
-
-          # @unit.errors[:base] << "Please set manual override field first"
-          # flash[:error] = @unit.errors.full_messages.join(',')
-          # format.html { render :action => "edit" }
-          # format.json { respond_with_bip(@unit) }
         end
       end
     end
   end
 
   def set_manually_updated_column
-    # @unit.update_column(:manually_updated, true)
     if @unit.sold
       @unit.update_columns(availability: "Occupied", available: false, available_date: '', availability_is_updated: true)
     end
@@ -350,6 +334,7 @@ class UnitsController < ApplicationController
       @unit.remove_secondary_image!
       @unit.save
     end
+
     redirect_to :back, notice: "Image removed successfully."
   end
 
@@ -357,17 +342,11 @@ class UnitsController < ApplicationController
     unit = @community.units.where(floorplate_id: nil, provider_unit_id: params[:id])
     if unit.present?
       unit = unit.first
-      #unit.first.update_columns(x_plot: params[:x_plot],y_plot: params[:y_plot])
       unit.x_plot = params[:x_plot]
       unit.y_plot = params[:y_plot]
       unit.save(validate: false)
       TourStop.where(stop_id: unit.id).update_all(latitude: unit.x_plot, longitude: unit.y_plot)
-      # if ts.present?
-      #   ts.update_all(latitude: unit.x_plot, longitude: unit.y_plot)
-      #   # ts.latitude = unit.x_plot
-      #   # ts.longitude = unit.y_plot
-      #   # ts.save
-      # end
+
       render json: { unit: unit }, status: 200
     else
       render json: {}, status: 404
@@ -377,19 +356,13 @@ class UnitsController < ApplicationController
   def ajaxplotunitforfloorplate
     unit = @community.units.where(provider_unit_id: params[:id])
     if unit.present?
-      #unit.first.update_columns(x_plot: params[:x_plot],y_plot: params[:y_plot],floorplate_id: params[:floorplate_id])
       unit = unit.first
       unit.x_plot = params[:x_plot]
       unit.y_plot = params[:y_plot]
       unit.floorplate_id = params[:floorplate_id]
       unit.save(validate: false)
-      
       TourStop.where(stop_id: unit.id).update_all(latitude: unit.x_plot, longitude: unit.y_plot)
-      # if ts.present?
-      #   ts.latitude = unit.x_plot
-      #   ts.longitude = unit.y_plot
-      #   ts.save
-      # end
+      
       @test_units = Floorplate.find(params[:floorplate_id]).fetch_units
       render json: { unit: unit }, status: 200
     else
@@ -397,7 +370,7 @@ class UnitsController < ApplicationController
     end
   end
 
-  def plot_unit_door                                # create or update
+  def plot_unit_door
     unit = @community.units.where(provider_unit_id: params[:id]).first
     if unit.present?
       door ||= unit.door || unit.build_door
@@ -480,9 +453,6 @@ class UnitsController < ApplicationController
     ts = TourStop.where(stop_id: @unit.id) unless @unit.modal_unit
     if @unit.modal_unit
       if ts.present?
-        # ts.latitude = 0
-        # ts.longitude = 0
-        # ts.save
         ts.update_all(latitude: 0, longitude: 0)
       end
     else
@@ -536,6 +506,7 @@ class UnitsController < ApplicationController
     else
       @community.units.where(id: params[:unit_ids]).update_all(availability: "Occupied", manually_updated: true, available: false, available_is_updated: true, availability_is_updated: true)
     end
+    
     flash[:notice] = "Available is updated for units successfully."
     redirect_to :back
   end
@@ -552,6 +523,7 @@ class UnitsController < ApplicationController
     else
       @community.units.where(id: params[:unit_ids]).update_all(sold: params[:sold], manually_updated: true, availability_is_updated: true, available_is_updated: true)
     end
+
     flash[:notice] = "Sold is updated for units successfully."
     redirect_to :back
   end
@@ -569,7 +541,7 @@ class UnitsController < ApplicationController
 
   def add_padding_description(desc)
     str = ""
-    ds = desc.split('<ul>') # Adding padding for <ul>
+    ds = desc.split('<ul>')
     if ds.count > 1
       ds.each do |d|
         unless d == ""
@@ -606,11 +578,6 @@ class UnitsController < ApplicationController
 
   def set_image
     UploadImageForUnit.perform_async @community, params[:unit_ids], params[:image_file]
-    # units = @community.units.where(id: params[:unit_ids])
-    # units.each do |unit|
-    #   units.update(image: params[:image_file],manually_updated: true)
-    # end
-    # flash[:notice] = "Image is uploaded for units successfully."
     redirect_to :back, notice: "Image is uploaded for units successfully."
   end
 
