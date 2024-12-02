a20002e7a73fe396a0c610f4b3cd75cb40730a16
Salahudin Sallu <salahudin@pynwheel.com>
2024-12-02 15:44:07 +0500

    pyn-1138 remove uglifier

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.compile = true
   config.action_controller.asset_host = ENV['HOST_URL']
   config.force_ssl = true
