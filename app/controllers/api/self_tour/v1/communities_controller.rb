a549a6085087e80f712e31fbe5d94b9d9097de15
Salahudin Sallu <salahudin@pynwheel.com>
2024-12-04 15:29:21 +0500

    logs

--- app/controllers/api/self_tour/v1/communities_controller.rb
+++ app/controllers/api/self_tour/v1/communities_controller.rb
@@ -123,14 +123,19 @@ module Api
 
         def check_lock_access
           counter = check_lock_access_counter(@tour_user)
-
+          puts "1 - Counter #{counter}"
           if ( @community.enable_locks && (params[:tour_type] == "self_tour" || params[:tour_type] == "guided_tour"))
             if ((@community.multiple_locks_provider.include?("Igloohome") && (@tour_user.igloohome_status == "in progress")) || (@community.multiple_locks_provider.include?("Dwelo") && (@tour_user.dwelo_status == "in progress")) || (@community.multiple_locks_provider.include?("EdgeState")  && (@tour_user.edge_state_status == "in progress")) || (@community.multiple_locks_provider.include?("Latch")  && (@tour_user.latch_status == "in progress")) || (@community.multiple_locks_provider.include?("Zerv")  && (@tour_user.zerv_status == "in progress")) && !(counter >= 20))
+              puts "Lock Condition Fail"
+              puts "Counter #{counter}" 
               render :json=> {success: "false", completed: false}
             else
+              puts "Lock Condition True"
+              puts "Counter #{counter}" 
               render :json=> {success: "true", completed: true}
             end
           else
+            puts "Else Condition Fail"
             render :json=> {success: "false", completed: false}
           end
         end
