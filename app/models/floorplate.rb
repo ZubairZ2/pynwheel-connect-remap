62b3bf0d4b5c84ad9b14a8b7236f2e6ab0629201
Salahudin Sallu <salahudin@pynwheel.com>
2024-12-20 17:10:07 +0500

    pyn-1138 map ocr fix

--- app/models/floorplate.rb
+++ app/models/floorplate.rb
@@ -23,9 +23,8 @@
 #
 
 class Floorplate < ApplicationRecord
-  # has_paper_trail
   include StandardUrl
-  serialize :map_ocr_data, Array
+  serialize :map_ocr_data
 
   mount_uploader :image, SiteMapUploader
   mount_uploader :label_image, SiteMapUploader
@@ -144,6 +143,13 @@ class Floorplate < ApplicationRecord
     self.height > 0 ? self.height : self.image.height
   end
 
+  def map_ocr_data
+    read_attribute(:map_ocr_data) || []
+  end
+
+  def map_ocr_data=(value)
+    write_attribute(:map_ocr_data, value.is_a?(Array) ? value.to_json : value)
+  end
 
   private
 
--- app/models/sitemap.rb
+++ app/models/sitemap.rb
@@ -10,8 +10,8 @@
 #
 
 class Sitemap < ApplicationRecord
-  # has_paper_trail
-  serialize :map_ocr_data, Array
+  serialize :map_ocr_data
+
   mount_uploader :image, SiteMapUploader
   mount_uploader :label_image, SiteMapUploader
   mount_uploader :file, DesignUploader
@@ -111,6 +111,12 @@ class Sitemap < ApplicationRecord
     }
   end
 
+  def map_ocr_data
+    read_attribute(:map_ocr_data) || []
+  end
 
+  def map_ocr_data=(value)
+    write_attribute(:map_ocr_data, value.is_a?(Array) ? value.to_json : value)
+  end
 
 end
