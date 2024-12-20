159bdfe3c799d4667cd3b40073f69d9e59e19e54
Salahudin Sallu <salahudin@pynwheel.com>
2024-12-20 17:01:49 +0500

    pyn-1138 sitemap fix

--- app/models/sitemap.rb
+++ app/models/sitemap.rb
@@ -121,12 +121,6 @@ class Sitemap < ApplicationRecord
     }
   end
 
-  def map_ocr_data
-    read_attribute(:map_ocr_data) || []
-  end
 
-  def map_ocr_data=(value)
-    write_attribute(:map_ocr_data, value.is_a?(Array) ? value.to_json : value)
-  end
 
 end
