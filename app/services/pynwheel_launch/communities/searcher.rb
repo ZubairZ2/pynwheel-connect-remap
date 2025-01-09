0ee31af94bf67f15158074378c7866e3a0ea3d1b
Salahudin Sallu <salahudin@pynwheel.com>
2025-01-09 18:46:55 +0500

    pyn-1138 remove dummy properties

--- app/services/pynwheel_launch/communities/searcher.rb
+++ app/services/pynwheel_launch/communities/searcher.rb
@@ -43,7 +43,7 @@ attr_reader :user , :params
 
   def pynwheel_launch_access(communities)
     return unless communities.present?
-    community_users_ids = communities.joins(:community).where('communities.pynwheel_launch_access = ? ', 'true')
+    community_users_ids = communities.joins(:community).where('communities.pynwheel_launch_access = ? AND communities.name != ? ', 'true', DUMMY_COMMUNITY_NAME)
     CommunityUser.where(id: community_users_ids)
   end
 
