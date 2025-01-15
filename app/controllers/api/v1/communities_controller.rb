e26e11d31b9fee5c3c7e61c0e483a2d84df71bed
Salahudin Sallu <salahudin@pynwheel.com>
2025-01-15 16:31:44 +0500

    pyn-1138 upcoming tours fix

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -517,7 +517,7 @@ module Api
               end
     
               last_visit = get_last_visited_community(@scheduled_tours)
-              data = {tour_user: @tour_user, last_visit: last_visit, upcoming: upcoming_tours.distinct, completed: completed_tours.distinct, exipred: expired_tours.distinct }
+              data = {tour_user: @tour_user, last_visit: last_visit, upcoming: upcoming_tours.uniq, completed: completed_tours.uniq, exipred: expired_tours.uniq }
               render :json=> {data: data.as_json, :status=> true, :message => "data returned succesfully", code: 200 }
             else
               data = {tour_user: @tour_user, last_visit: last_visit, upcoming: upcoming_tours, completed: completed_tours, exipred: expired_tours }
