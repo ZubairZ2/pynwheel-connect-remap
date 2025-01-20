d7a9dc6b661c43e315e67b82083773a353479eb9
Salahudin Sallu <salahudin@pynwheel.com>
2025-01-20 14:46:48 +0500

    pyn-1166 addtional fee added on webpages

--- db/migrate/20250116100133_add_addition_pricing_options_into_communities.rb
+++ db/migrate/20250116100133_add_addition_pricing_options_into_communities.rb
@@ -4,6 +4,5 @@ class AddAdditionPricingOptionsIntoCommunities < ActiveRecord::Migration[5.0]
     add_column :communities, :display_manual_additional_fee, :boolean, default: false
 
     add_column :communities, :additional_fee, :string, default: ""
-    add_column :communities, :manual_additional_fee, :string, default: ""
   end
 end
