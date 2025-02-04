002c37c8a1d8fe1dd910e04cf3c1c68a3c1d3e62
Salahudin Sallu <salahudin@pynwheel.com>
2025-02-04 19:09:48 +0500

    pyn-1138 map ocr to jsonb conversion

--- app/models/floorplate.rb
+++ app/models/floorplate.rb
@@ -159,14 +159,6 @@ class Floorplate < ApplicationRecord
     self.height > 0 ? self.height : self.image.height rescue 0
   end
 
-  # def map_ocr_data
-  #   read_attribute(:map_ocr_data) || []
-  # end
-
-  # def map_ocr_data=(value)
-  #   write_attribute(:map_ocr_data, value.is_a?(Array) ? value.to_json : value)
-  # end
-
   private
 
 
--- app/models/sitemap.rb
+++ app/models/sitemap.rb
@@ -69,14 +69,6 @@ class Sitemap < ApplicationRecord
     self.height > 0 ? self.height : self.image.height
   end
 
-  # def map_ocr_data
-  #   read_attribute(:map_ocr_data) || []
-  # end
-
-  # def map_ocr_data=(value)
-  #   write_attribute(:map_ocr_data, value.is_a?(Array) ? value.to_json : value)
-  # end
-
   private
 
   def unit_info unit
