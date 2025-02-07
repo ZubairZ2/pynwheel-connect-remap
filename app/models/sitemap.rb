6953a1ec7b4dd1d5e4bf945321c4d16544c330d0
Salahudin Sallu <salahudin@pynwheel.com>
2025-02-07 15:52:05 +0500

    pyn-1187 svg maps performance check

--- app/models/sitemap.rb
+++ app/models/sitemap.rb
@@ -25,6 +25,9 @@ class Sitemap < ApplicationRecord
 
   validates :image, :presence => {message: "cannot be blank. Please upload site map image first."}, if: -> { image.present? }
 
+  after_save :download_image_to_public, if: ->(obj) { obj.image_changed? }
+
+
   def as_json options = {}
     super(
       :only => [:id ,:image, :file, :label_image]
@@ -60,6 +63,30 @@ class Sitemap < ApplicationRecord
   end
 
   private
+
+  def download_image_to_public
+    return unless image.present? && image.url.present?
+    # image_url = image.url 
+    image_url = "https://images-pynwheel-cms-v2.s3.amazonaws.com/uploads/floorplate/image/2041/1718911220-Meadows_SM_fl2.png"
+   
+    # Define the file path with the required naming convention
+    extension = File.extname(image_url) # Get the file extension (e.g., .jpg, .png)
+    local_file_path = Rails.root.join("public", "maps", "property_#{community.id}#{extension}")
+
+    # Delete the old file if it exists
+    File.delete(local_file_path) if File.exist?(local_file_path)
+
+    # Ensure the directory exists
+    FileUtils.mkdir_p(File.dirname(local_file_path))
+
+    # Download and save the new file
+    File.open(local_file_path, 'wb') do |file|
+      file.write(URI.open(image_url).read)
+    end
+
+  rescue StandardError => e
+    puts "\n\n\n Failed to download image: #{e.message} \n\n\n\n"
+  end
 
   def unit_info unit
     {
