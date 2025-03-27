cd7525bf3f6cab88aff79acd051ccf101cd7a998
Salahudin Sallu <salahudin@pynwheel.com>
2025-03-27 15:25:06 +0500

    Carrierwave s3 acceleration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -37,12 +37,13 @@ CarrierWave.configure do |config|
   # config.asset_host = ActionController::Base.asset_host
 
   config.max_file_size     = 500.megabytes
-  config.fog_provider = 'fog/aws'                        # required
+  config.fog_provider = 'fog/aws'                         # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                       # required
-    region:                'us-west-2'                 # optional, defaults to 'us-east-1'
+    provider:                'AWS',                         # required
+    aws_access_key_id:       ENV['AWS_ACCESS_KEY_ID'],      # required
+    aws_secret_access_key:   ENV['AWS_SECRET_ACCESS_KEY'],  # required
+    region:                  'us-west-2',                   # optional, defaults to 'us-east-1'
+    use_accelerate_endpoint: true
   }
   config.fog_directory  = ENV['S3_BUCKET_NAME'] 
   config.cache_dir = "#{Rails.root}/public/uploads/tmp"
