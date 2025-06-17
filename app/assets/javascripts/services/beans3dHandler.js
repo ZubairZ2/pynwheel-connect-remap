f07551ba9479a4daa1eee8dd60532de0c5864ff6
Salahudin Sallu <salahudin@pynwheel.com>
2025-06-17 12:01:55 +0500

    Beans 3D map amenities custmize option removed

--- app/assets/javascripts/services/beans3dHandler.js
+++ app/assets/javascripts/services/beans3dHandler.js
@@ -182,9 +182,9 @@ function getFormattedBeansAmenities() {
 
 function setup3dArray() {
   const formattedUnits = getFormattedBeansUnits();
-  const formattedAmenities = getFormattedBeansAmenities();
-  const combinedArr = [...formattedUnits, ...formattedAmenities];
-
+  // const formattedAmenities = getFormattedBeansAmenities();
+  const combinedArr = [...formattedUnits/*, ...formattedAmenities*/];
+  debugger;
   const _3dArray = convertUnitsArr(
     { address: beansAddress },
     combinedArr,
