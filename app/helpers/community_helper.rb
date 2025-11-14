8961a2e860bdc3b180e74882bd2f88e4dfb243a9
Salahudin Sallu <salahudin@pynwheel.com>
2025-11-15 01:17:26 +0500

    pyn-1337 property logo added

--- app/helpers/community_helper.rb
+++ app/helpers/community_helper.rb
@@ -3,7 +3,7 @@ module CommunityHelper
   SMALL_HEIGHT = 200
 
   def map_logo_url(community, default_logo = false)
-    logo = community.map_logo.presence
+    logo = community.map_logo.presence || community.logo.presence
     
     return ( default_logo ? asset_path("default.jpeg") : asset_path("pynwheel-connect-logo.png") )if logo.blank?
     return logo.url if logo.respond_to?(:url)
@@ -12,7 +12,7 @@ module CommunityHelper
   end
 
   def small_map_logo?(community)
-    logo = community.map_logo.presence
+    logo = community.map_logo.presence || community.logo.presence
     return false unless logo
     return safe_small_image?(logo) if logo.respond_to?(:width) && logo.respond_to?(:height)
     false
