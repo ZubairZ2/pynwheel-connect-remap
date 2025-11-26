8cf00cf0aa9c49087adb7994773417fec9fb16bf
Salahudin Sallu <salahudin@pynwheel.com>
2025-11-26 22:01:13 +0500

    pyn-1328 unit object updates for sdk

--- app/controllers/api/partner/maps/sdk_controller.rb
+++ app/controllers/api/partner/maps/sdk_controller.rb
@@ -2,6 +2,7 @@ module Api
   module Partner
     module Maps
       class SdkController < ApplicationController
+        include ApplicationHelper
         before_action :verify_api_key
         before_action :load_property, only: [:authorized, :fetch_data]
 
@@ -49,15 +50,15 @@ module Api
 
           return render_error("Property ID is missing.", 400) if property_id.blank?
 
-          @property = Community.find_by(id: property_id)
+          @community = Community.find_by(id: property_id)
 
-          return render_error("Invalid Property ID or not associated.", 404) if @property.nil?
+          return render_error("Invalid Property ID or not associated.", 404) if @community.nil?
         end
 
         def sitemap_json
-          return nil unless @property&.is_sitemap?
+          return nil unless @community&.is_sitemap?
 
-          sitemap = @property.sitemap
+          sitemap = @community.sitemap
           return nil unless sitemap
 
           {
@@ -67,9 +68,9 @@ module Api
         end
 
         def floorplates_json
-          return [] if @property.is_sitemap?
+          return [] if @community.is_sitemap?
 
-          @property.floorplates.map do |fp|
+          @community.floorplates.map do |fp|
             {
               mapId: fp.id,
               range: fp.range,
@@ -79,26 +80,65 @@ module Api
         end
 
         def units_json
-          @property.units.map do |u|
+          @community.units.map do |unit|
+            floorplan = unit.floorplan
+
             {
-              unitNumber: u.marketing_name,
-              mapId: map_for_unit(u),
-              unitId: u.id,
-              building: u.building,
-              floor: u.floor,
-              floorplanId: u.floorplan_id,
-              pointerData: u.pointer_data
+              unitNumber: unit.marketing_name,
+              mapId: map_for_unit(unit),
+              unitId: unit.id,
+              building: unit.building,
+              floor: unit.floor,
+              sold: unit.sold,
+              bedrooms: if floorplan.present?
+                          hide_decimals(floorplan.bathrooms)
+                        else
+                          nil
+                        end,
+              bathrooms:  if floorplan.present?
+                            hide_decimals(floorplan.bathrooms)
+                          else
+                            nil
+                          end,
+              square_feet:  if unit.square_feet?
+                              hide_decimals(unit.square_feet)
+                            elsif floorplan.present?
+                              hide_decimals(floorplan.square_feet)
+                            else
+                              nil
+                            end,
+              floorplanId: unit.floorplan_id,
+              pointerData: unit.pointer_data,
+              market_rent: unit.get_market_rent(),
+              availability: unit.availability,
+              availability_url: unit.get_availability_url(),
+              available_date: unit.available_date,
+              available: unit.available,
+              lease_term: unit.lease_term,
+              lease_pricing: (unit.lease_pricing.present? && unit.community.display_pricing_options) ? unit.lease_pricing : "",
+              description:  if unit.description.present?
+                              unit.description
+                            elsif floorplan.description.present?
+                              floorplan.description
+                            else
+                              ""
+                            end,
+              display_rent: unit&.community&.display_rent,
+              additional_fees: @community.get_additional_fees(unit),
+              property_id: unit.property_id,
+              unit_status: unit&.unit_status,
+              model_unit: unit&.modal_unit
             }
           end
         end
 
         def map_for_unit(unit)
-          return @property.sitemap.id if @property.is_sitemap?
-          @property.floorplate_for_floor(unit.floor)&.id
+          return @community.sitemap.id if @community.is_sitemap?
+          @community.floorplate_for_floor(unit.floor)&.id
         end
 
         def floorplans_json
-          @property.floorplans.map do |fp|
+          @community.floorplans.map do |fp|
             {
               floorplanId: fp.id,
               name: fp.name,
