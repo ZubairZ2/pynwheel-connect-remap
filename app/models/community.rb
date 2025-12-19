849613ec2197e341988d585785bf0a17a6b99b62
Salahudin Sallu <salahudin@pynwheel.com>
2025-12-19 20:09:31 +0500

    Entrata unit pricing toggle change run job

--- app/models/community.rb
+++ app/models/community.rb
@@ -97,6 +97,7 @@ class Community < ApplicationRecord
   after_create :create_property_credentials
   after_create :change_touchscreen_app_for_dwelo
   before_save :turn_off_chat, if: Proc.new { chat_control == false }
+  after_update :update_availability_data, if: -> { saved_change_to_enable_unit_type_pricing? }
   after_save :set_community_time_zone, if: ->(obj) { obj.latitude_changed? || obj.longitude_changed? }
   after_save :set_country_code, if: ->(obj) { obj.latitude_changed? || obj.longitude_changed? || obj.city_changed? || obj.state_changed? || obj.address_changed? || obj.zip_changed? }
 
@@ -1187,6 +1188,11 @@ class Community < ApplicationRecord
     when "xml"
       XmlDataUpdateWorker.perform_async self.id
     end
+  end
+
+  def update_availability_data
+    return unless data_provider === "psi"
+    EntrataDataUpdateWorker.perform_async self.id
   end
 
   def use_yardi_as_lead?
