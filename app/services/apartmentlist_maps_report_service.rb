313c8d9931a05c11ece2cf192210d401165520d8
Salahudin Sallu <salahudin@pynwheel.com>
2026-01-30 04:49:38 +0500

    updated maps service to import

--- app/services/apartmentlist_maps_report_service.rb
+++ app/services/apartmentlist_maps_report_service.rb
@@ -7,6 +7,74 @@ require 'digest'
 class ApartmentlistMapsReportService < BaseService
   PARTNER = 'apartmentlist'
   SHEET_URL = ENV['APARTMENTLIST_PROPERTIES_URL']
+
+  STATE_MAP = {
+    'alabama' => 'AL',
+    'alaska' => 'AK',
+    'arizona' => 'AZ',
+    'arkansas' => 'AR',
+    'california' => 'CA',
+    'colorado' => 'CO',
+    'connecticut' => 'CT',
+    'delaware' => 'DE',
+    'florida' => 'FL',
+    'georgia' => 'GA',
+    'hawaii' => 'HI',
+    'idaho' => 'ID',
+    'illinois' => 'IL',
+    'indiana' => 'IN',
+    'iowa' => 'IA',
+    'kansas' => 'KS',
+    'kentucky' => 'KY',
+    'louisiana' => 'LA',
+    'maine' => 'ME',
+    'maryland' => 'MD',
+    'massachusetts' => 'MA',
+    'michigan' => 'MI',
+    'minnesota' => 'MN',
+    'mississippi' => 'MS',
+    'missouri' => 'MO',
+    'montana' => 'MT',
+    'nebraska' => 'NE',
+    'nevada' => 'NV',
+    'new hampshire' => 'NH',
+    'new jersey' => 'NJ',
+    'new mexico' => 'NM',
+    'new york' => 'NY',
+    'north carolina' => 'NC',
+    'north dakota' => 'ND',
+    'ohio' => 'OH',
+    'oklahoma' => 'OK',
+    'oregon' => 'OR',
+    'pennsylvania' => 'PA',
+    'rhode island' => 'RI',
+    'south carolina' => 'SC',
+    'south dakota' => 'SD',
+    'tennessee' => 'TN',
+    'texas' => 'TX',
+    'utah' => 'UT',
+    'vermont' => 'VT',
+    'virginia' => 'VA',
+    'washington' => 'WA',
+    'west virginia' => 'WV',
+    'wisconsin' => 'WI',
+    'wyoming' => 'WY'
+  }.freeze
+
+  STREET_MAP = {
+    'street'    => 'st',
+    'avenue'    => 'ave',
+    'boulevard' => 'blvd',
+    'road'      => 'rd',
+    'lane'      => 'ln',
+    'drive'     => 'dr',
+    'court'     => 'ct',
+    'place'     => 'pl',
+    'circle'    => 'cir',
+    'parkway'   => 'pkwy',
+    'way'       => 'way'
+  }.freeze
+
   
   # example:
   # https://docs.google.com/spreadsheets/d/.../export?format=csv&gid=...
@@ -79,7 +147,7 @@ class ApartmentlistMapsReportService < BaseService
   def build_communities_fingerprint_map
     @communities_by_fingerprint = {}
 
-    Community.active_client_properties.find_each do |community|
+    Community.active_client_properties do |community|
       fp = fingerprint(
         community.address,
         community.city,
@@ -106,19 +174,40 @@ class ApartmentlistMapsReportService < BaseService
   def fingerprint(address, city, state, zip)
     Digest::SHA1.hexdigest(
       [
-        normalize("#{address} #{city}"),
-        normalize(city),
-        normalize(state),
+        normalize_address(address, city),
+        normalize_city(city),
+        normalize_state(state),
         zip.to_s.strip
       ].join('|')
     )
   end
 
+  def normalize_address(address, city)
+    combined = "#{address} #{city}"
+    normalize(combined)
+      .gsub(/\b(lane|ln|street|st|avenue|ave|road|rd|drive|dr|circle|cir)\b/, '')
+      .squeeze(' ')
+      .strip
+  end
+
+  def normalize_city(city)
+    normalize(city)
+      .gsub(/\b(lane|ln|street|st|avenue|ave|road|rd|drive|dr|circle|cir)\b/, '')
+      .strip
+  end
+
+  def normalize_state(state)
+    return '' if state.blank?
+
+    value = state.to_s.downcase.gsub(/[^\w\s]/, '').strip
+    STATE_MAP[value] || (value.length == 2 ? value.upcase : value.upcase)
+  end
+
   def normalize(value)
-    value.to_s
-         .downcase
-         .gsub(/[^\w\s]/, ' ')
-         .squeeze(' ')
-         .strip
+    return '' if value.blank?
+
+    normalized = value.downcase.gsub(/[^\w\s]/, ' ').squeeze(' ').strip
+    STREET_MAP.each { |long, short| normalized.gsub!(/\b#{long}\b/, short) }
+    normalized
   end
 end
