0bee261012bdc7a9968fd8ce2d0129591051172b
Salahudin Sallu <salahudin@pynwheel.com>
2026-03-31 19:17:55 +0500

    Prod url uncommented

--- public/sdk/pyn-map-sdk-v1.js
+++ public/sdk/pyn-map-sdk-v1.js
@@ -1659,8 +1659,8 @@
       if (this.config.environment === "staging") {
         return "https://pynwheel-staging.herokuapp.com";
       }
-      return "http://localhost:3000";
-      // return "https://pynwheelconnect.com"; // production
+      // return "http://localhost:3000";
+      return "https://pynwheelconnect.com"; // production
     }
   };
 
