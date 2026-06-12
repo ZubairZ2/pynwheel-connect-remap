90ac18c62bc521ea4136cde92765e2cc8e5699a2
Salahudin Sallu <salahudin@pynwheel.com>
2026-06-12 16:14:16 +0500

    Remove validation on rent

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -77,7 +77,7 @@ class Unit < ApplicationRecord
     "vacant unrented ready"
   ]
 
-  validates :effective_rent, :numericality => { :greater_than => 0, :less_than => 100000001 }, :length => { :maximum => 11}
+  # validates :effective_rent, :numericality => { :greater_than => 0, :less_than => 100000001 }, :length => { :maximum => 11}
   validates_uniqueness_of :provider_unit_id, scope: :community_id
   # validates_uniqueness_of :marketing_name, scope: :community_id
   has_many :amenities, as: :amenityable
