957d7cb4811fff8842a3100c37849ad0a06163ff
Salahudin Sallu <salahudin@pynwheel.com>
2026-06-19 18:44:09 +0500

    Color cache remove

--- app/models/bedroom_marker_color.rb
+++ app/models/bedroom_marker_color.rb
@@ -1,5 +1,11 @@
 class BedroomMarkerColor < ApplicationRecord
   belongs_to :community
-
   validates :bedroom, uniqueness: { scope: :community_id }
+  after_commit :bust_sdk_cache
+
+  private
+  
+  def bust_sdk_cache
+    SdkCacheService.invalidate_fetch_data(community_id)
+  end
 end
